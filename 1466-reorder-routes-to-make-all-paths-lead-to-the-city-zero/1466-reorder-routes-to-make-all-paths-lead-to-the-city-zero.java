2f6d802886ec7723b792d3d37dcc2723ebd40943
lyn <46067184+lyn200M@users.noreply.github.com>
2024-07-16 07:56:45 +0200

    Time: 169 ms (15.18%), Space: 90.3 MB (6.89%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,42 +1,41 @@
 class Solution {
+    HashMap<Integer, List<Integer>> graph;
+    boolean[] seen;
+    HashSet<String> currentRoads;
     int ans = 0;
-    HashSet<String> roads;
-    public void buildGraph(HashMap<Integer, List<Integer>> graph, int[][] connections, HashSet<String> roads) {
+
+    public String convertToHash(int n1, int n2) {
+        return (String.valueOf(n1) + "," + String.valueOf(n2));
+    }
+
+    public void buildGraph(int[][] connections) {
         for (int[] connection : connections) {
             graph.putIfAbsent(connection[0], new ArrayList<Integer>());
             graph.putIfAbsent(connection[1], new ArrayList<Integer>());
             graph.get(connection[0]).add(connection[1]);
             graph.get(connection[1]).add(connection[0]);
-            roads.add(String.valueOf(connection[0]) + "," + String.valueOf(connection[1]));
+            currentRoads.add(convertToHash(connection[0], connection[1]));
         }
     }
     public int minReorder(int n, int[][] connections) {
-        HashMap<Integer, List<Integer>> graph = new HashMap<>();
-        roads = new HashSet<>();
-        boolean[] seen = new boolean[n];
-        buildGraph(graph, connections, roads);
-        int i = 0;
+        graph = new HashMap<>();
+        seen = new boolean[n];
+        currentRoads = new HashSet<>();
+
+        buildGraph(connections);
         seen[0] = true;
-        dfs(0, graph, seen);
-        /*while (i < n) {
-            if (!seen[i]) {
-                seen[i] = true;
-                dfs(i, graph, seen);
-            }
-            i++;
-        }*/
+        dfs(0);
         return (ans);
     }
 
-    public void dfs(int node, HashMap<Integer, List<Integer>> graph, boolean[] seen) {
-        for (Integer neighbor : graph.getOrDefault(node, new ArrayList<Integer>())) {
-            if(!seen[neighbor]) {
-                if (roads.contains(String.valueOf(node) + "," + String.valueOf(neighbor)))
+    public void dfs(int city) {
+        for (int neighbor : graph.get(city)) {
+            if (!seen[neighbor]) {
+                if (currentRoads.contains(convertToHash(city, neighbor)))
                     ans++;
                 seen[neighbor] = true;
-                dfs(neighbor, graph, seen);
+                dfs(neighbor);
             }
- 
         }
     }
 }
