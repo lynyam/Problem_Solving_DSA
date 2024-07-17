fe0a2a09530ea8f0049e8bf2589ae0e824d19c9d
lyn <46067184+lyn200M@users.noreply.github.com>
2024-07-17 09:16:06 +0200

    Time: 340 ms (9.81%), Space: 183.8 MB (24.28%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,34 +1,34 @@
 class Solution {
-    boolean[] seen;
     HashMap<Integer, List<Integer>> graph;
+    boolean[] seen;
     int destination;
-
-    public void buildGraph(int[][] edges) {
-        for (int[] edge: edges) {
-            graph.putIfAbsent(edge[0], new ArrayList<>());
-            graph.putIfAbsent(edge[1], new ArrayList<>());
-            graph.get(edge[0]).add(edge[1]);
-            graph.get(edge[1]).add(edge[0]);
-        }
-    }
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         if (source == destination)
             return (true);
-        this.destination = destination;
         seen = new boolean[n];
         graph = new HashMap<>();
+        this.destination = destination;
 
         buildGraph(edges);
         seen[source] = true;
-        return (dfs(source));
+        return(dfs(source)); 
+    }
+
+    public void buildGraph(int[][] edges) {
+        for (int[] edge : edges) {
+                graph.putIfAbsent(edge[0], new ArrayList<Integer>());
+                graph.putIfAbsent(edge[1], new ArrayList<Integer>());
+                graph.get(edge[0]).add(edge[1]);
+                graph.get(edge[1]).add(edge[0]);
+        }
     }
 
     public boolean dfs(int source) {
         boolean ans = false;
-        for (int neighbor : graph.getOrDefault(source, new ArrayList<Integer>())) {
+        for (int neighbor : graph.get(source)) {
             if (!seen[neighbor]) {
                 seen[neighbor] = true;
-                ans |= (neighbor == destination || dfs(neighbor));
+                ans |= (neighbor == destination ? true : dfs(neighbor));
             }
         }
         return (ans);
