5bcd67c805dc858a0ee15de1581b8ec21414ff51
lyn <46067184+lyn200M@users.noreply.github.com>
2024-07-17 10:18:14 +0200

    Time: 2 ms (67.91%), Space: 45 MB (14.66%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -3,17 +3,17 @@ class Solution {
     int n;
     int m;
     public int maxAreaOfIsland(int[][] grid) {
-        m = grid.length;
-        n = grid[0].length;
-        seen = new boolean[m][n];
         int i = 0;
         int j = 0;
         int ans = 0;
+        m = grid.length;
+        n = grid[0].length;
+        seen = new boolean[m][n];
 
         while (i < m) {
             j = 0;
             while (j < n) {
-                if (grid[i][j] == 1 && !seen[i][j]) {
+                if (!seen[i][j] && grid[i][j] == 1) {
                     seen[i][j] = true;
                     ans = Math.max(ans, dfs(i, j, grid) + 1);
                 }
@@ -24,21 +24,21 @@ class Solution {
         return (ans);
     }
 
-    public int dfs(int row, int col, int[][] grid) {
+    public int dfs(int row, int col, int [][] grid) {
         int ans = 0;
-        if (row - 1 >= 0 && grid[row - 1][col] == 1 && !seen[row - 1][col]) {
+        if (row - 1 >= 0 && !seen[row - 1][col] && grid[row - 1][col] == 1) {
             seen[row - 1][col] = true;
             ans += (1 + dfs(row - 1, col, grid));
         }
-        if (row + 1 < m && grid[row + 1][col] == 1 && !seen[row + 1][col]) {
+        if (row + 1 < m && !seen[row + 1][col] && grid[row + 1][col] == 1) {
             seen[row + 1][col] = true;
             ans += (1 + dfs(row + 1, col, grid));
         }
-        if (col - 1 >= 0 && grid[row][col - 1] == 1 && !seen[row][col - 1]) {
+        if (col - 1 >= 0 && !seen[row][col - 1] && grid[row][col - 1] == 1) {
             seen[row][col - 1] = true;
             ans += (1 + dfs(row, col - 1, grid));
         }
-        if (col + 1 < n && grid[row][col + 1] == 1 && !seen[row][col + 1]) {
+        if (col + 1 < n && !seen[row][col + 1] && grid[row][col + 1] == 1) {
             seen[row][col + 1] = true;
             ans += (1 + dfs(row, col + 1, grid));
         }
