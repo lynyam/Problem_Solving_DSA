8240e85f402acd874e13fe4d75c2f98da336701c
lyn <46067184+lyn200M@users.noreply.github.com>
2024-07-17 08:47:42 +0200

    Time: 8 ms (97.35%), Space: 82.5 MB (61.97%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.java
@@ -1,15 +1,16 @@
 class Solution {
     public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
-        boolean[] store = new boolean[n];
+        boolean[] seen = new boolean[n];
         List<Integer> ans = new ArrayList<>();
         int i = 0;
-        
+
         for (List<Integer> edge : edges) {
-            store[edge.get(1)] = true;
+            seen[edge.get(1)] = true;
         }
         while (i < n) {
-            if (!store[i])
+            if (seen[i] == false) {
                 ans.add(i);
+            }
             i++;
         }
         return (ans);
