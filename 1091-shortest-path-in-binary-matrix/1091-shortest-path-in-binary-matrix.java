c5b62ed4d7a76a23c4abb4e2e581d37b84b4b138
lyn <46067184+lyn200M@users.noreply.github.com>
2024-07-18 11:24:58 +0200

    Time: 19 ms (40.31%), Space: 45.5 MB (37.03%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,57 +1,56 @@
-  class Path {
-        int[] cell;
-        int count;
-        public Path(int row, int col , int count) {
-            this.cell = new int[]{row, col};
-            this.count = count;
-        }
+class Path {
+    int row;
+    int col;
+    int countCel;
+    public Path(int row, int col, int countCel) {
+        this.row = row;
+        this.col = col;
+        this.countCel = countCel;
     }
-    
-    class Solution {
-    int[][] directions = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}, {1, 1}, {-1, 1}, {1, -1},{-1, -1}};
+}
+class Solution {
+    int[][] directions = new int[][]{
+        {1, 0}, {-1, 0},
+        {0, 1}, {0, -1},
+        {1, 1}, {1, -1},
+        {-1, 1}, {-1, -1}};
     int n;
-    boolean[][] seen;
-    int ans;
 
     public boolean isValid(int x, int y) {
         return (x >= 0 && x < n && y >= 0 && y < n);
     }
-    
     public int shortestPathBinaryMatrix(int[][] grid) {
-        ans = Integer.MAX_VALUE;
         n = grid.length;
-        int length = 0;
+        if (grid[0][0] != 0 || grid[n - 1][n - 1] != 0)
+            return (-1);
+        if (n == 1)
+            return (1);
         Queue<Path> queue = new LinkedList<>();
-        seen = new boolean[n][n];
-
-        if (n == 1) {
-            return (grid[0][0] == 0 ? 1 : -1);
-        }
-        if (grid[0][0] == 0 && grid[n - 1][n - 1] == 0)
-            queue.add(new Path(0, 0, 1));
+        boolean[][] seen = new boolean[n][n];
+        queue.add(new Path(0, 0, 1));
         while (!queue.isEmpty()) {
-            int i = 0;
             int size = queue.size();
+            int i = 0;
             while (i < size) {
                 Path path = queue.remove();
-                int row = path.cell[0];
-                int col = path.cell[1];
-                int count = path.count;
+                int row = path.row;
+                int col = path.col;
+                int cel = path.countCel;
+                if (col == n - 1 && row == n - 1) {
+                    return (cel);
+                }
                 for (int[] direction : directions) {
                     int neiRow = row + direction[0];
                     int neiCol = col + direction[1];
-                    if (isValid(neiRow, neiCol) && grid[neiRow][neiCol] == 0 && !seen[neiRow][neiCol] && !(neiRow == n - 1 && neiCol == n - 1)) {
+                    int neiCel = cel + 1;
+                    if (isValid(neiRow, neiCol) && grid[neiRow][neiCol] == 0 && !seen[neiRow][neiCol]) {
                         seen[neiRow][neiCol] = true;
-                        queue.add(new Path(neiRow, neiCol, count + 1));
-                    }
-                    else if (neiRow == n - 1 && neiCol == n - 1) {
-                        ans = Math.min(ans, count + 1);
+                        queue.add(new Path(neiRow, neiCol, neiCel));
                     }
                 }
                 i++;
-            }
-
+            }        
         }
-        return (ans == Integer.MAX_VALUE ? -1 : ans);
+        return (-1);
     }
 }
