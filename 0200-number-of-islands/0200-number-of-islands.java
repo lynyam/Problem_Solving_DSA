a1e6c4bf6edfbea48b92ae9b48f0b73acad69ef8
lyn <46067184+lyn200M@users.noreply.github.com>
2024-07-19 09:13:14 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,46 +1,47 @@
 class Solution {
+    int[][] directions = new int[][]{
+        {1, 0},
+        {-1, 0},
+        {0, -1},
+        {0, 1}};
     boolean[][] seen;
-    int m;
     int n;
+    int m;
     public int numIslands(char[][] grid) {
         m = grid.length;
         n = grid[0].length;
-        seen = new boolean[m][n];
+        int ans = 0;
         int i = 0;
         int j = 0;
-        int ans = 0;
+        seen = new boolean[n][n];
 
         while (i < m) {
             j = 0;
             while (j < n) {
-                if (grid[i][j] == '1' && !seen[i][j]) {
-                    ans++;
+                if (!seen[i][j] && grid[i][j] == '1') {
                     seen[i][j] = true;
+                    ans++;
                     dfs(i, j, grid);
                 }
                 j++;
-            }
+            }  
             i++;
         }
         return (ans);
     }
 
+    public boolean isValid(int x, int y) {
+        return (x >= 0 && x < m && y >= 0 && y < n && !seen[x][y]);
+    }
+
     public void dfs(int row, int col, char[][] grid) {
-        if (row - 1 >= 0 && grid[row - 1][col] == '1' && !seen[row - 1][col]) {
-            seen[row - 1][col] = true;
-            dfs(row - 1, col, grid);
-        }
-         if (row + 1 < m && grid[row + 1][col] == '1' && !seen[row + 1][col]) {
-            seen[row + 1][col] = true;
-            dfs(row + 1, col, grid);
-        }
-         if (col - 1 >= 0 && grid[row][col - 1] == '1' && !seen[row][col - 1]) {
-            seen[row][col - 1] = true;
-            dfs(row, col - 1, grid);
-        }
-         if (col + 1 < n && grid[row][col + 1] == '1' && !seen[row][col + 1]) {
-            seen[row][col + 1] = true;
-            dfs(row, col + 1, grid);
+        for (int[] direction : directions) {
+            int neiRow = row + direction[0];
+            int neiCol = col + direction[1];
+            if (isValid(neiRow, neiCol) && grid[neiRow][neiCol] == '1') {
+                seen[neiRow][neiCol] = true;
+                dfs(neiRow, neiCol, grid);
+            }
         }
     }
 }
