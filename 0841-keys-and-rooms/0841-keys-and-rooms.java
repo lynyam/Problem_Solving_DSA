93f663897a6f5531b32b8355795fa633a53bf604
lyn <46067184+lyn200M@users.noreply.github.com>
2024-07-19 18:45:47 +0200

    Time: 1 ms (77.93%), Space: 43.9 MB (78.14%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,20 +1,29 @@
 class Solution {
-    HashSet<Integer> seen;
-    int n;
+    boolean[] seen;
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        n = rooms.size();
-        seen = new HashSet<Integer>();
-        seen.add(0);
+        int n = rooms.size();
+        seen = new boolean[n];
+        seen[0] = true;
         dfs(0, rooms);
-        return (seen.size() == n);
+        return (is_all_visited(n));
     }
 
-    public void dfs(int room, List<List<Integer>> rooms) {
-        for (int neighbor : rooms.get(room)) {
-            if (!seen.contains(neighbor)) {
-                seen.add(neighbor);
+    public void dfs(int node, List<List<Integer>> rooms) {
+        for (int neighbor : rooms.get(node)) {
+            if (!seen[neighbor]) {
+                seen[neighbor] = true;
                 dfs(neighbor, rooms);
             }
         }
+    }
+
+    public boolean is_all_visited(int n) {
+        int i = 0;
+        while (i < n) {
+            if (!seen[i])
+                return (false);
+            i++;
+        }
+        return (true);
     }
 }
