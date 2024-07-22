f5b06fc03d4cbd2598a3ef462f5a080076523482
lyn <46067184+lyn200M@users.noreply.github.com>
2024-07-22 21:25:25 +0200

    Time: 4 ms (62.37%), Space: 43.6 MB (99.92%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
@@ -1,30 +1,29 @@
 class Solution {
-    List<List<Integer>> graph;
     boolean[] seen;
+    List<List<Integer>> adjList;
 
-    public void builGraph(int n, int[][] edges) {
+    void    buildGraph(int n, int[][] edges) {
         int i = 0;
         while (i < n) {
-            graph.add(new ArrayList<Integer>());
+            adjList.add(new ArrayList<>());
             i++;
         }
         for (int[] edge : edges) {
-            graph.get(edge[0]).add(edge[1]);
-            graph.get(edge[1]).add(edge[0]);
+            adjList.get(edge[0]).add(edge[1]);
+            adjList.get(edge[1]).add(edge[0]);
         }
     }
     public int countComponents(int n, int[][] edges) {
-        graph = new ArrayList<>();
         seen = new boolean[n];
+        adjList = new ArrayList<>();
         int ans = 0;
         int i = 0;
 
-        builGraph(n, edges);
-        i = 0;
+        buildGraph(n, edges);
         while (i < n) {
             if (!seen[i]) {
-                ans++;
                 seen[i] = true;
+                ans++;
                 dfs(i);
             }
             i++;
@@ -33,7 +32,7 @@ class Solution {
     }
 
     public void dfs(int node) {
-        for (int neighbor : graph.get(node)) {
+        for (int neighbor : adjList.get(node)) {
             if (!seen[neighbor]) {
                 seen[neighbor] = true;
                 dfs(neighbor);
