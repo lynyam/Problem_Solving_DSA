9b8c15acdb754b294605f70ad1e48c2dcca0d9bd
lyn <46067184+lyn200M@users.noreply.github.com>
2024-07-22 21:47:17 +0200

    Time: 40 ms (87.73%), Space: 107 MB (72.64%) - LeetHub

--- 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.java
+++ 2368-reachable-nodes-with-restrictions/2368-reachable-nodes-with-restrictions.java
@@ -3,10 +3,11 @@ class Solution {
     boolean[] seen;
     int ans = 0;
 
-    public void buildGraph(int n, int[][] edges) {
+    public void buildGraph(int n, int[][] edges)  {
         int i = 0;
+
         while (i < n) {
-            adjList.add(new ArrayList<Integer>());
+            adjList.add(new ArrayList<>());
             i++;
         }
         for (int[] edge : edges) {
@@ -16,22 +17,22 @@ class Solution {
     }
 
     public int reachableNodes(int n, int[][] edges, int[] restricted) {
-        adjList = new ArrayList<>();
         seen = new boolean[n];
+        adjList = new ArrayList<>();
         int i = 0;
 
         buildGraph(n, edges);
-        seen[0] = true;
         while (i < restricted.length) {
             seen[restricted[i]] = true;
             i++;
         }
+        seen[0] = true;
         dfs(0);
         return (ans + 1);
     }
 
-    public void dfs(int node) {
-        for (int neighbor :  adjList.get(node)) {
+    public  void    dfs(int source) {
+        for (int neighbor : adjList.get(source)) {
             if (!seen[neighbor]) {
                 ans++;
                 seen[neighbor] = true;
@@ -39,4 +40,5 @@ class Solution {
             }
         }
     }
+
 }
