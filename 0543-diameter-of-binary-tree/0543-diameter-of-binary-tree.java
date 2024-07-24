d6390dfbb0bedf18239a32edacdfbbd5d8c3ae5a
lyn <46067184+lyn200M@users.noreply.github.com>
2024-07-24 07:14:00 +0200

    Time: 0 ms (100%), Space: 44.9 MB (35.94%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -10,12 +10,7 @@
  *         this.val = val;
  *         this.left = left;
  *         this.right = right;
- *     }        1
-               2 3
-              5 6
-             7
-             1 2 (5 (7 ((null) (null)))
-             ans = 1, ret 2
+ *     }
  * }
  */
 class Solution {
