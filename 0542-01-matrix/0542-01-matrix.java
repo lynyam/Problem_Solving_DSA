6fbecc247258be31c822b15cbdc8132ce70a7896
lyn <46067184+lyn200M@users.noreply.github.com>
2024-07-24 09:29:34 +0200

    Time: 13 ms (81.58%), Space: 46.5 MB (93.52%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -4,67 +4,59 @@ class Cell {
     public Cell(int row, int col) {
         this.row = row;
         this.col = col;
-    }
+    }    
 }
-
 class Solution {
-    int m;
     int n;
-    int[][] directions = new int[][]{
-        {1, 0}, {0, 1},
-        {-1, 0}, {0, -1}
-    };
+    int m;
 
     public boolean isValid(int x, int y) {
         return (x >= 0 && x < m && y >= 0 && y < n);
     }
-
     public int[][] updateMatrix(int[][] mat) {
         m = mat.length;
         n = mat[0].length;
         int i = 0;
         int j = 0;
+        int level = 0;
+        Queue<Cell> queue = new LinkedList<>();
+        boolean[][] seen = new boolean[m][n];
+        int[][] directions = new int[][] {
+            {1, 0},{0, 1},
+            {-1, 0},{0, -1}
+        };
 
         while (i < m) {
             j = 0;
             while (j < n) {
-                if (mat[i][j] != 0) {
-                    bfs(i, j, mat);
+                if (mat[i][j] == 0) {
+                    queue.add(new Cell(i,j));
+                    seen[i][j] = true;
                 }
                 j++;
             }
             i++;
         }
-        return (mat);
-    }
-
-    public void bfs(int row, int col, int[][] mat) {
-        Queue<Cell> queue = new LinkedList<>();
-        boolean[][] seen = new boolean[m][n];
-        queue.add(new Cell(row, col));
-        int level = 0;
-        seen[row][col] = true;
         while (!queue.isEmpty()) {
             int size = queue.size();
-            int i = 0;
+            i = 0;
             while (i < size) {
                 Cell cell = queue.remove();
+                int x = cell.row;
+                int y = cell.col;
                 for (int[] direction : directions) {
-                    int x = cell.row + direction[0];
-                    int y = cell.col + direction[1];
-                    if (isValid(x, y) && !seen[x][y]) {
-                        if (mat[x][y] == 0) {
-                            mat[row][col] = Math.abs(row - x) + Math.abs(col - y);
-                            return ;
-                        }
-                        seen[x][y] = true;
-                        queue.add(new Cell(x, y));
+                    int row = x + direction[0];
+                    int col = y + direction[1];
+                    if (isValid(row, col) && !seen[row][col]) {
+                        seen[row][col] = true;
+                        mat[row][col] = level + 1;
+                        queue.add(new Cell(row, col));
                     }
                 }
                 i++;
             }
             level++;
         }
-        return ;
+        return (mat);
     }
 }
