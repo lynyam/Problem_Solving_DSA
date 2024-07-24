dd160aade2cc6cdaea8a08b0673bd64df10893b9
lyn <46067184+lyn200M@users.noreply.github.com>
2024-07-24 06:56:48 +0200

    Time: 0 ms (100%), Space: 41.7 MB (64.77%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
@@ -14,9 +14,9 @@
  * }
  */
 class Solution {
-    int ans = 0;
+    int ans = Integer.MIN_VALUE;
     public int maxAncestorDiff(TreeNode root) {
-        if (root == null)
+        if (root == null) 
             return (0);
         dfs(root, root.val, root.val);
         return (ans);
@@ -25,9 +25,9 @@ class Solution {
     public void dfs(TreeNode root, int max, int min) {
         if (root == null)
             return ;
-        ans = Math.max(ans, Math.max(Math.abs(max - root.val), Math.abs(min - root.val)));
+        ans = Math.max(ans, Math.max(Math.abs(max - root.val) , Math.abs(min - root.val)));
         max = Math.max(max, root.val);
-        min = Math.min(min, root.val);
+        min = Math.min(min,  root.val);
         dfs(root.left, max, min);
         dfs(root.right, max, min);
     }
