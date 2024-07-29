3c3b382ff822682f393a922bf229fd3c5999755e
lyn <46067184+lyn200M@users.noreply.github.com>
2024-07-29 15:58:44 +0200

    Time: 7 ms (47.71%), Space: 46.1 MB (69.26%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -16,16 +16,17 @@
 class Solution {
     public int deepestLeavesSum(TreeNode root) {
         if (root == null) return (0);
-        Queue<TreeNode> queue = new LinkedList<>();//0(n)
-        int ans = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
-        while (!queue.isEmpty()) {//0(n)time
-            int size = queue.size();
+        int sum = 0;
+
+        while (!queue.isEmpty()) {
             int i = 0;
-            ans = 0;
+            int size = queue.size();
+            sum = 0;
             while (i < size) {
                 TreeNode node = queue.remove();
-                ans += node.val;
+                sum += node.val;
                 if (node.left != null)
                     queue.add(node.left);
                 if (node.right != null)
@@ -33,6 +34,6 @@ class Solution {
                 i++;
             }
         }
-        return (ans);
+        return (sum);
     }
 }
