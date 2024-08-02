4aabc72d1287fb045fcea272d97a4f2975f352fd
lyn <46067184+lyn200M@users.noreply.github.com>
2024-08-02 13:18:00 +0200

    Time: 28 ms (38.48%), Space: 45.6 MB (39.48%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -2,22 +2,22 @@ class Cell {
     int row;
     int col;
     int obst;
-
-    public Cell(int row, int col, int obst) {
-        this.row = row;
-        this.col = col;
-        this.obst = obst;
-    }    
+    public Cell(int x, int y, int o) {
+        row = x;
+        col = y;
+        obst = o;
+    }
 }
 
 class Solution {
-    int[][] directions = new int[][]{
-        {1, 0},{0, 1},
-        {-1, 0},{0, -1}
-    };
-    int n;
     int m;
-    
+    int n;
+    int[][] directions = new int[][] {
+        {1, 0}, {0, 1},
+        {-1, 0}, {0, -1}
+    };
+    boolean[][][] seen;
+
     public boolean isValid(int x, int y) {
         return (x >= 0 && x < m && y >= 0 && y < n);
     }
@@ -25,31 +25,30 @@ class Solution {
     public int shortestPath(int[][] grid, int k) {
         m = grid.length;
         n = grid[0].length;
-        boolean[][][] seen = new boolean[m][n][k+1];
-        int level = 0;
+        seen = new boolean[m][n][k + 1];
         Queue<Cell> queue = new LinkedList<>();
+        int level = 0;
 
-        queue.add(new Cell(0, 0, 0));
         seen[0][0][0] = true;
-
+        queue.add(new Cell(0, 0, 0));
         while (!queue.isEmpty()) {
-            int size = queue.size();
             int i = 0;
+            int size = queue.size();
             while (i < size) {
                 Cell cell = queue.remove();
                 int row = cell.row;
                 int col = cell.col;
                 int obst = cell.obst;
-                if (row == m - 1 && col == n - 1 && obst <= k)
+                if (row == m - 1 && col == n - 1)
                     return (level);
                 for (int[] direction : directions) {
-                    int x = row + direction[0];
-                    int y = col + direction[1];
+                    int x = direction[0] + row;
+                    int y = direction[1] + col;
                     if (isValid(x, y)) {
-                        int neibObst = grid[x][y] == 1 ? obst + 1 : obst;
-                        if (neibObst <= k && !seen[x][y][neibObst]) {
-                            seen[x][y][neibObst] = true;
-                            queue.add(new Cell(x, y, neibObst));
+                        int neiObst = grid[x][y] == 1 ? obst + 1 : obst;
+                        if (neiObst <= k && !seen[x][y][neiObst]) {
+                            seen[x][y][neiObst] = true;
+                            queue.add(new Cell(x, y, neiObst));
                         }
                     }
                 }
@@ -60,3 +59,11 @@ class Solution {
         return (-1);
     }
 }
+/*
+    - integer mat[m][n]
+        - mat[i][j] = 0 => empty || mat[i][j] = 1 => obstacle
+    - directions moov in on step (1, 0) (0, 1) (-1, 0) (0, -1) &&  cell == 0
+    - ret : minimum number of step: upper left (0, 0) --> right corner (m-1)(n-1)
+        - contrains: you can eliminate at most k obstacles
+        - ret -1 if it is not possible
+*/
