5b9b0cfb2d8e906ee39b276cb9c52683fba43c41
lyn <46067184+lyn200M@users.noreply.github.com>
2024-08-06 12:33:40 +0200

    Time: 175 ms (34.41%), Space: 176.9 MB (30.62%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
@@ -2,37 +2,54 @@ class Solution {
     List<List<Integer>> adjList;
     boolean[] seen;
 
-    public void buildGraph(int[][] edges) {
-        for (int[] edge : edges) {
-            adjList.get(edge[0]).add(edge[1]);
-            adjList.get(edge[1]).add(edge[0]);
-        }
-    }
-
-    public boolean validPath(int n, int[][] edges, int source, int destination) {
-        seen = new boolean[n];
-        adjList = new ArrayList<>();
+    public void buildGraph(int[][] edges, int n) {
         int i = 0;
+
         while (i < n) {
             adjList.add(new ArrayList<Integer>());
             i++;
         }
-        buildGraph(edges);
+        for (int[] edge : edges) {
+            int from = edge[0];
+            int to = edge[1];
+            adjList.get(from).add(to);
+            adjList.get(to).add(from);
+        }
+    }
+
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        adjList = new ArrayList<>();
+        seen = new boolean[n];
+        
+        if (source == destination)
+            return (true);
+        buildGraph(edges, n);
+        if (adjList.get(source).isEmpty() || adjList.get(destination).isEmpty())
+            return (false);
         seen[source] = true;
         return (dfs(source, destination));
     }
 
-    public boolean dfs(int source, int destination) {
+    public boolean dfs(int s, int d) {
         boolean ans = false;
-        if (source == destination) {
+        if (s == d)
             return (true);
-        }
-        for (int neighbor : adjList.get(source)) {
-            if (!seen[neighbor]) {
-                seen[neighbor] = true;
-                ans |= dfs(neighbor, destination);
+        for (int nei : adjList.get(s)) {
+            if (!seen[nei]) {
+                seen[nei] = true;
+                ans |= dfs(nei, d);
             }
         }
         return (ans);
     }
 }
+/*
+    - bi-direction graph
+    - n vertices 0 to n - 1 (inclu..)
+    - edges is. 2D int[] edges
+        - edges[i] = [ui, vi] => ui-><-vi
+        - every vertes is connected / one edge mini / no vertex edge itself
+    - det if valid path exist s -> d
+    - in: edges, n, d, s, 
+    - ret true if  or false
+*/
