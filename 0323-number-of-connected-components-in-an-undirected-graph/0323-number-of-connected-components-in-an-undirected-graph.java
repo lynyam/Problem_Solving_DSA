eedc028f4de31931233f7b4c76b5e3a5a7ba6e42
lyn <46067184+lyn200M@users.noreply.github.com>
2024-08-07 09:40:11 +0200

    Time: 4 ms (61.93%), Space: 44.2 MB (93.06%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.java
@@ -1,34 +1,35 @@
 class Solution {
-    boolean[] seen;
     List<List<Integer>> adjList;
+    boolean[] seen;
 
-    void    buildGraph(int n, int[][] edges) {
-        int i = 0;
-        while (i < n) {
-            adjList.add(new ArrayList<>());
-            i++;
-        }
+    public void buildGraph(int[][] edges) {
         for (int[] edge : edges) {
             adjList.get(edge[0]).add(edge[1]);
             adjList.get(edge[1]).add(edge[0]);
         }
     }
+
     public int countComponents(int n, int[][] edges) {
         seen = new boolean[n];
         adjList = new ArrayList<>();
-        int ans = 0;
         int i = 0;
+        int count = 0;
 
-        buildGraph(n, edges);
+        while (i < n)  {
+            adjList.add(new ArrayList<Integer>());
+            i++;
+        }
+        buildGraph(edges);
+        i = 0;
         while (i < n) {
             if (!seen[i]) {
                 seen[i] = true;
-                ans++;
+                count++;
                 dfs(i);
             }
             i++;
         }
-        return (ans);
+        return (count);
     }
 
     public void dfs(int node) {
@@ -40,3 +41,20 @@ class Solution {
         }
     }
 }
+/*
+    - graph n nodes
+    - int n
+    - edges [][]
+            - edges[i] = [ai, bi], edge btw ai->bi
+    - ret number of connected component
+    result :
+        - builGrap ->adjList List<Integer>//0(m) m edge space 0(n + m) t
+        - walk throught on node 0 to n-1 //0(n)t
+            - if node not visited mark visted and count++
+            - call dfs on this node to visited all connected node.
+                - while neighbor in List of neighbor of node
+                    - if neighbor is not visited 
+                        - mark visited and call dfs recursive
+
+        return (count)
+*/
