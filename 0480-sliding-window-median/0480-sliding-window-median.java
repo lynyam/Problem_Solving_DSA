70b1085a645e463124bf2e720721a0ed61cd0b5e
lyn <46067184+lynyam@users.noreply.github.com>
2024-08-11 13:27:18 +0200

    Time: 96 ms (52.03%), Space: 57.2 MB (11.16%) - LeetHub

--- 0480-sliding-window-median/0480-sliding-window-median.java
+++ 0480-sliding-window-median/0480-sliding-window-median.java
@@ -1,21 +1,21 @@
 class Solution {
     PriorityQueue<Integer> leftHeap;
     PriorityQueue<Integer> rightHeap;
-    HashMap<Integer, Integer> deleteMap;
+    HashMap<Integer, Integer> markAsDelete;
     int lSize;
     int rSize;
     public double[] medianSlidingWindow(int[] nums, int k) {
-        leftHeap = new PriorityQueue<>(Comparator.reverseOrder());
-        rightHeap = new PriorityQueue<>();
-        deleteMap = new HashMap<>();
-        int n = nums.length;
-        double[] ans = new double[n - k + 1];
-        int right = 0;
-        int left = 0;
-        int i = 0;
-        lSize = 0;
-        rSize = 0;
-
+       leftHeap = new PriorityQueue<>(Comparator.reverseOrder());
+       rightHeap = new PriorityQueue<>();
+       lSize = 0;
+       rSize = 0;
+       int i = 0;
+       int n = nums.length;
+       double[] ans = new double[n - k + 1];
+       int left = 0;
+       int right = 0;
+       markAsDelete = new HashMap<>();
+       
         while (right < n) {
             int num = nums[right];
             leftHeap.add(num);
@@ -23,19 +23,19 @@ class Solution {
             rSize++;
             if (rSize > lSize) {
                 leftHeap.add(rightHeap.remove());
-                rSize--;
                 lSize++;
+                rSize--;
             }
             if (right >= k - 1) {
-                removeLeft();
-                ans[i] = k % 2 == 1 ? leftHeap.peek() : ((double)rightHeap.peek() + leftHeap.peek()) / 2.0;
+                balanceHeaps();
+                ans[i] = k % 2 == 1 ? leftHeap.peek() : ((double)leftHeap.peek() + rightHeap.peek()) / 2.0;
                 int numl = nums[left];
                 if (numl <= leftHeap.peek())
                     lSize--;
                 else
                     rSize--;
-                deleteMap.put(numl, deleteMap.getOrDefault(numl, 0) + 1);
-                removeLeft();
+                markAsDelete.put(numl, markAsDelete.getOrDefault(numl, 0) + 1);
+                balanceHeaps();
                 left++;
                 i++;
             }
@@ -44,34 +44,28 @@ class Solution {
         return (ans);
     }
 
-    public void removeLeft() {
-        while (!leftHeap.isEmpty() && deleteMap.getOrDefault(leftHeap.peek(), 0) > 0) {
-            int num = leftHeap.remove();
-            deleteMap.put(num, deleteMap.getOrDefault(num, 0) - 1);
+    public void balanceHeaps() {
+        while (!leftHeap.isEmpty() && markAsDelete.
+            getOrDefault(leftHeap.peek(), 0) > 0) {
+                    int remove = leftHeap.remove();
+                    markAsDelete.put(remove, markAsDelete.
+                        getOrDefault(remove, 0) - 1);
         }
-        while (!rightHeap.isEmpty () && deleteMap.getOrDefault(rightHeap.peek(), 0) > 0) {
-            int num2 = rightHeap.remove();
-            deleteMap.put(num2, deleteMap.getOrDefault(num2, 0) - 1);
+        while (!rightHeap.isEmpty() && markAsDelete.getOrDefault(
+                rightHeap.peek(), 0) > 0) {
+                    int remove1 = rightHeap.remove();
+                    markAsDelete.put(remove1, markAsDelete.
+                        getOrDefault(remove1, 0) - 1);
         }
         if (lSize > rSize + 1) {
-            rightHeap.add(leftHeap.remove());
-            rSize++;
-            lSize--;
+                rightHeap.add(leftHeap.remove());
+                lSize--;
+                rSize++;
         }
         if (rSize > lSize) {
-            leftHeap.add(rightHeap.remove());
-            rSize--;
-            lSize++;
+                leftHeap.add(rightHeap.remove());
+                lSize++;
+                rSize--;
         }
     }
 }
-/*
-    - median = middle val List<Int> ordered
-    - list.size() = even, median of two middle val
-    - nums[] int[]
-    - int k
-    - slide wiindow size k moove left to right
-    - only see k number in windows
-    - each time moove right by one position
-    - ret median arra for each window in orginal array
-*/
