0d176b89d0dc6d2cb9226d19f66b377a05ca96ae
lyn <46067184+lynyam@users.noreply.github.com>
2024-08-11 22:17:27 +0200

    Time: 86 ms (70.02%), Space: 44.8 MB (51.72%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.java
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.java
@@ -1,64 +1,60 @@
 class Solution {
-    HashMap<Integer, List<Integer>> graph;
+    List<List<Integer>> adjList;
+    int n;
+    int max = 0;
+    public int maximumDetonation(int[][] bombs) {
+        n = bombs.length;
+        int i = 0;
+        adjList = new ArrayList<>();
+
+        buildGraph(bombs);
+        while (i < n) {
+            max = Math.max(max, dfs(i, new boolean[n]));
+            i++;
+        }
+        return (max);
+    }
+
+    public int dfs(int node, boolean[] seen) {
+        int ans = 1;
+        seen[node] = true;
+        for (int neighbor : adjList.get(node)) {
+            if (!seen[neighbor]) {
+                ans += dfs(neighbor, seen);
+            }
+        }
+        return (ans);
+    }
 
     public void buildGraph(int[][] bombs) {
-        int n = bombs.length;
         int i = 0;
         while (i < n) {
-            graph.put(i, new ArrayList<>());
             int j = 0;
+            int xi = bombs[i][0];
+            int yi = bombs[i][1];
+            int ri = bombs[i][2];
+            adjList.add(new ArrayList<Integer>());
             while (j < n) {
-                if (i == j) {
-                    j++;
-                    continue ;
-                }
-                long x = bombs[i][0] - bombs[j][0];
-                long y = bombs[i][1] - bombs[j][1];
-                if ((long)bombs[i][2] * bombs[i][2] >= x*x + y*y) {
-                    graph.get(i).add(j);
+                int xj = bombs[j][0];
+                int yj = bombs[j][1];
+                int rj = bombs[j][2];
+                if ((double)(xi - xj) * (xi - xj) + (double)(yi - yj) * (yi - yj) <=
+                    (double)ri * ri) {
+                    adjList.get(i).add(j);
                 }
                 j++;
             }
             i++;
         }
-
-        
-    }
-
-    public int maximumDetonation(int[][] bombs) {
-        int ans = 0;
-        graph = new HashMap<>();
-        int n = bombs.length;
-        int i = 0;
-        buildGraph(bombs);
-        while (i < n) {
-            int count = dfs(i, new HashSet<>());
-            ans = Math.max(ans, count);
-            i++;
-        }
-        return (ans);
-    }
-
-    public int dfs(int node, Set<Integer> seen) {
-        int connection = 1;
-        seen.add(node);
-        for (int neighbor : graph.get(node)) {
-            if (!seen.contains(neighbor)) {
-                connection += dfs(neighbor, seen);
-            }
-        }
-        return (connection);
     }
 }
 /*
-    - list<Bomb> bombs => 
-        - bomb range = area effet can be felt
-            - area = cricle center bomb location
-        - bombs[i] = [xi, yi, ri], (xi, yi) coordinate, ri radius
-    - detonate a single bomb bombs[i]
-        - bombs[i] detonate all bombs area =2pir2 .. .. 
-    - ret. max bombs detonate if you allowed to detonate one
-
-    node = bombs[i]
-    adge = r
+    - bombs List<>
+        - bonbs[i] = [xi, yi, ri]
+    - range bpmbs[i] is area where effet can be felt
+        - area = shape circle
+            - center = loacation of bob
+            - ri = radius
+    - Detonate sigle bomb => detonate all bombs in dist() <= ri
+    - ret max number of bomb can detonate if you detyonate one
 */
