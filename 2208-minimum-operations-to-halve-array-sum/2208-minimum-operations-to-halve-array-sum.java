aaf097aca67b16f17ebf336ddfb6ca22faed2533
lyn <46067184+lynyam@users.noreply.github.com>
2024-08-11 02:25:05 +0200

    Time: 204 ms (38.38%), Space: 58.1 MB (76.75%) - LeetHub

--- 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.java
+++ 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.java
@@ -1,30 +1,28 @@
 class Solution {
     public int halveArray(int[] nums) {
-        PriorityQueue<Double> heap = new PriorityQueue<>(Comparator.reverseOrder());
-        int i = 0;
-        int n = nums.length;
-        double sum = 0;
-        double removeSum = 0;
+        double oldSum = 0;
+        double delSum = 0;
         int step = 0;
+        PriorityQueue<Double> heap = new PriorityQueue<>
+            (Comparator.reverseOrder());
 
-        while (i < n) {//0(nlogn)t 0(n) z
-            heap.add(nums[i] * 1.0);//0(logn)
-            sum += nums[i];
-            i++;
+        for (int num : nums) {
+            oldSum += num;
+            heap.add(num * 1.0);
         }
-        while (!heap.isEmpty() && removeSum < sum / 2.0) {//0(n)
+        while (!heap.isEmpty() && delSum < oldSum / 2) {
             double remove = heap.remove() / 2.0;
-            heap.add(remove); //0(logn)
-            removeSum += remove;
+            delSum += remove;
+            heap.add(remove);
             step++;
         }
         return (step);
     }
 }
 /*
-    - nums int[]
-    - nums[i] > 0
-    - 1 ope/ choose nums[i] => nums[i] = nums[i]/2
-        - u may choose this nums[i] after
-    - minimum number of ope to reduce the sum of nums/at least half
+    - nums int[] positif
+    - 1 ope choose nums[i] and make nums[i]/2
+    - ret mini number ope to reduce newsum(nums[i]) <= sum(nums[i]) /2 
+    newSum = oldSum - sumDelet <= oldSum / 2
+    sumDelet >= oldSum/ 2
 */
