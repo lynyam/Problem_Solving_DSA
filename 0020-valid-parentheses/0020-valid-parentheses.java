d90bb1475a28cf48dd141efcff914ec689a17d21
lyn <46067184+lynyam@users.noreply.github.com>
2024-08-16 20:12:16 +0200

    Time: 2 ms (52.39%), Space: 41.3 MB (81.34%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,21 +1,17 @@
 class Solution {
-
-    public boolean isMatch(char open, char close) {
-        return (open == '(' ? close == ')' : open == '{' ? close == '}' :
-            open == '[' ? close ==']' : false);
+    public boolean is_match(char c1, char c2) {
+        return (c1 == '(' ? c2 == ')' : c1 == '{' ? c2 == '}' :
+                c1 == '[' ? c2 == ']' : false);
     }
-
-    public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-
-        for (char c : s.toCharArray()){
-            if (c == '(' || c == '{' || c == '[') {
-                stack.push(c);
-            } else if (stack.empty() || !isMatch(stack.peek(), c)) {
-                return (false);
-            } else
-                stack.pop();
+    public boolean isValid(String str) {
+        List<Character> stack = new ArrayList<>();
+        for (char c : str.toCharArray()) {
+            if (stack.size() > 0 && is_match(stack.get(stack.size() - 1), c)) {
+                stack.remove(stack.size() - 1);
+                continue ;
+            }
+                stack.add(c); 
         }
-        return (stack.empty());
+        return (stack.size() == 0);
     }
 }
