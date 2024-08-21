70e961cef4c351877c7b2cf7a25c5c31daf3804f
lyn <46067184+lynyam@users.noreply.github.com>
2024-08-21 22:22:34 +0200

    Time: 31 ms (54.08%), Space: 50.9 MB (73.13%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,15 +1,16 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> heap = new PriorityQueue<>((p1, p2) -> ((p2[0] * p2[0] + p2[1] * p2[1]) - (p1[0] * p1[0] + p1[1] * p1[1])));
-        
+        PriorityQueue<int[]> heap = new PriorityQueue<>((p1, p2) -> {
+            return ((p2[0] * p2[0]) + (p2[1] * p2[1]) - ((p1[0] * p1[0]) + (p1[1] * p1[1])));});
+
         for (int[] p : points) {
             heap.add(p);
             if (heap.size() > k) {
                 heap.remove();
             }
         }
-        int i = 0;
         int[][] ans = new int[k][2];
+        int i = 0;
         while (!heap.isEmpty()) {
             ans[i++] = heap.remove();
         }
@@ -17,9 +18,10 @@ class Solution {
     }
 }
 /**
-    - points[i]= [xi, yi] int[][] point on X-Y
-    - integer k
-    - ret : k closest points to origin (0,0)
-    - distance btw 2 point X-Y plane = racine(x1-x2)2 + (y1 - y2)2)
-    - 
+    - points int[][]
+        - points[i] = [xi, yi] a point on the X-Y plane
+        - int k, 
+        - ret k closest points to (0, 0)
+        - dist btw 2 point (x1-x2)2 + (y1-y2)2
+        - ret ans in any order
  */
