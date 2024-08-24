179bc27f1e25a3511d4c7a1bc277eeda634f8c0d
lyn <46067184+lynyam@users.noreply.github.com>
2024-08-25 00:56:41 +0200

    Time: 16 ms (30.64%), Space: 42.6 MB (17.98%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,27 +1,30 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
         HashMap<Character, Character> store = new HashMap<>();
-		//HashSet<Character> check = new HashSet<>();
-        int[] check = new int[256];
-        Arrays.fill(check, 0);
-		int	i = 0;
-		int	n1 =  s.length();
-		int	n2 = t.length();
-			
-		if (n1 != n2)
-			return (false);
-		while (i < n1) {
-			char si = s.charAt(i);
-			char ti = t.charAt(i);
-            boolean b = store.containsKey(si);
-			if ((b && store.get(si) != ti) || (!b && check[ti] > 0))
-				return (false);
-			if (!b) {
-				store.put(si, ti); //{a:e, b:b, c:d}
-				check[ti]++; //{e, b, d}
-			}
-			i++;
-		}
-		return (true);
+        HashSet<Character> seen = new HashSet<>();
+        int n = s.length();
+        int i = 0;
+
+        if (n != t.length())
+            return (false);
+        while (i < n) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            if ((store.containsKey(c1) && !store.get(c1).equals(c2)) || 
+                (!store.containsKey(c1) && seen.contains(c2)))
+                return (false);
+            else if (!store.containsKey(c1)) {
+                store.put(c1, c2);
+                seen.add(c2);
+            }
+            i++;
+        }
+        return (true);
     }
 }
+/**
+    - s String
+    - t String
+    - s ans t isomorphic
+        - char in s can be replaced to get t
+ */
