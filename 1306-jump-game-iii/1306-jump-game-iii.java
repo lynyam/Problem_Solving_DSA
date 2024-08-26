61eaabc5aa921fb9e5b3e4777731b8199e8075a4
lyn <46067184+lynyam@users.noreply.github.com>
2024-08-26 08:56:01 +0200

    Time: 11 ms (20.03%), Space: 54.7 MB (92.56%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.java
+++ 1306-jump-game-iii/1306-jump-game-iii.java
@@ -1,45 +1,37 @@
 class Solution {
-    public boolean canReach(int[] arr, int start) {
-        int countZeroIndex = 0;
-        int n = arr.length;
-        boolean[] seen = new boolean[n];
-        Queue<Integer> queue = new LinkedList<>();
-        int i = 0;
+    int n;
+    public boolean isValid(int x) {
+        return (x >= 0 && x < n);
+    }
 
-        while (i < n) {
-            if (arr[i] == 0)
-                countZeroIndex++;
-            i++;
-        }
-        if (countZeroIndex == 0)
-            return (false);
-        int temp = countZeroIndex;
+    public boolean canReach(int[] arr, int start) {
+        n = arr.length;
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] seen = new boolean[n];
         queue.add(start);
         seen[start] = true;
-        while (!queue.isEmpty()) {
-                int index = queue.remove();
-                countZeroIndex -= arr[index] == 0 ? 1 : 0;
-                int n1 = index + arr[index];
-                int n2 = index - arr[index];
-                if (n1 >= 0 && n1 < n && !seen[n1]) {
-                    seen[n1] = true;
-                    queue.add(n1);
-                }
-                if (n2 >= 0 && n2 < n && !seen[n2]) {
-                    seen[n2] = true;
-                    queue.add(n2);
-                }
-        }
-        return (temp > countZeroIndex);
 
+        while (!queue.isEmpty()) {//
+            int curr = queue.remove();//5
+            if (arr[curr] == 0)//1
+                return (true);
+            if (isValid(curr + arr[curr]) && !seen[curr + arr[curr]]) {//
+                seen[curr + arr[curr]] = true;
+                queue.add(curr + arr[curr]);
+            }
+            if (isValid(curr - arr[curr]) && !seen[curr - arr[curr]]) {
+                seen[curr - arr[curr]] = true;
+                queue.add(curr - arr[curr]);
+            }
+        }
+        return (false);
     }
 }
-/*
-    - arr[] arr[i] >= 0 n = len
-    - init start = index start
-    - index i, jump to
-        - i + arr[i]
-        - i - arr[i]
-    - check if reach any index with arr[index] = 0
-    - not jump outside (isValid(index) {index >= 0 && index < n})
-*/
+/**
+    - arr 
+        - arr[i] >= 0;
+    - u are positioned at start index of array
+    - u are index i => jump i+arr[i] or i-arr[i]
+    - can u reach any index with val 0?
+    - u can not jump outside
+ */
