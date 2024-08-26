b1a4ecdefe2a142857d0635001cf32c8941fb7f4
lyn <46067184+lynyam@users.noreply.github.com>
2024-08-26 10:39:41 +0200

    Time: 23 ms (14.56%), Space: 50 MB (8.27%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
@@ -2,34 +2,24 @@ class KthLargest {
     PriorityQueue<Integer> heap;
     int k;
 
-
     public KthLargest(int k, int[] nums) {
         heap = new PriorityQueue<>();
         this.k = k;
         for (int num : nums) {
             heap.add(num);
-            if (heap.size() > k) {
+            if (heap.size() > k)
                 heap.remove();
-            }
         }
-        
     }
     
     public int add(int val) {
         heap.add(val);
-        if (heap.size() > k) {
+        if (heap.size() > k)
             heap.remove();
-        }
         return (heap.peek());
     }
 }
-/**
-    - design class to find kth largest elt in a stream
-        - kth largest elt in sorted order not distinct elt
-    - class init object with int k ans stream nums
-    - add(int val) appends int val to stream and returne the elt represent kth largest il stream
 
- */
 /**
  * Your KthLargest object will be instantiated and called as such:
  * KthLargest obj = new KthLargest(k, nums);
