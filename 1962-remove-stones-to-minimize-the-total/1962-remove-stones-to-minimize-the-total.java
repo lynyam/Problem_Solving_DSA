a704f4ef2962a8d1f7406a508be4c8e38fc04071
lyn <46067184+lynyam@users.noreply.github.com>
2024-08-26 12:00:10 +0200

    Time: 417 ms (69.95%), Space: 58.3 MB (88.32%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.java
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.java
@@ -1,30 +1,29 @@
 class Solution {
     public int minStoneSum(int[] piles, int k) {
-        double sum = 0;
+        int sum = 0;
         PriorityQueue<Integer> heap = new PriorityQueue<>(Comparator.reverseOrder());
-        for (int p : piles) {
-            heap.add(p);
-            sum += p;
+
+        for (int stones : piles) {
+            heap.add(stones);
+            sum += stones;
         }
-        while (!heap.isEmpty() && k > 0) {
-            int curr = heap.remove();
-            sum -= curr / 2;
-            heap.add(curr - curr / 2);
+        while (k > 0) {
+            int stones = heap.remove();
+            int remove = stones / 2;
+            sum -= remove;
+            heap.add(stones - remove);
             k--;
         }
-        return ((int)sum);
-        
+        return (sum);
     }
 }
 /**
     - piles int[]
-        - piles[i] = number of stones ith pile
-    - k int
-    - apply ope:
-        - choose piles[i]
-        - remove floor(piles[i]/2) stones from it
-        - apply ope recursivly
-    - ret: minimum possible total number of stones remaining after k ope
-    - floor(x) = greatest int that is smaller than or equal to x
-        - round x down
+        - piless[i] = nbr of stone in ith pile
+    - int k
+    - apply operation k times
+        - choose piles[i] remove floor(piles[i] / 2) stone from it
+        - apply more and more
+    - ret : min possible total of stones after k ope
+    - floor(x) is greatest int that is <= x
  */
