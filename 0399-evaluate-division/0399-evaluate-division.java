e91184ab8d53e79e3128abc6ec1f43f7dbf8a388
lyn <46067184+lynyam@users.noreply.github.com>
2024-08-26 22:24:43 +0200

    Time: 1 ms (98.7%), Space: 42.3 MB (19.64%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.java
+++ 0399-evaluate-division/0399-evaluate-division.java
@@ -1,72 +1,59 @@
 class Solution {
     HashMap<String, HashMap<String, Double>> graph;
-
-    public void buildGraph(List<List<String>> equations, double[] values) {
-        int i = 0;
-        int n = equations.size();
-
-        while (i < n) {
-            List<String> eq = equations.get(i);
-            String dividand = eq.get(0);
-            String diviseur = eq.get(1);
-            double quotien = values[i];
-            double invQuotien = 1 / values[i];
-            graph.putIfAbsent(dividand, new HashMap<String, Double>());
-            graph.putIfAbsent(diviseur, new HashMap<String, Double>());
-            graph.get(dividand).put(diviseur, quotien);
-            graph.get(diviseur).put(dividand, invQuotien);
-            i++;
-        }
-
-    }
+    int n;
 
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
         graph = new HashMap<>();
+        n = values.length;
         int i = 0;
-        int n = queries.size();
-        double[] ans = new double[n];
-
-        buildGraph(equations, values);
+        double[] ans = new double[queries.size()];
+        
+        for (List<String> eq : equations) {
+            String num = eq.get(0);
+            String den = eq.get(1);
+            double quotien = values[i++];
+            graph.putIfAbsent(num, new HashMap<String, Double>());
+            graph.putIfAbsent(den, new HashMap<String, Double>());
+            graph.get(num).put(den, quotien);
+            graph.get(den).put(num, 1 / quotien);
+        }
+        i = 0;
         for (List<String> query : queries) {
-            String dividand = query.get(0);
-            String diviseur = query.get(1);
-            if (!graph.containsKey(dividand) || !graph.containsKey(diviseur)) {
+            String num = query.get(0);
+            String den = query.get(1);
+            if (!graph.containsKey(num) || !graph.containsKey(den)) {
                 ans[i++] = -1.0;
                 continue ;
             }
-            double res = dfs(dividand, diviseur, 1.0, new HashSet<String>());
-            ans[i++] = res == Double.MAX_VALUE ? -1.0 : res;
+            double res = dfs(num, den, 1.0, new HashSet<String>());
+            ans[i++] = res == Double.MIN_VALUE ? -1.0 : res;
         }
         return (ans);
     }
 
-    public double dfs(String dividand, String diviseur, double ans, HashSet<String> seen) {
-        double res = Double.MAX_VALUE;
-        
-        seen.add(dividand);
-        if (dividand.equals(diviseur)) {
-            return (ans);
-        }
-        HashMap<String, Double> neighbor = graph.get(dividand);
-        for (String bridge : neighbor.keySet()) {
-            if (!seen.contains(bridge)) {
-                res = Math.min(res, dfs(bridge, diviseur, ans * neighbor.get(bridge), seen));
+    public double dfs(String num, String den, double quotian, HashSet<String> seen) {
+        double ans = Double.MIN_VALUE;
+        if (num.equals(den))
+            return (quotian);
+        seen.add(num);
+        HashMap<String, Double> connexion = graph.get(num);
+        for (String key : connexion.keySet()) {
+            if (!seen.contains(key)) {
+                ans = Math.max(ans, dfs(key, den, quotian * connexion.get(key), seen));
             }
         }
-        return (res);
-
+        return (ans);
     }
 }
-/*
-     - eq Pair[]
-        - eq[i] = [Ai, Bi]
-     - val double[]
-        - val[i] = Ai/ Bi => result of eq[i]
-    - Ai or Bi represent String single var
-    - queries
-        - queries[j] = [Cj, Dj] jth query to answ Cj / Dj
-    - ret asw of all queries | -1.0 if not
-    - if var not occ in list of eq, ans = -1.0
-
-
-*/
+/**
+    - eq List<List<String>>
+        - eq[i] = [ai, bj]
+    - value double[]
+        - val[i] = ai / bi
+    - q[i] = [cj, dj] jth  query
+        - u must find as cj/dj =?
+    - ans off all q[i] | -1
+    - ex :
+        - equations = [["a","b"],["b","c"]], values = [2.0,3.0], queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
+        - [6.00000,0.50000,-1.00000,1.00000,-1.00000]
+ */
