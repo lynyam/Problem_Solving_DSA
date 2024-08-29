61a5a355c9148dbfff0fb3b97e2d46ac5e59f855
lyn <46067184+lynyam@users.noreply.github.com>
2024-08-29 15:26:31 +0200

    Time: 105 ms (26.97%), Space: 66.6 MB (9.21%) - LeetHub

--- 0502-ipo/0502-ipo.java
+++ 0502-ipo/0502-ipo.java
@@ -1,65 +1,36 @@
 class Solution {
     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
-        int n = profits.length;
-        int[][] projects = new int[n][2];
-        PriorityQueue<Integer> heap = new PriorityQueue<>(Comparator.reverseOrder());
-        int max = w;
         int i = 0;
+        int n = capital.length;
+        long finalCap = w;
+        PriorityQueue<Integer> heap = new PriorityQueue<>(Comparator.reverseOrder());
+        int[][] projects = new int[n][2];
 
         while (i < n) {
-            projects[i] = new int[]{capital[i], profits[i]};
+            projects[i] = new int[]{profits[i], capital[i]};
             i++;
         }
-        Arrays.sort(projects, (a, b) -> Integer.compare(a[0], b[0]));
-        if (w < projects[0][0])
-            return (w);
+        Arrays.sort(projects, (a, b) -> (a[1] - b[1]));
         i = 0;
-        while (i < n) {
-            if (projects[i][0] > max && !heap.isEmpty() && k > 0) {
-                max += heap.remove();
-                k--;
+        while (k > 0) {
+            while (i < n) {
+                if (finalCap < projects[i][1]) {
+                    break ;
+                }
+                heap.add(projects[i][0]);
+                i++;
             }
-            if ((projects[i][0] > max && heap.isEmpty()) || k <= 0)
-                return (max) ;
-            if (projects[i][0] > max)
-                continue ;
-            heap.add(projects[i][1]);
-            i++;
-        }
-        while (!heap.isEmpty() && k > 0) {
-            max += heap.remove();
+            if (heap.isEmpty())
+                return ((int)finalCap);
+            finalCap += heap.remove();
             k--;
         }
-        /*int ptr = 0;
-        for (int j = 0; j < k; j++) {
-            while (ptr < n && projects[ptr][0] <= max) {
-                heap.add(projects[ptr++][1]);
-            }
-            if (heap.isEmpty()) {
-                return (max);
-            }
-            max += heap.poll();
-        }*/
-        return (max);
+        return ((int)finalCap);
     }
 }
-
 /**
-    - IPO leetcode
-    - desire to work on some projects
-    - it can finish <= k projects
-    - best way to maximize after finish <= k projects
-    - given n projects,
-        - ith project => profits[i] => capital[i] to start
-        - Init = w capital, after projet capital = w + profit[i]
-    - pick <= k dstinct projets to max final capital
-    - ret: final maximized capital
-    - ans fit in int
-    k = 2, w = 0, profits = [2,3,10], capital = [0,1,2]
-    =>0 1 => 2+3 = 5 min capital <= currCap
-    =>0 2 => 2+10 = 12 capital <= currCap with max profit
-    - init : [0, maxIndex <= cap0] => choose Max profit[[0, maxIndex <= cap0]]
-        - range min, range max <= capital ? DSA
-            - 0 1 2 3 4
-            - x x x x x 
+    - finish <= k projet
+    - maximise total capital after k project
+    - capital[i]->projects[i]
+    - w capital
  */
