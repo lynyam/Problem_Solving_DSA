8d75ca5336b078163bebdbb493909dc03e1cca46
lyn <46067184+lynyam@users.noreply.github.com>
2024-08-29 21:19:21 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -1,27 +1,22 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
         Arrays.sort(people);
+        int i = 0;
+        int n = people.length;
         int ans = 0;
-        int left = 0;
-        int right = people.length - 1;
 
-        while (left <= right) {
-            if (people[left] + people[right] <= limit) {
-                left++;
-            }
-            right--;
+        while (i < n) {
+            int wi = people[i];
+            if (i + 1 < n && wi + people[i + 1] <= limit)
+                i++;
+            i++;
             ans++;
         }
         return (ans);
     }
 }
 /**
-    - peaple array[]
-        - people[i] = w of ith person
-    - infinit number of boats
-        - each boat can carry limit weight
-        - each boat carries at most 2 peoples at same time provided sum of weight of those people it at most limit
-    - ret mini number of boats to carry every given person
-    - ex:
-        people = [1,2], limit = 3
+    - people[i]
+    - boat carry max limit weight 2 people at same time
+    - ret max nbr of boats to carry every person
  */
