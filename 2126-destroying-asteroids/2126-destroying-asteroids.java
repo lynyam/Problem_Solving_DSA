ae36c4a1910a8ead0703b00fb113af7041a0e681
lyn <46067184+lynyam@users.noreply.github.com>
2024-08-29 13:58:44 +0200

    Time: 21 ms (93.83%), Space: 59.8 MB (87.5%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.java
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -1,32 +1,21 @@
 class Solution {
     public boolean asteroidsDestroyed(int mass, int[] asteroids) {
-        List<Integer> local = new ArrayList<>();
-        Arrays.sort(asteroids);//3, 5, 9, 19, 21
+        Arrays.sort(asteroids);
         int i = 0;
         int n = asteroids.length;
+        long masses = mass;
+
         while (i < n) {
-            int ast = asteroids[i];
-            if (ast <= mass) {
-                local.add(ast);//3,5,21
-                i++;
-                continue ;
-            }
-            if (!local.isEmpty()) {
-                mass += local.get(local.size() - 1);//mass=38
-                local.remove(local.size() - 1);//3 5
-            }
-            else
+            if (masses < asteroids[i])
                 return (false);
+            masses += asteroids[i];
+            i++;
         }
-        return (local.get(local.size() - 1) <= mass);
-    }       
+        return (true);
+    }
 }
 /**
-    - mass int: planet mass original
-    - asteroids int[] | asteroid[i] is mass of ith asteroid
-    - u can arrage for planet collide with aster.. in arbitr.. order
-        - if mass >= asteroidd[i]
-            - art.. destro.. mass += aster..[i]
-        - else planet destroyed
-    - ret true if all asteroids can be. destroyed. | false 
+    - mass planet
+    - asteroid[n] mass of asteroid
+    - if mass >= mass asteroid mass += mass asteroid
  */
