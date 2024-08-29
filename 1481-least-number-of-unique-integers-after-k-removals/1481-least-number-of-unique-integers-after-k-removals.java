a64e2b35276d74f0c90b0523ae4e6f0a5e5a5853
lyn <46067184+lynyam@users.noreply.github.com>
2024-08-29 20:27:34 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.java
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.java
@@ -1,4 +1,4 @@
-class Solution {//klogn + nlogn = (n + k)logn
+class Solution {
     public int findLeastNumOfUniqueInts(int[] arr, int k) {
         HashMap<Integer, Integer> store = new HashMap<>();
         PriorityQueue<Integer> heap = new PriorityQueue<>((a, b) -> store.get(a) - store.get(b));
@@ -6,30 +6,24 @@ class Solution {//klogn + nlogn = (n + k)logn
         for (int num : arr) {
             store.put(num, store.getOrDefault(num, 0) + 1);
         }
-        for (int key : store.keySet()) {
-            heap.add(key);
+        for (int num : arr) {
+            heap.add(num);
         }
-        int i = 0;
         while (k > 0) {
-            int min = heap.remove();
-            int freq = store.get(min);
+            int num = heap.remove();
+            int freq = store.get(num);
             if (freq <= k) {
                 k -= freq;
-                store.remove(min);
-            }
-            else {
-                heap.add(min);
+                store.remove(num);
+            } else {
                 break ;
             }
         }
-        return (heap.size());
+        return (store.size());
     }
 }
 /**
-    - arr int[]
+    - arr[n]
     - int k
-    - find least number of unique integer, after removing k elts
-    - ex: 
-        [5,5,4] k = 1
-        1
+    - find the least number of unique int / after removing exactly k elt
  */
