c759c8dcd2730a1fa31a08b84a6cb87ede9f6d58
lyn <46067184+lynyam@users.noreply.github.com>
2024-09-05 16:24:33 +0200

    Time: 28 ms (65.25%), Space: 48.5 MB (11.72%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.java
@@ -2,36 +2,32 @@ class Solution {
     public int smallestDivisor(int[] nums, int threshold) {
         int left = 1;
         int right = 0;
-        int n = nums.length;
-        int i = 0;
 
-        while (i < n) {
-            right = Math.max(right, nums[i]);
-            i++;
+        for (int num : nums) {
+            right = Math.max(right, num);
         }
         while (left < right) {
             int mid = left + (right - left) / 2;
             if (check(mid, nums, threshold)) {
-                left = mid + 1;
+                right = mid;
             }
             else
-                right = mid;
+                left = mid + 1;
         }
         return (left);
     }
 
-    public boolean check(int mid, int[] nums, int threshold) {
-        long sum = 0;
+    public boolean check(int mid, int[] nums, int th) {
+        int sum = 0;
         for (int num : nums) {
-            sum += (num % mid == 0 ? num / mid : (num / mid) + 1);
+            sum += (int)Math.ceil(num / (double)mid);
         }
-        return (sum > threshold); //6 > 6
+        return (sum <= th);
     }
 }
 /**
-    - nums, th, sum += nums[i] / div
-    - smallest divisor <= th
-    - 7/3 = 3 10/2=5
-    -  mid grand > res proche de thresolt
-        - check > res => left = mid + 1
+    - nums[] . int th
+    - int div > 0 do sum += nums[i++] / div 
+    - find a small divisor / res <= th
+    - rounded division
  */
