54a0e3bdd10595f49aa6b34439bae2aa85f1b24e
lyn <46067184+lynyam@users.noreply.github.com>
2024-09-06 06:59:44 +0200

    Time: 21 ms (57.82%), Space: 44.6 MB (96.61%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,17 +1,15 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-        //Arrays.sort(piles);
-        int n = piles.length;
         int left = 1;
-        int right = 0;
-        for (int bananas: piles) {
+        int right = 1;
+
+        for (int bananas : piles) {
             right = Math.max(right, bananas);
         }
-        //int right = piles[n - 1];
-        while (left < right) {
+        while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (check(piles, mid) <= h) {
-                right = mid;
+            if (check(mid, piles, h)) {
+                right = mid - 1;
             }
             else
                 left = mid + 1;
@@ -19,24 +17,18 @@ class Solution {
         return (left);
     }
 
-    public int check(int[] piles, int mid) {
-        int ans = 0;
-        int i = 0;
-        while (i < piles.length) {
-            int pile = piles[i];
-            ans += pile % mid == 0 ? pile / mid : (pile / mid) + 1;
-            i++;
+    public boolean check(int mid, int[] piles, int h) {
+        long hour = 0;
+        for (int bans : piles) {
+            hour += (long)Math.ceil(bans / (double)mid);
         }
-        return (ans);
+        return (hour <= h);
     }
 }
 /**
     - n piles bananas
-    - piles int[] ith pile has piles[i] 
-    - h hours
-    - k bananas/hour speed
-    - if pils[i] <= k eat all of them and not eat other bananas in this hour
-    - eat all bananas befor guards come
-    - ret minimum k/ eat all bananas in h hours
-    - ex: piles = [3,6,7,11], h = 8 => 4
+    - piles[i] = nbr banana pile ith
+    - gard come in h hours
+    - speed k bananas/h pile[i] < k eat all piles or eat rest after
+    - ret mini k int/eat all bananas within h hours
  */
