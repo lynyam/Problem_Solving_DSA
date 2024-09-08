f69b02e7608b53411bf9a13366d33a26ecdde94d
lyn <46067184+lynyam@users.noreply.github.com>
2024-09-08 05:02:51 +0200

    Time: 18 ms (78.44%), Space: 94.5 MB (63.82%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,11 +1,14 @@
 class Solution {
     List<List<Integer>> ans;
+    int n;
     public List<List<Integer>> combine(int n, int k) {
+        this.n = n;
         ans = new ArrayList<>();
-        backtrack(new ArrayList<Integer>(), 1, k, n);
+        backtrack(new ArrayList<Integer>(), 1, k);
         return (ans);
     }
-    public void backtrack(List<Integer> curr, int i, int k, int n) {
+
+    public void backtrack(List<Integer> curr, int i, int k) {
         if (curr.size() == k) {
             ans.add(new ArrayList<Integer>(curr));
             return ;
@@ -13,14 +16,17 @@ class Solution {
         int j = i;
         while (j <= n) {
             curr.add(j);
-            backtrack(curr, j + 1, k, n);
+            backtrack(curr, j + 1, k);
             curr.remove(curr.size() - 1);
             j++;
         }
     }
 }
 /**
-    - int n, int k
-    - all possible combinaisons of k numbers chosen from range [1, n]
-    - ans in order
+    - n, k int
+    - ret comb(knumber) range[1, n] any order 
+    k = 2 n = 4 => 1, 2, 3, 4
+                            []
+                [1]        [2]     [3]      [4]
+            [1 2]         [2 3]    [3 4]
  */
