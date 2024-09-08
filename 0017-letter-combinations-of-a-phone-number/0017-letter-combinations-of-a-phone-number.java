d7c989bf6cc9f87eb0f8f306e51b98667748caf1
lyn <46067184+lynyam@users.noreply.github.com>
2024-09-09 00:19:54 +0200

    Time: 1 ms (64.07%), Space: 42.2 MB (33.16%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,47 +1,41 @@
 class Solution {
     String[] map = new String[]{
-            "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+        "", "", "abc", "def", "ghi", "jkl",
+        "mno", "pqrs", "tuv", "wxyz"
     };
     List<String> ans;
     public List<String> letterCombinations(String digits) {
-        if (digits.length() == 0) 
+        if (digits.length() == 0)
             return (new ArrayList<>());
         ans = new ArrayList<>();
-        backtrack(new StringBuilder(), 0, digits);
+        backtrack(new StringBuilder(), digits, 0);
         return (ans);
     }
 
-    public void backtrack(StringBuilder curr, int i, String digits) {
-        int n = digits.length();
-        if (i == n) {
+    public void backtrack(StringBuilder curr, String digits, int i) {
+        if (i == digits.length()) {
             ans.add(new String(curr));
             return ;
         }
-        int num = digits.charAt(i) - '0';
-        String si = map[num];
-        for (char c : si.toCharArray()) {
+        String str = map[digits.charAt(i) - '0'];
+        for (char c : str.toCharArray()) {
             curr.append(c);
-            backtrack(curr, i + 1, digits);
+            backtrack(curr, digits, i + 1);
             curr.deleteCharAt(curr.length() - 1);
         }
     }
 }
 /**
-    - string "2 to 9"
-    - ret all possible letter comb that the number could represent
-    - ret any order
-    - 1 not map any letter 23 2(abc) 3(def)
-        curr                        []
-        curr        [a]             [b]         [c]
-        curr    [d] [e] [f]     [d][e][f]      [d][e][f]
-        if(curr.length == digits.length()) base case
-
-        String[10] = {
-            "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
-        };
-        for (i : digit)
-            i = i - 0;
-
-
-
+    - string digit 2-9
+    - ret all possible comb that the number could represent
+    
+        "23"
+                                []
+        2           [a]         [b]         [c]
+        3        [d] [e] [f]  [d][e][f]   [d][e][f]
+        => leaf => result
+            base case (curr.length == digits.length)
+            str = map[i]
+            while (c : str)
+                bcktr(curr, i + 1, ..)
  */
