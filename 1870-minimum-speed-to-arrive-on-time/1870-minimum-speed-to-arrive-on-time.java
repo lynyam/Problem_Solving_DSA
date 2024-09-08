7a558a8274527c639ad5fe327d1ce1d3e98b283c
lyn <46067184+lynyam@users.noreply.github.com>
2024-09-08 04:05:44 +0200

    Time: 179 ms (17.56%), Space: 57 MB (70.74%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.java
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.java
@@ -2,44 +2,38 @@ class Solution {
     public int minSpeedOnTime(int[] dist, double hour) {
         int left = 1;
         int right = (int)Math.pow(10, 7);
-        int i = 0;
 
-        if (dist.length > Math.ceil(hour)) {
+        if (dist.length > (int)Math.ceil(hour))
             return (-1);
-        }
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (check(mid, dist, hour)) {
+            if (check(mid, dist, hour))
                 right = mid - 1;
-            }
             else
                 left = mid + 1;
         }
         return (left);
     }
 
-    public boolean check(int mid, int[] dist, double hour) {
-        double h = 0;
-        int i = 0;
-
+    public boolean check(int speed, int[] dist, double hour) {
+        double h = 0.0;
         for (int d : dist) {
-            if (i < dist.length - 1)
-                h += (d % mid == 0 ? d / mid : (d / mid + 1));
-            else
-                h += d / (double)mid;
-            i++;
+            h = Math.ceil(h);
+            //h = h / 1 == 1 ? (h / 1) + 1 : h;
+            h += d / (double)speed;
         }
         return (h <= hour);
     }
 }
 /**
-    - hour float = your time
-    - n train, trains[i] = dist km for the train i
-    - each train can only depart at int hour => arrondi
-    - ret minimum positive int speed (km /h) | -1
-    - v augmente => h diminue
-    - V diminue => h augmente
-    - h > 6 left = mid + 1
-    - h <= 6 
+    - hour reach office
+    - n train dist[i] = ith train distance
+        - each hour take minimum 1h
+    -   ret minim positive int speed | -1 if not
+    - max speed = 10pow7
+    - v = d / t => t = d / v
+        - v >  => t <
+        - t > => v <
+        -  t <= hour grow time => down v => right = mid - 1
  */
