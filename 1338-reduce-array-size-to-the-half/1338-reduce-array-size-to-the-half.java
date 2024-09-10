de67b4439cfdeecc0064c41e82a7d49972ee4549
lyn <46067184+lynyam@users.noreply.github.com>
2024-09-10 07:52:53 +0200

    Time: 46 ms (17.32%), Space: 55.7 MB (89.26%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -1,27 +1,25 @@
 class Solution {
     public int minSetSize(int[] arr) {
         HashMap<Integer, Integer> store = new HashMap<>();
+        PriorityQueue<Integer> heap = new PriorityQueue<>((a, b) -> store.get(b) - store.get(a));
+        int ans = 0;
 
         for (int num : arr) {
             store.put(num, store.getOrDefault(num, 0) + 1);
         }
-        List<Integer> values = new ArrayList<>(store.values());
-        Collections.sort(values, Collections.reverseOrder());
-        int ans = 0;
-        int size = 0;
-        for (int freq : values) {
-            size += freq;
+        for (int key : store.keySet()) {
+            heap.add(key);
+        }
+        int half = 0;
+        while (half < arr.length / 2) {
+            half += store.get(heap.remove());
             ans++;
-            if (size >= arr.length / 2)
-                break ;
         }
         return (ans);
     }
 }
 /**
-    - arr int[]
-    - u can choose a set of int and remove all occ in arr
-    - ret minimim size of the set so that at least half of int are removed
-    -  ex : arr = [3,3,3,3,5,5,5,2,2,7] => 2
-
+    - arr[]
+    - choose set of int and  remove all occ of thses int in arr
+    - ret mini size of the set so that at leat half of the int of arry are removed
  */
