12437369552686a651eef303e7b672b6c17ce4a6
lyn <46067184+lynyam@users.noreply.github.com>
2024-09-10 02:41:21 +0200

    Time: 147 ms (50.92%), Space: 41.1 MB (98.26%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,26 +1,28 @@
 class Solution {
-    int[][] directions = new int[][]{
-        {-1, 0}, {1, 0},
-        {0, -1}, {0, 1}
+    int[][] directions = new int[][] {
+        {-1, 0}, {0, -1},
+        {1, 0}, {0, 1}
     };
-    int m;
     int n;
-    public boolean isValid(int x, int y) {
-        return (x >= 0 && x < m && y >= 0 && y < n);
+    int m;
+    public boolean  isValid(int x,  int y) {
+        return(x >= 0 && x < m && y >=0 && y < n); 
     }
 
     public boolean exist(char[][] board, String word) {
         m = board.length;
         n = board[0].length;
         int i = 0;
+
         while (i < m) {
             int j = 0;
             while (j < n) {
                 if (board[i][j] == word.charAt(0)) {
                     boolean[][] seen = new boolean[m][n];
                     seen[i][j] = true;
-                    if (backtrack(i, j, 1, board, seen, word))
+                    if (backtrack(i, j, 1, seen, board, word))
                         return (true);
+                    seen[i][j] = false;
                 }
                 j++;
             }
@@ -29,16 +31,17 @@ class Solution {
         return (false);
     }
 
-    public boolean backtrack(int x, int y, int start, char[][] board, boolean[][] seen, String word) {
-        if (start == word.length())
+    public boolean backtrack(int x, int y, int start, boolean[][] seen, char[][] board, String word) {
+        if (start == word.length()) {
             return (true);
+        }
         boolean ans = false;
         for (int[] dir : directions) {
             int row = x + dir[0];
             int col = y + dir[1];
             if (isValid(row, col) && !seen[row][col] && board[row][col] == word.charAt(start)) {
                 seen[row][col] = true;
-                ans |= backtrack(row, col, start + 1, board, seen, word);
+                ans |= backtrack(row, col, start + 1, seen, board, word);
                 seen[row][col] = false;
             }
         }
@@ -46,13 +49,6 @@ class Solution {
     }
 }
 /**
-    - mxn of char board[][] ans string word
-    - ret true id word exists in the  grid
-    - construct word with seq adj cells
-    - use letter once
-    basecase i = word.length
-                return (true)
-    for (dir : directions) 
-        if (!isValid(x, y) || set.contains(x#y))
-            continue ;
+    - mxn grid board, string word
+    - ret true if word exists in the grid
  */
