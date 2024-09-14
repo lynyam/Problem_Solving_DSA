76c238d226e33d3aab9f034babd777d5f894411f
lyn <46067184+lynyam@users.noreply.github.com>
2024-09-14 23:50:45 +0200

    Time: 0 ms (100%), Space: 41.8 MB (58.73%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
@@ -14,30 +14,33 @@
  * }
  */
 class Solution {
-    int ans = 0;
+    int result;
     public int maxAncestorDiff(TreeNode root) {
-        if (root == null)
-            return (0);
         dfs(root, root.val, root.val);
-        return (ans);
+        return (result);
     }
 
-    public void dfs(TreeNode root, int min, int max) {
+    public void dfs(TreeNode root, int max, int min) {
         if (root == null)
             return ;
-        ans = Math.max(ans, Math.max(Math.abs(min - root.val), Math.abs(max - root.val)));
-        min = Math.min(min, root.val);
-        max = Math.max(max, root.val);
-        dfs(root.left, min, max);
-        dfs(root.right, min, max);
+        result = Math.max(result, Math.max(Math.abs(max - root.val), 
+            Math.abs(min - root.val)));
+        dfs(root.left, Math.max(max, root.val), Math.min(min, root.val));
+        dfs(root.right, Math.max(max, root.val), Math.min(min, root.val));
     }
 }
-/*
-    - root BT
-    - find maxVal v
+/**
+    - root  binary tree
+    - find max  val v
         - v = |a.val - b.val|
-        - a is ancestor of b
-            - a = b
-            - a child is ancestor of b 
-
-*/
+    - a is ancestor of b
+    - a accesestor of b if :
+        - any child of a = b
+        - any child of a is ancestor of b
+    - max, min
+    - each node :
+        - update result with max(max-node.val, max, min-node.val)
+        - update max  and min
+        ..
+    return result
+ */
