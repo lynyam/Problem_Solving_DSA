fff3754eed214d9472891cc602b269c6a3ef824d
lyn <46067184+lynyam@users.noreply.github.com>
2024-09-18 03:25:37 +0200

    Time: 12 ms (81.44%), Space: 45 MB (48.09%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -3,32 +3,45 @@ class Solution {
         int left = 1;
         int right = 1;
 
-        for (int bananas : piles) {
-            right = Math.max(right, bananas);
+        for (int  pile : piles) {
+            right = Math.max(right, pile);
         }
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (check(mid, piles, h)) {
+            if (check(mid, piles, h))
                 right = mid - 1;
-            }
-            else
+            else 
                 left = mid + 1;
         }
         return (left);
     }
 
     public boolean check(int mid, int[] piles, int h) {
-        long hour = 0;
-        for (int bans : piles) {
-            hour += (long)Math.ceil(bans / (double)mid);
+        long hh = 0;
+        for (int pile : piles) {
+            hh += (pile / mid) + (pile % mid > 0 ? 1 : 0);
         }
-        return (hour <= h);
+        return (hh <= h);
     }
 }
 /**
-    - n piles bananas
-    - piles[i] = nbr banana pile ith
-    - gard come in h hours
-    - speed k bananas/h pile[i] < k eat all piles or eat rest after
-    - ret mini k int/eat all bananas within h hours
+    - n piles
+        - piles ith has piles[i] bananas
+    - h hours time to eats
+    - speed k = nb banana/ hour
+    - each hour -> k bananas on pile
+        - if pile[i] < k, 1 pile / h or nb h = pile[i] / k + 1 (if pile[i] % k > 0)
+    - wants to finsh banans before the gards return
+    - ret minimum int k /can eat all the bananas within h hours
+
+    Solution:
+        k existe such that koko eat all banana in h hours. if speed > k solution is again good
+        - use binanry search
+        - left = 1 minimum speed because 0 +> koko never eat
+        - right = max piles[i]. because if k > piles[i] koko eat 1 pile / hour
+        check(mid) {
+            for each pile
+                H += Math.ceil (piles[i] / (double)k)
+            return (H <= h)
+        }
  */
