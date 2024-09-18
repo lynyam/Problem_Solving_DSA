128e1f94b62c24a2f2a617f5abed2d8339c9a378
lyn <46067184+lynyam@users.noreply.github.com>
2024-09-19 01:27:51 +0200

    Time: 1 ms (53.22%), Space: 42.6 MB (69.9%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,30 +1,29 @@
 class Solution {
-    List<List<Integer>> ans; 
+    List<List<Integer>> ans;
     public List<List<Integer>> subsets(int[] nums) {
         ans = new ArrayList<>();
-        int i = 0;
-        int n = nums.length;
-        backtrack(0, new ArrayList<Integer>(), nums);
+        backtrack(new ArrayList<>(), 0, nums);
         return (ans);
     }
 
-    public void backtrack(int i, List<Integer> curr, int[] nums) {
-        if (i > nums.length) {
-            return ;
-        }
-        ans.add(new ArrayList<Integer>(curr));
+    public void backtrack(List<Integer> curr, int i, int[] nums) {
+        ans.add(new ArrayList<>(curr));
         int j = i;
         while (j < nums.length) {
-            int num = nums[j];
-            curr.add(num);
-            backtrack(j + 1, curr, nums);
+            curr.add(nums[j]);
+            backtrack(curr, j + 1, nums);
             curr.remove(curr.size() - 1);
             j++;
         }
     }
 }
 /**
-    - nums[] 
-    - ret all posssible subsets (the power set) backtracking
-    - not dupplicate subset
+    - nums int[] unique elt
+    - ret all possible subset
+    - not contains duplicate subset, any order
+
+                                                []
+                        [1]                     [2]                     [3]
+                    [1 2] [1 3]         [2 3]
+            [1 2 3]
  */
