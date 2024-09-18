bf2df69436bc9c6b48c1c83b43b2f23fc9bc29b7
lyn <46067184+lynyam@users.noreply.github.com>
2024-09-18 02:07:54 +0200

    Time: 0 ms (100%), Space: 51.4 MB (16.17%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.java
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.java
@@ -16,15 +16,32 @@
 class Solution {
     int sum = 0;
     public int rangeSumBST(TreeNode root, int low, int high) {
-        if (root == null) return (0);
-        if (root.val >= low && root.val <= high)
-            sum += root.val;
-        if (root.val > low) {
-            rangeSumBST(root.left, low, high);
-        }
-        if (root.val < high) {
-            rangeSumBST(root.right, low, high);
-        }
+        dfs(root, low, high);
         return (sum);
     }
+
+    public void dfs(TreeNode root, int low, int high) {
+        if (root == null)
+            return ;
+        if (low <= root.val && root.val <= high)
+            sum += root.val;
+        if (root.val > low)
+            dfs(root.left, low, high);
+        if (root.val < high)
+            dfs(root.right, low, high);
+    }
 }
+/**
+    - root TreeNOde BST
+    - low, hight int
+    - sum of values of all nodes with value in [low,  hight]
+
+    Solution
+    - dfs on all node ande check if value is in [low, right]
+    OPtimisation
+    - dfs but for all node check:
+        - if node.val > low
+            call dfs on left child
+        if node.val < right
+            call dfs on right child
+ */
