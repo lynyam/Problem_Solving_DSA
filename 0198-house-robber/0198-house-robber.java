4fd92ca2d0c03fdcffd263679e2ae2bfa8c93712
lyn <46067184+lynyam@users.noreply.github.com>
2024-09-18 07:51:10 +0200

    Time: 1 ms (5.14%), Space: 41.3 MB (16.19%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,54 +1,15 @@
 class Solution {
-    //Bottom- up optimiser
-    public int rob(int[] nums) {
-        int n = nums.length;
-        int backOne = nums[0];
-        int backTwo = nums[0];
-
-        int i = 0;
-        while (i < n) {
-            if (i == 1)
-                backOne = Math.max(nums[0], nums[1]);
-            if (i > 1) {
-                int temp = backOne;
-                backOne = Math.max(backOne, backTwo + nums[i]);
-                backTwo = temp;
-            }
-            i++;
-        }
-        return (backOne);
-    }
-    /*
-    //Bottom- up
-    public int rob(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n];
-
-        int i = 0;
-        while (i < n) {
-            if (i == 0)
-                dp[0] = nums[0];
-            if (i == 1)
-                dp[1] = Math.max(nums[0], nums[1]);
-            if (i > 1) {
-                dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
-            }
-            i++;
-        }
-        return (dp[n - 1]);
-    }*/
-}
-
-/**class Solution {
     HashMap<Integer, Integer> memo;
     public int rob(int[] nums) {
         memo = new HashMap<>();
-        return(dp(nums.length - 1, nums));
+        if (nums.length == 0)
+            return (0);
+        return (dp(nums.length - 1, nums));
     }
-    
+
     public int dp(int i, int[] nums) {
         if (i == 0)
-            return (nums[i]);
+            return (nums[0]);
         if (i == 1)
             return (Math.max(nums[0], nums[1]));
         if (memo.containsKey(i))
@@ -56,15 +17,14 @@ class Solution {
         memo.put(i, Math.max(dp(i - 1, nums), dp(i - 2, nums) + nums[i]));
         return (memo.get(i));
     }
-}*/
+}
 /**
- - nums[i] = amount of money of house i
- - contrains = adj houses have secu systems/ if two adj house brokeninto on same night
- - ret : max amount of money u can rob 1 night wiithout aleting police
+    - two adj houses were broken into on the same night
+    - nums[i] amount of money of each house
+    - ret max amount of money you can ron tonight without aleting police
 
- Solution 
- - Top - Down
-    1- retun = max amount of money dp(State)/ state =>(index i d'une maison)
-    2- recursif formula: F(n) = max (F(n-1), F(n-2) + nums[n])
-    3- base case F(0) = nums[0] , F(1) = max(nums[0], nums[1])
+    Solution
+    - return max amount of money Fn(State) state = i [0, n-1] house
+    - Function : Fn(n) = max(F(n-1), F(n-2) + nums[n])
+    - base case: F(0) = nums[0], F(1) = max(nums[0], nums[1])
  */
