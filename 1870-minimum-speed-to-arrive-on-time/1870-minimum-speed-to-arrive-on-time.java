34f48094f5a6ea6d0b045c19919a39f737eba437
lyn <46067184+lynyam@users.noreply.github.com>
2024-09-19 00:48:57 +0200

    Time: 180 ms (14.37%), Space: 57.2 MB (29.01%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.java
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.java
@@ -1,38 +1,57 @@
 class Solution {
     public int minSpeedOnTime(int[] dist, double hour) {
-        int left = 1;
+        int left = 0;
         int right = (int)Math.pow(10, 7);
 
-        if (dist.length > (int)Math.ceil(hour))
-            return (-1);
+        if (dist.length > Math.ceil(hour)) return (-1);
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (check(mid, dist, hour))
+            if (check(mid, dist, hour)) {
                 right = mid - 1;
-            else
+            }
+            else 
                 left = mid + 1;
         }
         return (left);
     }
 
-    public boolean check(int speed, int[] dist, double hour) {
-        double h = 0.0;
+    public boolean check(int mid, int[] dist, double hour) {
+        double hh = 0.0;
         for (int d : dist) {
-            h = Math.ceil(h);
-            h += d / (double)speed;
+            hh = Math.ceil(hh);
+            hh += d / (double)mid;
         }
-        return (h <= hour);
+        return (hh <= hour);
     }
+
+
 }
 /**
-    - hour reach office
-    - n train dist[i] = ith train distance
-        - each hour take minimum 1h
-    -   ret minim positive int speed | -1 if not
-    - max speed = 10pow7
-    - v = d / t => t = d / v
-        - v >  => t <
-        - t > => v <
-        -  t <= hour grow time => down v => right = mid - 1
+    - hour  double
+    - n train
+    - dist[] dist[i] km of ith train
+    - minimum for 1 train 1h
+    - ret minimum positive int speed  to reach office | -1
+    - ans max 10^7
+    - v = d /t => t = d / v
+    - l = 1
+    - right = 10^7
+    - if (dist.length > Ceil(hour))
+        return (-1)
+    - while (left <= right) {
+        if (check(mid))
+            right = mid - 1;
+        else 
+            left = mid + 1;
+    return (left)
+    }
+
+    check(mid, dist) {
+         hh = 0
+         for (d:dist)
+            hh = (int)hh % 1 > 0 ? (hh / 1) + 1 : hh 
+            hh += d / (double)mid
+        return (hh <= h)
+    }
  */
