43bef0ecc7953b2bc8201d8529275b4ab5adc5ba
lyn <46067184+lynyam@users.noreply.github.com>
2024-09-19 05:33:49 +0200

    Time: 39 ms (69.27%), Space: 43.9 MB (79.04%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,6 +1,31 @@
 class Solution {
-    HashMap<Integer, Integer> memo;
     public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int i = 1;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        int res = 1;
+
+        while (i < n) {
+            int j = 0;
+            int ans = 1;
+            while (j < i) {
+                if (nums[j] < nums[i]) {
+                    ans = Math.max(ans, dp[j] + 1);
+                }
+                j++;
+            }
+            dp[i] = ans;
+            res = Math.max(res, ans);
+            i++;
+        }
+        return (res);
+    }
+}
+
+/**class Solution {
+    HashMap<Integer, Integer> memo;
+    public int lengthOfLIS(int[] nums) {//0(n2)t 0(n)
         memo = new HashMap<>();
         int i = 0;
         int n = nums.length;
@@ -27,7 +52,7 @@ class Solution {
         memo.put(i, ans);
         return (memo.get(i));
     }
-}
+}*/
 /**
     - nums int[]
     - ret length of LIS
