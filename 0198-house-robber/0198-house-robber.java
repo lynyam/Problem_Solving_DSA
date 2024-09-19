dff10adf81edaab501e04788efb20db569487f79
lyn <46067184+lynyam@users.noreply.github.com>
2024-09-19 04:10:08 +0200

    Time: 0 ms (100%), Space: 40.9 MB (62.82%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,4 +1,30 @@
 class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int backOne = 0;
+        int backTwo = 0;
+        int i = 0;
+
+        while (i < n) {
+            if (i == 0)
+                backOne = nums[0];
+            if (i == 1) {
+                backTwo = nums[0];
+                backOne = Math.max(nums[0], nums[1]); 
+            }
+            else {
+                int temp = backOne;
+                backOne = Math.max(backOne, backTwo + nums[i]);
+                backTwo = temp;
+            }
+            i++;
+        }
+        return (backOne);
+    }
+}
+
+
+/**class Solution {
     HashMap<Integer, Integer> memo;
     public int rob(int[] nums) {
         memo = new HashMap<>();
@@ -15,7 +41,7 @@ class Solution {
         memo.put(i, Math.max(dp(i - 1, nums), dp(i - 2, nums) + nums[i]));
         return (memo.get(i));
     }
-}
+}*/
 /**
     - rob 2 adj house call possible
     - nums[] amount of money of each house.
