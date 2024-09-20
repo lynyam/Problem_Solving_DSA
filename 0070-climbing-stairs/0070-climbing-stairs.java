61a4f70fbc456c34c80a649cf37fa8249cce72e3
lyn <46067184+lynyam@users.noreply.github.com>
2024-09-20 03:00:33 +0200

    Time: 0 ms (100%), Space: 40.7 MB (6.21%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,21 +1,39 @@
 class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        int i = 1;
+        while (i <= n) {
+            if (i == 1)
+                dp[i] = 1;
+            else if (i == 2)
+                dp[i] = 2;
+            else {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+            i++;
+        }
+        return (dp[n]);
+    }
+}
+/**
+class Solution {
     HashMap<Integer, Integer> memo;
     public int climbStairs(int n) {
         memo = new HashMap<>();
-        return (dp(n));
+        return (dp(0, n));
     }
 
-    public  int dp(int i) {
-        if (i == 1)
-            return 0;
-        if (i == 2)
-            return 2;
+    public  int dp(int i, int n) {
+        if (i > n)
+            return (0);
+        if (i == n)
+            return 1;
         if (memo.containsKey(i))
             return (memo.get(i));
-        memo.put(i, dp(i - 1) + dp(i - 2) + 1);
+        memo.put(i, dp(i + 1, n) + dp(i + 2, n));
         return (memo.get(i));
     }
-}
+}*/
 /**
     - Climbing staircase, take n steps to reach top
     - can ether 1 or 2 steps
@@ -30,6 +48,8 @@ class Solution {
         4 => 1 + 1 + 1 + 1| 1 + 2 + 1 | 2 + 1 + 1 | 1 + 1 + 2 => 4
         5 => 1 + 1 + 1 + 1 | 1 + 2 + 1 + 1 | 1 + 2 + 2 | 1 + 1 + 2 + 1 |  1 + 1 + 1 + 2 | 2 + 1 + 1 + 1 | 2 + 2 + 1
         2 + 1 + 2 => 4 Fn(4) + Fn(3) +  1
+         3 case
+         F(0) = F(1) + F(2)
 
 
  */
