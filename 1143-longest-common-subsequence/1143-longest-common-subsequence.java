d5ac50379e4382a1f21b5e7a0ae90d2bf4980cb8
lyn <46067184+lynyam@users.noreply.github.com>
2024-09-25 09:16:22 +0200

    Time: 20 ms (76.28%), Space: 50.9 MB (46.86%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,20 +1,17 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int i;
-        int j;
         int n1 = text1.length();
         int n2 = text2.length();
         int[][] dp = new int[n1 + 1][n2 + 1];
-        i = n1 - 1;
-        j = n2 - 1;
+        int i = n1 - 1;
+        int j = n2 - 1;
 
         while (i >= 0) {
             j = n2 - 1;
             while (j >= 0) {
                 if (text1.charAt(i) == text2.charAt(j)) {
-                    dp[i][j] = 1 + dp[i + 1][j + 1];
-                }
-                else {
+                    dp[i][j] = dp[i + 1][j + 1] + 1;
+                } else {
                     dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
                 }
                 j--;
@@ -24,3 +21,12 @@ class Solution {
         return (dp[0][0]);
     }
 }
+/**
+    - t1, t2, ret length of LCS or 0
+    
+    Solution
+    - return length LCS Fn(State) state i, j index of t1, t2
+    - recursive formula : Fn(i, j) = t1[i] == t2[j] ? {Fn(i + 1, j + 1) + 1} : max(Fn(i+1, j), Fn(i, j+1))
+    - Base case: i == t1.length || j == t2.length return 0
+    - int[][] dp = new int[n1][n2]
+ */
