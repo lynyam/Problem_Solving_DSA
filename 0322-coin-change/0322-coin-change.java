afdef3f090159e538a149a5b0fbb327beede10f0
lyn <46067184+lynyam@users.noreply.github.com>
2024-09-25 08:41:46 +0200

    Time: 15 ms (79.13%), Space: 44.3 MB (59.24%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,57 +1,37 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
+        int i = 0;
+        int n = coins.length;
         Arrays.fill(dp, amount + 1);
-        int i = 1;
         dp[0] = 0;
+
         while (i <= amount) {
-            for (int coin : coins) {
-                int rest = i - coin;
+            int j = 0;
+            while (j < n) {
+                int rest = i - coins[j];
                 if (rest >= 0) {
                     dp[i] = Math.min(dp[i], dp[rest] + 1);
                 }
+                j++;
             }
             i++;
         }
-        return (dp[amount] > amount ? -1 : dp[amount]);
+        return (dp[amount] >= amount + 1 ? -1 : dp[amount]);
+
     }
 }
-
-/**class Solution {
-    //HashMap<Integer, Integer> memo;
-    int[] memo;
-    public int coinChange(int[] coins, int amount) {
-        //memo = new HashMap<>();
-        memo = new int[amount + 1];
-        int res = dp(coins, amount);
-        return (res == Integer.MAX_VALUE ? -1 : res);
-    }
-
-    public int dp(int[] coins, int amount) {//11/6/1
-        if (amount == 0)
-            return (0);
-        if (memo[amount] > 0)//
-            return (memo[amount]);
-        int min = Integer.MAX_VALUE;
-        for (int coin : coins) {
-            int rest = amount - coin;
-            if (rest >= 0) {
-                min = Math.min(min, dp(coins, rest));//6/1/
-            }
-        }
-        memo[amount] = min == Integer.MAX_VALUE ? Integer.MAX_VALUE : min + 1;//{1:I}
-        return (memo[amount]);//-1
-    }
-}
-
-    - coins[]
-    - integer amount total money
-    - fewest number of coins u need to make up that amount.
-    - if any comb not -1
-    - infinite number of each kind of coin
+/**
+    - coins[] #coins denominations
+    - amount --> total amount
+    - ret fewest numb of coins u can make or -1
+    - infinite number of kind of coins
 
     Solution
-    - return feewest number of coins Fn(State) i, index coins and rest amount
-    - recursive formula: Fn(m) = Min(F(m - Ci)) + 1/ i [0, coins.length - 1] 
-    - base case : F(0) = 0
+    - In this problem we need to return a fewest numb of coins and we not garantie that solution exist
+    - So every decision of coins choice u make have an impact on future choice.
+    - I use DP to resolve that point
+        - return value is fewest numb of coins for Fn(State) state is i : 0 to amount 
+        - recursive formula : Fn(i) = Min(for(Fn(i - ci)))) + 1
+        - base case: i <= 0 return 0
  */
