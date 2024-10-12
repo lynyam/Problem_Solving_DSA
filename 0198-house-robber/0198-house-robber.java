5f6c47e8ead3ceb71f983f5c6ef3a009274ddd97
lyn <46067184+lynyam@users.noreply.github.com>
2024-10-12 22:13:26 +0200

    Time: 0 ms (100%), Space: 41 MB (35.94%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,35 +1,35 @@
 class Solution {
+    //int[] memo;
     public int rob(int[] nums) {
-        int backOne = nums[0];
         int n = nums.length;
-        if (n > 1)
-            backOne = Math.max(nums[0], nums[1]);
-        int backTwo = nums[0];
-
-        int i = 2;
+        int one, two;
+        one = two = 0;
+        if (n == 0) return (0);
+        int i = 0;
         while (i < n) {
-            int temp = backOne;
-            backOne = Math.max(backOne, backTwo + nums[i]);
-            backTwo = temp;
+            if (i == 0)
+                one = two = nums[0];
+            else if (i == 1)
+                one = Math.max(nums[0], nums[1]);
+            else {
+                int temp = one;
+                one = Math.max(nums[i] + two, one);
+                two = temp;
+            }
             i++;
         }
-        return (backOne);
+        return (one);
+        /*
+        memo = new int[n];
+        Arrays.fill(memo, -1);
+        return (dp(n - 1, nums));*/
     }
+
+    /*public int dp(int i, int[] nums) {
+        if (i == 0) return nums[0];
+        if (i == 1) return (Math.max(nums[0], nums[1]));
+        if (memo[i] != -1) return (memo[i]);
+        memo[i] = Math.max(dp(i - 1, nums), dp(i - 2, nums) + nums[i]);
+        return (memo[i]);
+    }*/
 }
-/**
-    - constaint: broken 2 adj house into same nigth
-    - nums = amount of each house
-    - ret max amount of money you can rob tonight
-
-    Soolution
-    - decision to rob each house make impact on hour next choice, you need max, I think we can use DP
-    - return value is max amount on fonction Fn(state) with state is i index of home u rob
-    - think about recuresion formula: Fn(i) = Max(F(i - 1), F(i - 2) + nums(i))
-    - base case: Fn(1) = nums[0] F(2) = max(nums[1], nums[2]) 
-    1
-    2
-    2//4 =>4
-    4
-
-
- */
