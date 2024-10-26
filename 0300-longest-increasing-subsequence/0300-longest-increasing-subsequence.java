26ad40bf603cac48c0c573063335204963a18455
lyn <46067184+lynyam@users.noreply.github.com>
2024-10-26 10:20:09 +0200

    Time: 39 ms (66.64%), Space: 44.3 MB (39.12%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,63 +1,23 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int i = 1;
+        int i = n - 1;
         int[] dp = new int[n];
-        Arrays.fill(dp, 1);
-        int res = 1;
+        int res = 0;
+        //dp[n - 1] = 1;
 
-        while (i < n) {
-            int j = 0;
+        while (i >= 0) {
             int ans = 1;
-            while (j < i) {
-                if (nums[j] < nums[i]) {
-                    ans = Math.max(ans, dp[j] + 1);
-                }
+            int j = i + 1;
+            while (j < n) {
+                if (nums[i] < nums[j])
+                    ans = Math.max(ans, 1 + dp[j]);
                 j++;
             }
             dp[i] = ans;
-            res = Math.max(res, ans);
-            i++;
+            res = Math.max(res, dp[i]);
+            i--;
         }
         return (res);
     }
 }
-
-/**class Solution {
-    HashMap<Integer, Integer> memo;
-    public int lengthOfLIS(int[] nums) {//0(n2)t 0(n)
-        memo = new HashMap<>();
-        int i = 0;
-        int n = nums.length;
-        int ans = 1;
-
-        while (i < n) {
-            ans = Math.max(ans, dp(i, nums));
-            i++;
-        }
-        return (ans);
-    }
-
-    public int dp(int i, int[] nums) {
-        if (memo.containsKey(i))
-            return (memo.get(i));
-        int ans = 1;
-        int j = 0;
-        while (j < i) {
-            if (nums[j] < nums[i]) {
-                ans = Math.max(ans, dp(j, nums) + 1);
-            }
-            j++;
-        }
-        memo.put(i, ans);
-        return (memo.get(i));
-    }
-}*/
-/**
-    - nums int[]
-    - ret length of LIS
-    Solution
-    - return len of LIS Fn(State) index i
-    - recurence formula: Fn(i) = Max(F(j) + 1) / j in [0 i[ and nums[j] < nums[i]
-    - base case: i = 0 => 1
- */
