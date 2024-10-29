2f384f6318ba811397b800ef702ecc5aea593fda
lyn <46067184+lynyam@users.noreply.github.com>
2024-10-29 17:39:31 +0100

    Time: 19 ms (51.23%), Space: 94.3 MB (73.44%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -2,30 +2,20 @@ class Solution {
     List<List<Integer>> ans;
     public List<List<Integer>> combine(int n, int k) {
         ans = new ArrayList<>();
-        backtrack(new ArrayList<Integer>(), 1, n, k);
+        backtrack(new ArrayList<>(), 1, n, k);
         return (ans);
     }
 
     public void backtrack(List<Integer> curr, int i, int n, int k) {
         if (curr.size() == k) {
-            ans.add(new ArrayList<Integer>(curr));
+            ans.add(new ArrayList<>(curr));
             return ;
         }
-        int j = i;
-        while (j <= n) {
-            curr.add(j);
-            backtrack(curr, j + 1, n, k);
+        while (i <= n) {
+            curr.add(i);
+            backtrack(curr, i + 1, n, k);
             curr.remove(curr.size() - 1);
-            j++;
+            i++;
         }
     }
 }
-/**
-    - n, k int
-    - return all possible comb of k numbers [1, n] (k/n)
-    SOlution
-                            []
-                    [1]     [2]     [3]     [4]
-                [2] [3] [4]
-
- */
