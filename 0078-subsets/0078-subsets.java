0a3cef6d83c83f5997606aa8483d1b1becc9d8d0
lyn <46067184+lynyam@users.noreply.github.com>
2024-10-29 16:34:00 +0100

    Time: 0 ms (100%), Space: 42.7 MB (50.83%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,29 +1,18 @@
 class Solution {
     List<List<Integer>> ans;
     public List<List<Integer>> subsets(int[] nums) {
-        ans = new ArrayList<>();
+        ans = new ArrayList();
         backtrack(new ArrayList<>(), 0, nums);
         return (ans);
     }
 
     public void backtrack(List<Integer> curr, int i, int[] nums) {
         ans.add(new ArrayList<>(curr));
-        int j = i;
-        while (j < nums.length) {
-            curr.add(nums[j]);
-            backtrack(curr, j + 1, nums);
+        while (i < nums.length) {
+            curr.add(nums[i]);
+            backtrack(curr, i + 1, nums);
             curr.remove(curr.size() - 1);
-            j++;
+            i++;
         }
     }
 }
-/**
-    - nums int[] unique elt
-    - ret all possible subset
-    - not contains duplicate subset, any order
-
-                                                []
-                        [1]                     [2]                     [3]
-                    [1 2] [1 3]         [2 3]
-            [1 2 3]
- */
