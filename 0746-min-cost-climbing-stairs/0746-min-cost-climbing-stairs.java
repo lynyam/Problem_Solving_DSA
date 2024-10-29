0699688e88f411701031859129fd79b14fae65e3
lyn <46067184+lynyam@users.noreply.github.com>
2024-10-29 07:53:22 +0100

    Time: 0 ms (100%), Space: 42.9 MB (76.1%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,36 +1,16 @@
 class Solution {
-    //int[] memo;
     public int minCostClimbingStairs(int[] cost) {
+        int one = 0;
+        int two = 0;
         int n = cost.length;
-        if (n == 0) return (0);
-        //memo = new int[n + 1];
-        //Arrays.fill(memo, -1);
-        int i = 0;
-        int one;
-        int two;
-        one = two = 0;
+        int i = 2;
+
         while (i <= n) {
-            if (i < 2) {
-                one = two = 0;
-                i++;
-                continue ;
-            }
             int temp = one;
             one = Math.min(one + cost[i - 1], two + cost[i - 2]);
             two = temp;
-            //memo[i] = Math.min(memo[i - 1] + cost[i - 1], memo[i - 2] + cost[i - 2]);
-            i++; 
+            i++;
         }
         return (one);
     }
-
-    /*public int dp(int i, int[] cost) {
-        if (i < 2)
-            return (0);
-        if (memo[i] != -1)
-            return (memo[i]);
-        memo[i] = Math.min(dp(i - 1, cost) + cost[i - 1], dp(i - 2, cost) + cost[i - 2]);
-        System.out.println(memo[i]);
-        return (memo[i]);
-    }*/
 }
