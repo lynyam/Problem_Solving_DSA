f4acc9ce2adb80c8246cfe82ed8dab0182ee3f37
lyn <46067184+lynyam@users.noreply.github.com>
2024-10-31 23:27:52 +0100

    Time: 0 ms (100%), Space: 42.1 MB (40.61%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,41 +1,24 @@
 class Solution {
-    String[] map = new String[]{
-        "", "", "abc", "def", "ghi", "jkl",
-        "mno", "pqrs", "tuv", "wxyz"
-    };
+    String[] map = new String[] {"", "", "abc", "def", "ghi",
+                                    "jkl", "mno", "pqrs", "tuv", "wxyz"};
     List<String> ans;
     public List<String> letterCombinations(String digits) {
-        if (digits.length() == 0)
+        if (digits.isEmpty())
             return (new ArrayList<>());
         ans = new ArrayList<>();
-        backtrack(new StringBuilder(), digits, 0);
+        backtrack(new StringBuilder(), 0, digits);
         return (ans);
     }
 
-    public void backtrack(StringBuilder curr, String digits, int i) {
-        if (i == digits.length()) {
+    public void backtrack(StringBuilder curr, int i, String digits) {
+        if (i >= digits.length()) {
             ans.add(new String(curr));
             return ;
         }
-        String str = map[digits.charAt(i) - '0'];
-        for (char c : str.toCharArray()) {
+        for (char c : map[digits.charAt(i) - '0'].toCharArray()) {
             curr.append(c);
-            backtrack(curr, digits, i + 1);
+            backtrack(curr, i + 1, digits);
             curr.deleteCharAt(curr.length() - 1);
         }
     }
 }
-/**
-    - string digit 2-9
-    - ret all possible comb that the number could represent
-    
-        "23"
-                                []
-        2           [a]         [b]         [c]
-        3        [d] [e] [f]  [d][e][f]   [d][e][f]
-        => leaf => result
-            base case (curr.length == digits.length)
-            str = map[i]
-            while (c : str)
-                bcktr(curr, i + 1, ..)
- */
