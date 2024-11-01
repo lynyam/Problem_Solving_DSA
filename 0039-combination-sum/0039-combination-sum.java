da2191701bde07c5cb8c33754272f875300b1d53
lyn <46067184+lynyam@users.noreply.github.com>
2024-11-01 07:02:31 +0100

    Time: 1 ms (99.97%), Space: 44.7 MB (35.46%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -2,38 +2,22 @@ class Solution {
     List<List<Integer>> ans;
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         ans = new ArrayList<>();
-        backtrack(new ArrayList<Integer>(), 0, 0, candidates, target);
+        backtrack(new ArrayList<>(), 0, 0, candidates, target);
         return (ans);
     }
 
-    public void backtrack(List<Integer> curr, int sum, int i, int[] candidates, int target) {
+    public void backtrack(List<Integer> curr, int i, int sum, int[] candidates, int target) {
         if (sum == target) {
-            ans.add(new ArrayList<Integer>(curr));
+            ans.add(new ArrayList<>(curr));
             return ;
         }
-        int j = i;
-        while (j < candidates.length) {
-            int cand = candidates[j];
-            if (sum + cand <= target) {
-                curr.add(cand);
-                backtrack(curr, sum + cand, j, candidates, target);
+        while (i < candidates.length) {
+            if (sum + candidates[i] <= target) {
+                curr.add(candidates[i]);
+                backtrack(curr, i, sum + candidates[i], candidates, target);
                 curr.remove(curr.size() - 1);
             }
-            j++;
+            i++;
         }
-
     }
 }
-/**
-    - candidates[] distinct, target int
-    - list of all unique comb of candidates / sum = target
-    - ret comb in any order
-    - choose candidates an unlimited number of times. two com uniq if freq diff
-
-        [2 3 6 7]
-                                                        []
-        oldSum + cd = sum <= 7        [2]           [3]         [6]         [7]
-                                [2]         [3] [6] [7]x
-                        [2] [3]v[6]x[7]x    [3]
-                x[2][3]x[]
- */
