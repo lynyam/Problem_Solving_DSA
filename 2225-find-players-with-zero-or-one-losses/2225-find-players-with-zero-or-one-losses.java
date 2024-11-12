4317fde8baa1bf45fede5be1ef4058bc2009d03c
lyn <46067184+lynyam@users.noreply.github.com>
2024-11-12 09:14:09 +0100

    Time: 79 ms (73.96%), Space: 111.2 MB (12.06%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -1,35 +1,26 @@
 class Solution {
     public List<List<Integer>> findWinners(int[][] matches) {
         HashMap<Integer, Integer> store = new HashMap<>();
-        
+
         for (int[] match : matches) {
-            int wi = match[0];
-            int li = match[1];
-            store.putIfAbsent(wi, 0);
-            store.put(li, store.getOrDefault(li, 0) + 1);
+            int win = match[0];
+            int los = match[1];
+            store.putIfAbsent(win, 0);
+            store.put(los, store.getOrDefault(los, 0) + 1);
         }
-        List<Integer> winner = new ArrayList<>();
-        List<Integer> looser = new ArrayList<>();
-        for (int key : store.keySet()) {
-            if (store.get(key).equals(0))
-                winner.add(key);
-            else if (store.get(key).equals(1))
-                looser.add(key);
-        }
-        Collections.sort(winner);
-        Collections.sort(looser);
         List<List<Integer>> ans = new ArrayList<>();
-        ans.add(winner);
-        ans.add(looser);
+        List<Integer> allTWin = new ArrayList<>();
+        List<Integer> oneTWin = new ArrayList<>();
+        for (int key : store.keySet()) {
+            if (store.get(key) == 0)
+                allTWin.add(key);
+            if (store.get(key) == 1)
+                oneTWin.add(key);
+        }
+        Collections.sort(allTWin);
+        Collections.sort(oneTWin);
+        ans.add(allTWin);
+        ans.add(oneTWin);
         return (ans);
     }
 }
-/**
-    - matches[i] = [wi, li]
-        - wi defeat li
-    - ret list answer size 2:
-        - answer[0] = list player that have not lost any matches
-        - answer[i] = lost exactly one match
-        - ret increasing order
-    - player have played at leat one match
- */
