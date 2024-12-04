44615d4de9080aebad5c488d06809834faa90a33
lyn <46067184+lynyam@users.noreply.github.com>
2024-12-04 12:27:03 +0100

    Time: 21 ms (56.46%), Space: 51 MB (35.22%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -13,29 +13,45 @@ class Solution {
         len2 = text2.length();
         dp = new int[len1 + 1][len2 + 1];
         int i = 0;
-        while (i < len1) {
+        /*while (i < len1) {
             Arrays.fill(dp[i], -1);
             i++;
-        }
+        }*/
 
-        return(helper(0, 0));
+        //return(helper(0, 0));
+        return (lcs(len1 - 1, len2 - 1));
+    }
+
+    public int lcs(int n1, int len) {
+        while (n1 >= 0) {
+            int n2 = len;
+            while (n2 >= 0) {
+                if (text1.charAt(n1) == text2.charAt(n2))
+                    dp[n1][n2] = 1 + dp[n1 + 1][n2 + 1];
+                else {
+                    dp[n1][n2] = Math.max(dp[n1 + 1][n2], dp[n1][n2 + 1]);
+                }
+                n2--;
+            }
+            n1--;
+        }
+        return (dp[0][0]);
     }
 
     public int helper(int i, int j) {
+        if (i == len1 || j == len2)
+            return (0);
         if (dp[i][j] != -1) {
             return(dp[i][j]);
         }
-        int case1 = helper(i + 1, j);
-        int k = j;
-        int case2 = 0;
-        while (k < len2) {
-            if (text1.charAt(i) == text2.charAt(k)) {
-                case2 = 1 + helper(i + 1, k + 1);
-                break ;
-            }
-            k++;
+        int ans = 0;
+        if (text1.charAt(i) == text2.charAt(j)) {
+            ans = 1 + helper(i + 1, j + 1);
         }
-        dp[i][j] = Math.max(case1, case2);
+        else {
+            ans = Math.max(helper(i + 1, j), helper(i, j + 1));
+        }
+        dp[i][j] = ans;
         return (dp[i][j]);
     }
 }
