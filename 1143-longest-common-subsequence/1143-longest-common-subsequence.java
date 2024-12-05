105d5a12a9829930233503ab9b17752ff7208192
lyn <46067184+lynyam@users.noreply.github.com>
2024-12-05 10:24:34 +0100

    Time: 20 ms (77.68%), Space: 51.1 MB (17.73%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,57 +1,58 @@
 class Solution {
+    int[][] dp;
     String text1;
     String text2;
-    int len1;
-    int len2;
-    int ans;
-    int[][] dp;
     public int longestCommonSubsequence(String text1, String text2) {
-        ans = 0;
+        int n1 = text1.length();
+        int n2 = text2.length();
         this.text1 = text1;
         this.text2 = text2;
-        len1 = text1.length();
-        len2 = text2.length();
-        dp = new int[len1 + 1][len2 + 1];
+        dp = new int[n1 + 1][n2 + 1];
         int i = 0;
-        /*while (i < len1) {
-            Arrays.fill(dp[i], -1);
+
+        /*while (i < n1) {
+            int j = 0;
+            while (j < n2) {
+                dp[i][j] = -1;
+                j++;
+            }
             i++;
         }*/
-
-        //return(helper(0, 0));
-        return (lcs(len1 - 1, len2 - 1));
-    }
-
-    public int lcs(int n1, int len) {
-        while (n1 >= 0) {
-            int n2 = len;
-            while (n2 >= 0) {
-                if (text1.charAt(n1) == text2.charAt(n2))
-                    dp[n1][n2] = 1 + dp[n1 + 1][n2 + 1];
-                else {
-                    dp[n1][n2] = Math.max(dp[n1 + 1][n2], dp[n1][n2 + 1]);
-                }
-                n2--;
-            }
-            n1--;
-        }
-        return (dp[0][0]);
+        //return (helper(0, 0));
+        return (tabulation(n1 - 1, n2 - 1));
     }
 
     public int helper(int i, int j) {
-        if (i == len1 || j == len2)
-            return (0);
+        
         if (dp[i][j] != -1) {
-            return(dp[i][j]);
+            return (dp[i][j]);
         }
         int ans = 0;
         if (text1.charAt(i) == text2.charAt(j)) {
             ans = 1 + helper(i + 1, j + 1);
-        }
-        else {
+        } else {
             ans = Math.max(helper(i + 1, j), helper(i, j + 1));
         }
         dp[i][j] = ans;
         return (dp[i][j]);
     }
+
+    public int tabulation(int i, int j) {
+        while (i >= 0) {
+            int n2 = j;
+            while (n2 >= 0) {
+                if (text1.charAt(i) == text2.charAt(n2)) {
+                    dp[i][n2] = 1 + dp[i + 1][n2 + 1];
+                }
+                else {
+                    dp[i][n2] = Math.max(dp[i + 1][n2], dp[i][n2 + 1]);
+                }
+                n2--;
+            }
+            i--;
+        }
+        return (dp[0][0]);
+    }
+
+
 }
