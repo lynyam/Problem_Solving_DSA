20790b150f0f46c508744955e5dd174961f49ef1
lyn <46067184+lynyam@users.noreply.github.com>
2024-12-12 06:54:57 +0100

    Time: 1 ms (82.68%), Space: 41.8 MB (52.27%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -5,28 +5,46 @@ class Solution {
     public int maxProfit(int[] prices) {
         this.prices = prices;
         this.n = prices.length;
-        dp = new int[n][2];
-        int i = 0;
+        dp = new int[n + 2][2];
+        /*int i = 0;
 
         while(i < n) {
             Arrays.fill(dp[i++], -1);
-        }
-        return(helper(0, 0));
+        }*/
+        return(helper(n - 1));
     }
 
-    public int helper(int i, int status) {
+    public int helper2(int i, int status) {
         if (i >= n)
             return (0);
         if (dp[i][status] != -1) {
             return (dp[i][status]);
         }
-        int max = helper(i + 1, status);
+        int max = helper2(i + 1, status);
         if (status == 0) {
-            max = Math.max(max, -prices[i] + helper(i + 1, 1));
+            max = Math.max(max, -prices[i] + helper2(i + 1, 1));
         } else {
-            max = Math.max(max, prices[i] + helper(i + 2, 0));
+            max = Math.max(max, prices[i] + helper2(i + 2, 0));
         }
         dp[i][status] = max;
         return (max);
+    }
+
+    public int helper(int i) {
+        while (i >= 0) {
+            int status = 0;
+            while (status < 2) {
+                int max = dp[i + 1][status];
+                if (status == 0) {
+                    max = Math.max(max, -prices[i] + dp[i + 1][1]);
+                } else {
+                    max = Math.max(max, prices[i] + dp[i + 2][0]);
+                }
+                dp[i][status] = max;
+                status++;
+            }
+            i--;
+        }
+        return (dp[0][0]);
     }
 }
