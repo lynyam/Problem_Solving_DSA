9cb053369564e0e869540810cb63f47a8cc66811
lyn <46067184+lynyam@users.noreply.github.com>
2024-12-12 07:16:28 +0100

    Time: 17 ms (50.69%), Space: 55.3 MB (51.11%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,52 +1,28 @@
 class Solution {
-    int[] prices;
     int n;
-    int fee;
+    int[] prices;
     int[][] dp;
+    int fee;
     public int maxProfit(int[] prices, int fee) {
-        this.n = prices.length;
         this.prices = prices;
+        this.n = prices.length;
         this.fee = fee;
         dp = new int[n + 1][2];
-        int i = 0;
-
-        /*while (i < n) {
-            Arrays.fill(dp[i], -1);
-            i++;
-        }
-        return(helper(0, 0));*/
-        return(tabulation(n - 1));
+        return (helper(n - 1));
     }
 
-    public int helper(int i, int status) {
-        if (i == n)
-            return (0);
-        if (dp[i][status] != -1)
-            return (dp[i][status]);
-        int max = 0;
-        max = helper(i + 1, status);
-        if (status == 0) {
-            max = Math.max(max, -prices[i] + helper(i + 1, 1));
-        } else {
-            max = Math.max(max, prices[i] - fee + helper(i + 1, 0));
-        }
-        dp[i][status] = max;
-        return (max);
-    }
-
-    public int tabulation(int i) {
+    public int helper(int i) {
         while (i >= 0) {
-            int status = 0;
-            while (status < 2) {
-                int max = 0;
-                max = dp[i + 1][status];
-                if (status == 0) {
+            int j = 0;
+            while (j < 2) {
+                int max = dp[i + 1][j];
+                if (j == 0) {
                     max = Math.max(max, -prices[i] + dp[i + 1][1]);
                 } else {
                     max = Math.max(max, prices[i] - fee + dp[i + 1][0]);
                 }
-                dp[i][status] = max;
-                status++;
+                dp[i][j] = max;
+                j++;
             }
             i--;
         }
