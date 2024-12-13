e11879bda8f64480e488f180bc80bad6183741fd
lyn <46067184+lynyam@users.noreply.github.com>
2024-12-13 06:52:41 +0100

    Time: 0 ms (100%), Space: 40.3 MB (55.97%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -4,14 +4,16 @@ class Solution {
     public int uniquePaths(int m, int n) {
         this.m = m;
         this.n = n;
-        dp = new int[m][n];
+        dp = new int[m + 1][n + 1];
+        /*dp = new int[m][n];
         int i = 0;
-        
+
         while (i < m) {
             Arrays.fill(dp[i], -1);
             i++;
         }
-        return(helper(0, 0));
+        return(helper(0, 0));*/
+        return (tabulation(m - 1, n - 1));
     }
 
     public int helper(int i, int j) {
@@ -24,5 +26,20 @@ class Solution {
         int ans = helper(i + 1, j) + helper(i, j + 1);
         dp[i][j] = ans;
         return (ans);
+    }
+
+    public int tabulation(int i, int k) {
+        while (i >= 0) {
+            int j = k;
+            while (j >= 0) {
+                if (i == m - 1 && j == n - 1)
+                    dp[i][j] = 1;
+                else
+                    dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
+                j--;
+            }
+            i--;
+        }
+        return (dp[0][0]);
     }
 }
