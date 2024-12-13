41e389314bff46618eeaeac1405427f2b3b58dc6
lyn <46067184+lynyam@users.noreply.github.com>
2024-12-13 07:57:18 +0100

    Time: 2 ms (97.51%), Space: 41.6 MB (74.45%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,17 +1,18 @@
 class Solution {
-    public boolean is_match(char c1, char c2) {
-        return (c1 == '(' ? c2 == ')' : c1 == '{' ? c2 == '}' :
-                c1 == '[' ? c2 == ']' : false);
-    }
-    public boolean isValid(String str) {
-        List<Character> stack = new ArrayList<>();
-        for (char c : str.toCharArray()) {
-            if (stack.size() > 0 && is_match(stack.get(stack.size() - 1), c)) {
-                stack.remove(stack.size() - 1);
-                continue ;
-            }
-                stack.add(c); 
+    public boolean isValid(String s) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (char c : s.toCharArray()) {
+            if (!sb.isEmpty() && isMatch(sb.charAt(sb.length() - 1), c)) {
+                sb.deleteCharAt(sb.length() - 1);
+            } else 
+                sb.append(c);
         }
-        return (stack.size() == 0);
+        return (sb.length() == 0);
+    }
+
+    public boolean isMatch(char c1, char c2) {
+        return ((c1 == '(' && c2 == ')') || (c1 == '{' && c2 == '}') || 
+            (c1 == '[' && c2 == ']'));
     }
 }
