1dcb289a4841c35dc7578a54fdb535b074f6bade
lyn <46067184+lynyam@users.noreply.github.com>
2024-12-13 09:45:31 +0100

    Time: 2 ms (81.19%), Space: 42.5 MB (32.11%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,15 +1,14 @@
 class Solution {
     public String makeGood(String s) {
-        int n = s.length();
-        StringBuilder stack = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
 
         for (char c : s.toCharArray()) {
-            if (!stack.isEmpty() && Math.abs(c - stack.charAt(stack.length() - 1)) == 32) {
-                stack.deleteCharAt(stack.length() - 1);
-                continue ;
+            if (!sb.isEmpty() && Math.abs(c - sb.charAt(sb.length() - 1)) == 32) {
+                sb.deleteCharAt(sb.length() - 1);
+            } else {
+                sb.append(c);
             }
-            stack.append(c);
         }
-        return (stack.toString());
+        return (sb.toString());
     }
 }
