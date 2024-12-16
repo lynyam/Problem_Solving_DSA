e04c0d533d629cbbc062392827116a0967d1fd04
lyn <46067184+lynyam@users.noreply.github.com>
2024-12-16 08:08:04 +0100

    Time: 1 ms (82.74%), Space: 41.8 MB (42.1%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -3,26 +3,28 @@ class Solution {
     int n;
     int[][] dp;
     public int maxProfit(int[] prices) {
+        n = prices.length;
         this.prices = prices;
-        this.n = prices.length;
-        dp = new int[n + 2][2];
-        return (helper(n - 1));
+        dp = new int[n][2];
+        int i = 0;
+        while (i < n) {
+            Arrays.fill(dp[i++], -1);
+        }
+        return (helper(0, 0));
     }
 
-    public int helper(int i) {
-        while (i >= 0) {
-            int j = 0;
-            while (j < 2) {
-                int max = dp[i + 1][j];
-                if (j == 0)
-                    max = Math.max(max, -prices[i] + dp[i + 1][1]);
-                else
-                    max = Math.max(max, prices[i] + dp[i + 2][0]);
-                dp[i][j] = max;
-                j++;
-            }
-            i--;
+    public int helper(int i, int status) {
+        if (i >= n)
+            return (0);
+        if (dp[i][status] != -1)
+            return (dp[i][status]);
+        int max = helper(i + 1, status);
+        if (status == 0) {
+            max = Math.max(max, -prices[i] + helper(i + 1, 1));
+        } else {
+            max = Math.max(max, prices[i] + helper(i + 2, 0));
         }
-        return (dp[0][0]);
+        dp[i][status] = max;
+        return (dp[i][status]);
     }
 }
