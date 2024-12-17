d5a12a0181a82cde4c5011d761bdea573e561a70
lyn <46067184+lynyam@users.noreply.github.com>
2024-12-17 11:22:21 +0100

    Time: 24 ms (85.96%), Space: 56.2 MB (94.91%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,16 +1,16 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        int i = 0;
-        int n = temperatures.length;
         List<Integer> stack = new ArrayList<>();
+        int n = temperatures.length;
         int[] answer = new int[n];
+        int i = 0;
 
         while (i < n) {
-            int t = temperatures[i];
-            int days = 0;
-            while (stack.size() > 0 && temperatures[stack.get(stack.size() - 1)] < t) {
-                int curr = stack.remove(stack.size() - 1);
-                answer[curr] = i - curr;
+            while (stack.size() != 0 && temperatures[stack.get(stack.size() - 1)] < 
+                temperatures[i]) {
+                    int index = stack.get(stack.size() - 1);
+                    stack.remove(stack.size() - 1);
+                    answer[index] = i - index;
             }
             stack.add(i);
             i++;
