d9c560a23e8271ab79aac27a0bbbadcf51e1a27e
lyn <46067184+lynyam@users.noreply.github.com>
2024-12-18 09:50:54 +0100

    Time: 31 ms (63.74%), Space: 65.6 MB (5.51%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,33 +1,30 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int n = nums.length;
         int i = 0;
-        Deque<Integer> queue = new LinkedList<>();
-        int[] ans = new int[n - k + 1];
         int j = 0;
-        int l = 0;
-        
-        while (i < k) {
-            while (!queue.isEmpty() && queue.peekLast() < nums[i]) {
-                queue.removeLast();
-            }
-            queue.addLast(nums[i]);
-            i++;
-        }
-        ans[j] = queue.peekFirst();
-        j++;
+        int n = nums.length;
+        int[] answer = new int[n - k + 1];
+        Deque<Integer> queue = new LinkedList<>();
+
         while (i < n) {
-            while (!queue.isEmpty() && queue.peekLast() < nums[i]) {
+            while (!queue.isEmpty() && nums[queue.peekLast()] < nums[i]) {
                 queue.removeLast();
             }
-            queue.addLast(nums[i]);
-            if (nums[l] == queue.peekFirst())
-                queue.removeFirst();
-            ans[j] = queue.peekFirst();
-            l++;
-            j++;
+            queue.addLast(i);
+            if (i >= k - 1) {
+                answer[i - k + 1] = nums[queue.peekFirst()];
+                if (queue.peekFirst() == j)
+                    queue.removeFirst();
+                j++;
+            }
             i++;
         }
-        return (ans);
+        return (answer);
     }
+    /*
+        3 2 -1 1 0
+        [0 1 2] i = 2 j = 0 -> max = n(0) = 3
+        pop j = 0  [1 2] -> [1 3] -> max = n(1) = 2
+        pop j = 1 [3] -> [3, 4]-> max = n(3) = 1
+    */
 }
