845b60051ddcb1309bb8c0d45d2b9df5d7206a47
lyn <46067184+lynyam@users.noreply.github.com>
2024-12-18 08:17:55 +0100

    Time: 50 ms (30.67%), Space: 231.3 MB (14.46%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.java
+++ 0808-soup-servings/0808-soup-servings.java
@@ -1,10 +1,8 @@
 class Solution {
     double[][] dp;
     public double soupServings(int n) {
-        if (n > 4800) {
-            return 1.0;
-        }
-        n = (int)Math.ceil(n / 25.0);
+        if (n > 4800)
+            return (1.0);
         dp = new double[n + 1][n + 1];
         int i = 0;
         while (i <= n) {
@@ -13,28 +11,18 @@ class Solution {
         return (helper(n, n));
     }
 
-    public double helper(int soupA, int soupB) {
-        if (soupA <= 0 && soupB <= 0)
+    public double helper(int i, int j) {
+        if (i <= 0 && j <= 0)
             return (0.5);
-        if (soupA <= 0)
-            return (1.0);
-        if (soupB <= 0)
-            return (0.0);
-        if (dp[soupA][soupB] != -1)
-            return (dp[soupA][soupB]);
-        double ope1 = helper(soupA - 4, soupB);
-        double ope2 = helper(soupA - 3, soupB - 1);
-        double ope3 = helper(soupA - 2, soupB - 2);
-        double ope4 = helper(soupA - 1, soupB - 3);
-        dp[soupA][soupB] = (ope1 + ope2 + ope3 + ope4) * 0.25;
-        return (dp[soupA][soupB]);
+        if (i <= 0)
+            return (1);
+        if (j <= 0)
+            return (0);
+        if (dp[i][j] != -1)
+            return (dp[i][j]);
+        double prob = 0.25 * (helper(i - 100, j) + helper(i - 75, j - 25) + 
+            helper(i - 50, j - 50) + helper(i - 25, j - 75));
+        dp[i][j] = prob;
+        return (dp[i][j]);
     }
 }
-/*
-                                100
-    0,100                     25,100                50,50                       75,25
-    |                     /  \    \      \         /\      \   \                / \ \       \
-    1               -75,100 -50,75 -25,50 0,25  0,50 -25,25 0,0 25,-25.   -25,25 0,0 25,-25 25,-50 
-                    |        |      |       |.   |.    |      |.  |.         |.   |.    |.    |
-                    1        1      1       1.   1     1      0.5   0        1.   0.5   0.    0
-*/
