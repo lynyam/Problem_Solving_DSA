ca29a008ea5d7aaee71c59214f7284f6ecbec9bf
lyn <46067184+lynyam@users.noreply.github.com>
2024-12-19 07:51:15 +0100

    Time: 29 ms (19.08%), Space: 50.8 MB (64.26%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,54 +1,34 @@
 class Solution {
-    int n1;
-    int n2;
     String text1;
     String text2;
     int[][] dp;
+    int n, m;
     public int longestCommonSubsequence(String text1, String text2) {
-        n1 = text1.length();
-        n2 = text2.length();
+        n = text1.length();
+        m = text2.length();
         this.text1 = text1;
         this.text2 = text2;
-        dp = new int[n1 + 1][n2 + 1];
+        dp = new int[n][m];
         int i = 0;
-        /*while (i < n1) {
-            Arrays.fill(dp[i], -1);
-            i++;
+
+        while (i < n) {
+            Arrays.fill(dp[i++], -1);
         }
-        return(helper(0, 0));*/
-        return (downUp(n1 - 1, n2 - 1));
+        return (helper(0, 0));
     }
 
     public int helper(int i, int j) {
-        if (i == n1 || j == n2)
+        if (i >= n || j >= m)
             return (0);
         if (dp[i][j] != -1)
             return (dp[i][j]);
-        int lcs = 0;
+        int answer = 0;
         if (text1.charAt(i) == text2.charAt(j)) {
-            lcs = 1 + helper(i + 1, j + 1);
+            answer = Math.max(answer, 1 + helper(i + 1, j + 1));
         } else {
-            lcs = Math.max(helper(i + 1, j), helper(i, j + 1));
+            answer = Math.max(answer, Math.max(helper(i + 1, j), helper(i, j + 1)));
         }
-        dp[i][j] = lcs;
-        return (lcs);
-    }
-
-    public int downUp(int i, int k) {
-        while (i >= 0) {
-            int j = k;
-            while (j >= 0) {
-                int lcs = 0;
-                if (text1.charAt(i) == text2.charAt(j)) {
-                    lcs = 1 + dp[i + 1][j + 1];
-                } else {
-                    lcs = Math.max(dp[i + 1][j], dp[i][j + 1]);
-                }
-                dp[i][j] = lcs;
-                j--;
-            }
-            i--;
-        }
-        return (dp[0][0]);
+        dp[i][j] = answer;
+        return (dp[i][j]);
     }
 }
