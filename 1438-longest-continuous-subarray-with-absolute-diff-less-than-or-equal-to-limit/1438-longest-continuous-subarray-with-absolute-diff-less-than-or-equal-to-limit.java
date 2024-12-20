ad7edea0850f96d7601884f7b1041929d5046120
lyn <46067184+lynyam@users.noreply.github.com>
2024-12-20 11:17:47 +0100

    Time: 33 ms (89.14%), Space: 58.4 MB (46.47%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -1,25 +1,27 @@
 class Solution {
     public int longestSubarray(int[] nums, int limit) {
-        int n = nums.length;
         int left = 0;
         int right = 0;
+        int n = nums.length;
         Deque<Integer> dequeMin = new LinkedList<>();
         Deque<Integer> dequeMax = new LinkedList<>();
         int ans = 0;
+
         while (right < n) {
-            while(!dequeMin.isEmpty() && dequeMin.peekLast() > nums[right]) {
+            int numRight = nums[right];
+            while (!dequeMin.isEmpty() && dequeMin.peekLast() > numRight) {
                 dequeMin.removeLast();
             }
-            dequeMin.addLast(nums[right]);
-            while(!dequeMax.isEmpty() && dequeMax.peekLast() < nums[right]) {
+            while (!dequeMax.isEmpty() && dequeMax.peekLast() < numRight) {
                 dequeMax.removeLast();
             }
-            dequeMax.addLast(nums[right]);
-            while (Math.abs(dequeMin.getFirst() - dequeMax.getFirst()) > limit) {
-                if (nums[left] == dequeMin.getFirst()) {
+            dequeMin.addLast(numRight);
+            dequeMax.addLast(numRight);
+            while (Math.abs(dequeMin.peekFirst() - dequeMax.peekFirst()) > limit) {
+                if (nums[left] == dequeMin.peekFirst()) {
                     dequeMin.removeFirst();
                 }
-                if (nums[left] == dequeMax.getFirst()) {
+                if (nums[left] == dequeMax.peekFirst()) {
                     dequeMax.removeFirst();
                 }
                 left++;
@@ -29,21 +31,4 @@ class Solution {
         }
         return (ans);
     }
-    /*
-        nums = [10,1,2,4,7,2], limit = 5
-        l = 5
-        10 i = 0 j = 0 min = 10 max = 10 ans = 1 mono deque ascendant [10(0)]
-        while (num(deque.getFist) > num(i)) {
-                deque.removeLast()
-            }
-        1 i = 0 j = 1 abs(10 - 1) > 5 
-            while (abs(num(j) - num(deque.getFist))) {
-                deque.removeFirst()
-            }
-
-            min = min(10, 1)-> 1
-            max = max(10, 1)-> 10
-
-    */
-
 }
