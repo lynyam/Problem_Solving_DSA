129f39ea3281e2a43fb91a0d3d5fcaaabe1467e5
lyn <46067184+lynyam@users.noreply.github.com>
2024-12-21 14:31:49 +0100

    Time: 1 ms (100%), Space: 44.4 MB (50.97%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -4,19 +4,16 @@ class Solution {
         for (int[] trip : trips) {
             end = Math.max(end, trip[2]);
         }
-        int[] diff = new int[end + 2];
+        int[] diff = new int[end + 1];
         for (int[] trip : trips) {
             diff[trip[1]] += trip[0];
             diff[trip[2]] -= trip[0];
         }
-        int[] answer = new int[end + 1];
-        int i = 1;
-        answer[0] = diff[0];
-        if (answer[0] > capacity)
-                return (false);
+        int i = 0;
+        int curr = 0;
         while (i <= end) {
-            answer[i] = diff[i] + answer[i - 1];
-            if (answer[i] > capacity)
+            curr += diff[i];;
+            if (curr > capacity)
                 return (false);
             i++;
         }
