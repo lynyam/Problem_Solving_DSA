e995dbe2a60c64cc5abc080a6506ced22acf218c
lyn <46067184+lynyam@users.noreply.github.com>
2024-12-21 09:28:57 +0100

    Time: 25 ms (92.41%), Space: 55.4 MB (48.46%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.java
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -1,53 +1,18 @@
-class Stock {
-    int price;
-    int days;
-    public Stock(int price, int days) {
-        this.price = price;
-        this.days = days;
-    }
-}
-
 class StockSpanner {
-    List<Stock> store;   
-   // List<Integer> consecutive;   
+    List<int[]> stack;
     public StockSpanner() {
-        store = new ArrayList<>();
-       // consecutive = new ArrayList<>();
+        stack = new ArrayList<>();
     }
     
     public int next(int price) {
-        int day = 1;
-        while (!store.isEmpty() && store.get(store.size() - 1).price < price) {
-            Stock stock = store.remove(store.size() - 1);
-            day += stock.days;
+        int answer = 1;
+        while (stack.size() > 0 && stack.get(stack.size() - 1)[0] <= price) {
+            answer += stack.remove(stack.size() - 1)[1];
         }
-        store.add(new Stock(price, day));
-        /*int n = store.size();
-        int i = n - 1;
-        int count = 0;
-        while (i >= 0) {
-            if (store.get(i) > price) {
-                break ;
-            }
-            count += i > 1 ? consecutive.get(i - 1) : 1;
-            i -= i > 1 ? consecutive.get(i - 1) : 1;
-        }
-        consecutive.add(count);*/
-        return (day);
+        stack.add(new int[]{price, answer});
+        return answer;
     }
 }
-/**
-    1 2 3 4 5 6
-    [80, 60] 
-
-
-   store  List<Integer>
-    consec List<Integer>
-
-
-
-
- */
 
 /**
  * Your StockSpanner object will be instantiated and called as such:
