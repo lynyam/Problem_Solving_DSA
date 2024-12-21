ad90dba91af5fce54f7246fc397fd50523059039
lyn <46067184+lynyam@users.noreply.github.com>
2024-12-21 16:45:11 +0100

    Time: 0 ms (100%), Space: 41.8 MB (78.82%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.java
+++ 0808-soup-servings/0808-soup-servings.java
@@ -1,28 +1,30 @@
 class Solution {
     double[][] dp;
     public double soupServings(int n) {
-        if (n > 4800)
+        if (n > 4800) 
+        {
             return (1.0);
-        dp = new double[n + 1][n + 1];
-        int i = 0;
-        while (i <= n) {
-            Arrays.fill(dp[i++], -1);
         }
-        return (helper(n, n));
+        int parts = n % 25 != 0 ? 1 + n / 25 : n / 25;
+        dp = new double[parts + 1][parts + 1];
+        int i = 0;
+        while (i <= parts)
+            Arrays.fill(dp[i++], -1.0);
+        return (helper(parts, parts));
     }
 
-    public double helper(int i, int j) {
-        if (i <= 0 && j <= 0)
+    public double helper(int a, int b) {
+        if (a <= 0 && b <= 0)
             return (0.5);
-        if (i <= 0)
-            return (1);
-        if (j <= 0)
-            return (0);
-        if (dp[i][j] != -1)
-            return (dp[i][j]);
-        double prob = 0.25 * (helper(i - 100, j) + helper(i - 75, j - 25) + 
-            helper(i - 50, j - 50) + helper(i - 25, j - 75));
-        dp[i][j] = prob;
-        return (dp[i][j]);
+        if (a <= 0)
+            return (1.0);
+        if (b <= 0)
+            return (0.0);
+        if (dp[a][b] != -1.0)
+            return (dp[a][b]);
+        double prob = 0.25 * (helper(a - 4, b) + helper(a - 3, b - 1) +
+                    helper(a - 2, b - 2) + helper(a - 1, b - 3));
+        dp[a][b] = prob;
+        return (prob);
     }
 }
