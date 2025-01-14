580e48953357de4a3dddedece3580f2558a17700
lyn <46067184+lynyam@users.noreply.github.com>
2025-01-14 11:19:27 +0100

    Time: 7 ms (78.81%), Space: 45 MB (99.36%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,20 +1,19 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        int max = 0;
-        for (int[] interval : intervals)
-            max = Math.max(max, interval[1]);
-        int[] diff = new int[max + 1];
-        int ans = 0;
+        int maxpos = 0;
         for (int[] interval : intervals) {
-            diff[interval[0]] += 1;
-            diff[interval[1]] -= 1;
+            maxpos = Math.max(maxpos, interval[1]);
         }
+        int[] arr = new int[maxpos + 1];
+        for (int[] interval : intervals) {
+            arr[interval[0]] += 1;
+            arr[interval[1]] -= 1;
+        }
+        int ans = 0;
         int curr = 0;
-        int i = 0;
-        while (i <= max) {
-            curr += diff[i];
+        for (int time : arr) {
+            curr += time;
             ans = Math.max(ans, curr);
-            i++;
         }
         return (ans);
     }
