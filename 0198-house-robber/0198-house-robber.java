e533f6ab5404ee28e522c5c0969e35b1b7badd9f
lyn <46067184+lynyam@users.noreply.github.com>
2025-01-14 07:24:03 +0100

    Time: 0 ms (100%), Space: 40.8 MB (81.91%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,24 +1,21 @@
 class Solution {
+    int n;
+    int[] dp;
+    int[] nums;
     public int rob(int[] nums) {
-        int one = 0;
-        int two = 0;
-        int n = nums.length;
-        int i = 0;
-        
-        while (i < n) {
-            if (i == 0)
-                one = nums[0];
-            if (i == 1) {
-                one = Math.max(one, nums[1]);
-                two = nums[0];
-            }
-            else {
-                int temp = one;
-                one = Math.max(one, two + nums[i]);
-                two = temp;
-            }
-            i++;
-        }
-        return (one);
+        this.nums = nums;
+        n = nums.length;
+        dp = new int[n];
+        Arrays.fill(dp, -1);
+        return (helper(0));
+    }
+
+    public int helper(int i) {
+        if (i >= n)
+            return (0);
+        if (dp[i] != -1)
+            return (dp[i]);
+        dp[i] = Math.max(nums[i] + helper(i + 2), helper(i + 1));
+        return (dp[i]);
     }
 }
