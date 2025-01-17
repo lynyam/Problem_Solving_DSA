63d1fd5edb327783a43dcf5291c540682c36af90
lyn <46067184+lynyam@users.noreply.github.com>
2025-01-17 07:29:50 +0100

    Time: 3 ms (7.91%), Space: 44.5 MB (59.88%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,36 +1,24 @@
 class Solution {
-    List<List<Integer>> ans;
+    List<List<Integer>> answer;
+    int[] nums;
     public List<List<Integer>> permute(int[] nums) {
-        ans = new ArrayList<>();
-        List<Integer> curr = new ArrayList<>();
-        backtrack(curr, nums);
-        return (ans);
+        this.nums = nums;
+        answer = new ArrayList<>(); 
+        backtrack(new ArrayList<Integer>());
+        return (answer);
     }
 
-    public void backtrack(List<Integer> curr, int[] nums) {
+    public void backtrack(List<Integer> curr) {
         if (curr.size() == nums.length) {
-            ans.add(new ArrayList<Integer>(curr));
+            answer.add(new ArrayList<Integer>(curr));
             return ;
         }
         for (int num : nums) {
             if (!curr.contains(num)) {
                 curr.add(num);
-                backtrack(curr, nums);
+                backtrack(curr);
                 curr.remove(curr.size() - 1);
             }
         }
     }
 }
-/**
-    - nums[] distinct int
-    - ret all possible perutation
-
-    Solution
-    - make exhaustive permutation and eliminate all of them wo one number has duplicate.Note a good idea nexpn+2
-    - make backtrack
-        - create curr List<Int>
-        - base case : if (curr.size == n) ans.add(copy of curr) just don't copy ref because null after all
-        - for loop on num: make ckeck if num : nums contains in curr 0(n) 
-            - if no add to curr and call backtrack => reduce to don't no
-            - delete last add in curr to back to parent and explore another child
- */
