041cd3302d46f3eaa576be70822fdcdc172b3f17
lyn <46067184+lynyam@users.noreply.github.com>
2025-01-20 11:35:23 +0100

    Time: 45 ms (40.74%), Space: 44.4 MB (53.51%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,12 +1,11 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        if (n == 0) return (0);
-        int[] dp = new int[n];
+        int[] dp = new int[nums.length];
         int i = 1;
-        dp[0] = 1;
         int res = 1;
+        int n = nums.length;
 
+        Arrays.fill(dp, 1);
         while (i < n) {
             int j = 0;
             int ans = 1;
@@ -14,12 +13,51 @@ class Solution {
                 if (nums[j] < nums[i]) {
                     ans = Math.max(ans, dp[j] + 1);
                 }
+                dp[i] = ans;
+                res = Math.max(res, dp[i]);
                 j++;
             }
-            dp[i] = ans;
-            res = Math.max(res, ans);
             i++;
         }
         return (res);
     }
 }
+
+/**
+Solution Top-down
+class Solution {
+    HashMap<Integer, Integer> memo;
+    public int lengthOfLIS(int[] nums) {
+        memo = new HashMap<>();
+        int ans = 1;
+        int i = 0;
+        while (i < nums.length) {
+            ans = Math.max(ans, dp(i, nums));
+            i++;
+        }
+        return (ans);
+    }
+
+    public int dp(int i, int[] nums) {
+        if (memo.containsKey(i))
+            return (memo.get(i));
+        int ans = 1;
+        int j = 0;
+        while (j < i) {
+            if (nums[j] < nums[i]) {
+                ans = Math.max(ans, dp(j, nums) + 1);
+            }
+            j++;
+        }
+        memo.put(i, ans);
+        return (ans);
+    }
+}*/
+/**
+    - nums int[] 
+    - ret length of longest strictly increasing subssequence
+
+    Solution DP
+        - return longest sub Fn(state) => state = i in [0, n[
+        - recurtion formula: Fn(n) = Fn(n-1)
+ */
