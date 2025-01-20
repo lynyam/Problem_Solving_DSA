04659e36f206ad63043beae8a3211338d600a363
lyn <46067184+lynyam@users.noreply.github.com>
2025-01-20 12:13:10 +0100

    Time: 12 ms (30.58%), Space: 125.1 MB (32.65%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.java
@@ -1,35 +1,20 @@
 class Solution {
+    long[] dp;
+    int[][] questions;
     public long mostPoints(int[][] questions) {
         int n = questions.length;
-        long[] dp = new long[n + 1];
+        dp = new long[n];
+        this.questions = questions;
+        Arrays.fill(dp, -1);
+        return (helper(0));
+    }
 
-        int i = n - 1;
-        while (i >= 0) {
-            if (i == n - 1)
-                dp[i] = questions[i][0];
-            else {
-                int index = i + questions[i][1] + 1;
-                dp[i] = Math.max(questions[i][0] + (index >= n ? dp[n] : dp[index]), dp[i + 1]);
-            }
-            i--;
-        }
-        return (dp[0]);
+    public long helper(int i) {
+        if (i >= questions.length)
+            return (0);
+        if (dp[i] != -1)
+            return (dp[i]);
+        dp[i] = Math.max(questions[i][0] + helper(i + questions[i][1] + 1), helper(i + 1));
+        return (dp[i]);
     }
 }
-/**
-    - q[][] = [pi, bi]
-    - start q i = 0
-        - decision solve or skip each question
-    - solv qi --> pi => can't solve bi next qi
-    - skip qi decision qi+1
-    - ret max point u can earn for the exam
-
-    Solution
-    - return max and each decision has impact the next choice => I can use DP
-        - return max point Fn(State) / state i index of question i
-        - recursive folrmula : Fn(i) = max(pi + Fn(i + qi[1]), Fn(i + 1)) 
-        - base case: i >= n return (0)
-
-        3
-
- */
