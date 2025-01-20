76d28c65a0b4f1887bc195e7d2cbb9f443dd0d92
lyn <46067184+lynyam@users.noreply.github.com>
2025-01-20 12:31:08 +0100

    Time: 0 ms (100%), Space: 41 MB (5.04%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,55 +1,22 @@
 class Solution {
+    int n, dp[];
     public int climbStairs(int n) {
-        int[] dp = new int[n + 1];
-        int i = 1;
-        while (i <= n) {
-            if (i == 1)
-                dp[i] = 1;
-            else if (i == 2)
-                dp[i] = 2;
-            else {
-                dp[i] = dp[i - 1] + dp[i - 2];
-            }
-            i++;
+        this.n = n;
+        dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        dp[1] = 1;
+        if (n >= 2) {
+            dp[2] = 2;
         }
-        return (dp[n]);
+        return (helper(n));
+    }
+
+    public int helper(int i) {
+        if (i > n || i <= 0)
+            return (0);
+        if (dp[i] != -1)
+            return (dp[i]);
+        dp[i] = helper(i - 1) + helper(i - 2);
+        return (dp[i]);
     }
 }
-/**
-class Solution {
-    HashMap<Integer, Integer> memo;
-    public int climbStairs(int n) {
-        memo = new HashMap<>();
-        return (dp(0, n));
-    }
-
-    public  int dp(int i, int n) {
-        if (i > n)
-            return (0);
-        if (i == n)
-            return 1;
-        if (memo.containsKey(i))
-            return (memo.get(i));
-        memo.put(i, dp(i + 1, n) + dp(i + 2, n));
-        return (memo.get(i));
-    }
-}*/
-/**
-    - Climbing staircase, take n steps to reach top
-    - can ether 1 or 2 steps
-    - how many distinct way can you clim to top
-
-    SOlution:
-    - return nbr of way Fn(State) 
-    - recuresive formula:
-        1 =>  1 Fn(1) = 1
-        2 => 1 + 1 | 2 => 2 Fn(2) = Fn(1) + 1 => 2
-        3 => 1 + 1 + 1 | 1 + 2 | 2 + 1 => 3 
-        4 => 1 + 1 + 1 + 1| 1 + 2 + 1 | 2 + 1 + 1 | 1 + 1 + 2 => 4
-        5 => 1 + 1 + 1 + 1 | 1 + 2 + 1 + 1 | 1 + 2 + 2 | 1 + 1 + 2 + 1 |  1 + 1 + 1 + 2 | 2 + 1 + 1 + 1 | 2 + 2 + 1
-        2 + 1 + 2 => 4 Fn(4) + Fn(3) +  1
-         3 case
-         F(0) = F(1) + F(2)
-
-
- */
