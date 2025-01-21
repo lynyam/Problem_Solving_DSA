e310502a1e8e9c27d27e2ca609c260910d2ade3e
lyn <46067184+lynyam@users.noreply.github.com>
2025-01-21 07:44:07 +0100

    Time: 1 ms (99.89%), Space: 46.5 MB (25.44%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,5 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
+        // 0 ^ x = x
+        // x ^ x ^ y = 0 ^ y = y
+        // if one sigle number and other twice => 0 ^ each num = sigle num
+        // bcse num1 ^ 0 ^ num1 ^ num2 ^ num2 ^ num3 = 0 ^ num3 = num3
+
         int mask = 0;
         for (int num : nums) {
             mask ^= num;
