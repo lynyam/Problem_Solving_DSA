efb4d5b97df7eafac0f17c6ba2612e64ed8ff7ee
lyn <46067184+lynyam@users.noreply.github.com>
2025-01-22 11:39:41 +0100

    Time: 0 ms (100%), Space: 40.9 MB (10.47%) - LeetHub

--- 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.java
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.java
@@ -1,5 +1,18 @@
 class Solution {
-    int answer;
+    public int countNumbersWithUniqueDigits(int n) {
+        if (n == 0)     return 1;
+        
+        int res = 10;
+        int uniqueDigits = 9;
+        int availableNumber = 9;
+        while (n-- > 1 && availableNumber > 0) {
+            uniqueDigits = uniqueDigits * availableNumber;
+            res += uniqueDigits;
+            availableNumber--;
+        }
+        return res;
+    }
+    /*int answer;
     public int countNumbersWithUniqueDigits(int n) {
         answer = 0;
         if (n == 0)
@@ -14,7 +27,6 @@ class Solution {
         }
         if (curr.length() > 0)
             answer++;
-        //System.out.println(curr.toString());
         int i = 0;
         while (i < 10) {
             if (curr.length() == 0 || ((curr.length() > 0 && curr.charAt(0) != '0') && !check.contains(i))) {
@@ -26,5 +38,5 @@ class Solution {
             }
             i++;
         }
-    }
+    }*/
 }
