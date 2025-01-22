44b0c33c9391ee5fa59b55857cc63222cb1624cc
lyn <46067184+lynyam@users.noreply.github.com>
2025-01-22 21:38:29 +0100

    Time: 0 ms (100%), Space: 41.3 MB (34.6%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,21 +1,20 @@
 class Solution {
-    int n;
     int[] dp;
     int[] nums;
     public int rob(int[] nums) {
+        int length = nums.length;
+        dp = new int[length];
         this.nums = nums;
-        n = nums.length;
-        dp = new int[n];
         Arrays.fill(dp, -1);
         return (helper(0));
     }
 
-    public int helper(int i) {
-        if (i >= n)
+    public  int helper(int house) {
+        if (house >= nums.length)
             return (0);
-        if (dp[i] != -1)
-            return (dp[i]);
-        dp[i] = Math.max(nums[i] + helper(i + 2), helper(i + 1));
-        return (dp[i]);
+        if (dp[house] != -1)
+            return (dp[house]);
+        dp[house] = Math.max(nums[house] + helper(house + 2), helper(house + 1));
+        return (dp[house]);
     }
 }
