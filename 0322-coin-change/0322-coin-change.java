e0bf53322a8b0bb7afd74fa2c0c4af5ac9c0983d
lyn <46067184+lynyam@users.noreply.github.com>
2025-01-22 20:52:34 +0100

    Time: 21 ms (39.3%), Space: 44.3 MB (80.22%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,26 +1,27 @@
 class Solution {
-    int[] coins;
     int[] dp;
+    int[] coins;
     public int coinChange(int[] coins, int amount) {
         dp = new int[amount + 1];
         this.coins = coins;
         Arrays.fill(dp, -1);
+        dp[0] = 0;
         int answer = helper(amount);
         return (answer == Integer.MAX_VALUE ? -1 : answer);
     }
 
     public int helper(int amount) {
-        if (amount == 0)
-            return (0);
         if (amount < 0)
             return (Integer.MAX_VALUE);
         if (dp[amount] != -1)
             return (dp[amount]);
-        int number = Integer.MAX_VALUE;
+        int fewerNumber = Integer.MAX_VALUE;
         for (int coin : coins) {
-            number = Math.min(number, helper(amount - coin));
+            fewerNumber = Math.min(fewerNumber, helper(amount - coin));
         }
-        dp[amount] = number == Integer.MAX_VALUE ? Integer.MAX_VALUE : number + 1;
-        return (dp[amount]);
+        if (fewerNumber != Integer.MAX_VALUE)
+            fewerNumber++;
+        dp[amount] = fewerNumber;
+        return (fewerNumber);
     }
 }
