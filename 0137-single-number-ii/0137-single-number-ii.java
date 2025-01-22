b059edb997e7e3bbcde3467212da7bbe5f67b410
lyn <46067184+lynyam@users.noreply.github.com>
2025-01-22 09:47:57 +0100

    Time: 2 ms (73.16%), Space: 45.2 MB (94.99%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,16 +1,16 @@
 class Solution {
     public int singleNumber(int[] nums) {
+        int bitSum = 0;
         int single = 0;
         int shift = 0;
-        int bitSum = 0;
         
-    
         while (shift < 32) {
             bitSum = 0;
             for (int num : nums) {
                 bitSum += (num >> shift) & 1;
             }
-            single |= ((bitSum % 3) << shift);
+            bitSum %= 3;
+            single |= (bitSum << shift);
             shift++;
         }
         return (single);
