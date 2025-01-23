3ef4d610ae575e80c2278ef8df8b5c6aef330eb0
lyn <46067184+lynyam@users.noreply.github.com>
2025-01-24 00:53:49 +0100

    Time: 0 ms (100%), Space: 40.5 MB (39.56%) - LeetHub

--- 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.java
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.java
@@ -1,42 +1,18 @@
 class Solution {
     public int countNumbersWithUniqueDigits(int n) {
-        if (n == 0)     return 1;
-        
-        int res = 10;
-        int uniqueDigits = 9;
-        int availableNumber = 9;
-        while (n-- > 1 && availableNumber > 0) {
-            uniqueDigits = uniqueDigits * availableNumber;
-            res += uniqueDigits;
-            availableNumber--;
-        }
-        return res;
-    }
-    /*int answer;
-    public int countNumbersWithUniqueDigits(int n) {
-        answer = 0;
         if (n == 0)
             return (1);
-        backtrack(new StringBuilder(), new HashSet<Integer>(), n);
-        return (answer);
-    }
+        int answer = 10;
+        int curr = 9;
+        int children = 9;
+        int i = 2;
 
-    public void backtrack(StringBuilder curr, Set<Integer> check, int n) {
-        if (curr.length() > n) {
-            return ;
-        }
-        if (curr.length() > 0)
-            answer++;
-        int i = 0;
-        while (i < 10) {
-            if (curr.length() == 0 || ((curr.length() > 0 && curr.charAt(0) != '0') && !check.contains(i))) {
-                curr.append(i);
-                check.add(i);
-                backtrack(curr, check, n);
-                curr.deleteCharAt(curr.length() - 1);
-                check.remove(i);
-            }
+        while (i <= n) {
+            curr *= children;
+            answer += curr;
+            children--;
             i++;
         }
-    }*/
+        return (answer);
+    }
 }
