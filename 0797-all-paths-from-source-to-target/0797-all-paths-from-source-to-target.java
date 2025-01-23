004a8027137ee8ff47768c85f55545713a96ab45
lyn <46067184+lynyam@users.noreply.github.com>
2025-01-23 15:31:39 +0100

    Time: 1 ms (100%), Space: 46.3 MB (61.77%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -1,19 +1,23 @@
 class Solution {
-    List<List<Integer>> ans;
+    List<List<Integer>> answer;
+    int[][] graph;
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        ans = new ArrayList<>();
-        backtrack(new ArrayList<Integer>(Arrays.asList(0)), 0, graph);
-        return (ans);
+        answer = new ArrayList<>();
+        this.graph = graph;
+        List<Integer> curr = new ArrayList<Integer>();
+        curr.add(0);
+        backtrack(curr, 0);
+        return (answer);
     }
 
-    public void backtrack(List<Integer> curr, int node, int[][] graph) {
-        if (node == graph.length - 1) {
-            ans.add(new ArrayList<>(curr));
+    public void backtrack(List<Integer> curr, int node) {
+        if (node == graph.length -1 ) {
+            answer.add(new ArrayList<Integer>(curr));
             return ;
         }
-        for (int neig : graph[node]) {
-            curr.add(neig);
-            backtrack(curr, neig, graph);
+        for (int neighbor : graph[node]) {
+            curr.add(neighbor);
+            backtrack(curr, neighbor);
             curr.remove(curr.size() - 1);
         }
     }
