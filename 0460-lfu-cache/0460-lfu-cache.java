ef43e075aa93ca109ac0f06316513da5ccf8589c
lyn <46067184+lynyam@users.noreply.github.com>
2025-02-04 20:39:22 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0460-lfu-cache/0460-lfu-cache.java
@@ -0,0 +1,67 @@
+class LFUCache {
+        Map<Integer, int[]> store;
+        List<LinkedHashSet<Integer>> frequency;
+        int capacity;
+        int minimumFreq = Integer.MAX_VALUE;
+        int size;
+        public LFUCache(int capacity) {
+            this.capacity = capacity;
+            store = new HashMap<>();
+            frequency = new ArrayList<>();
+        }
+    
+    public int get(int key) {//[1]
+        int value  = -1;
+        if (store.containsKey(key)) {
+            value = store.get(key)[0];//1
+            int freq = store.get(key)[1];//1
+            frequency.get(freq - 1).remove(key);//{{2}}
+            if (frequency.size() == freq)
+                frequency.add(new LinkedHashSet<Integer>());//{{2}->{}}
+            frequency.get(freq).add(key);//{{2}->{1}}
+            //manage minimumFreq
+            if (minimumFreq == freq && frequency.get(freq - 1).size() == 0)
+                minimumFreq = freq + 1;
+            store.put(key, new int[] {value, freq + 1});//{1:[1, 2], 2:[2, 1]}
+        }
+        return (value);
+    }
+    
+    public void put(int key, int value) {//[1,1],[2,2],[1],[3,3]
+        if (capacity <= 0)//2
+            return ;
+        if (size == capacity) {//2==2
+                int nkey = frequency.get(minimumFreq - 1).iterator().next();
+                frequency.get(minimumFreq - 1).remove(nkey);
+                store.remove(nkey);
+                size--;
+                if (frequency.get(minimumFreq - 1).size() == 0)
+                    minimumFreq = Integer.MAX_VALUE;
+        }
+		size++;//2
+		if (!store.containsKey(key)) {
+			store.put(key, new int[]{value, 1});//{1:[1, 2], 2:[2, 1]}
+			if (frequency.size() == 0) //0
+				frequency.add(new LinkedHashSet<Integer>());//{}
+			frequency.get(0).add(key);//{{2}->{1}}
+			minimumFreq = 1;//1
+		} else {
+                int freq = store.get(key)[1];
+                frequency.get(freq - 1).remove(key);//{}{}{}
+                if (frequency.size() == freq)
+                    frequency.add(new LinkedHashSet<Integer>());
+                frequency.get(freq).add(key);
+                //manage minimumFreq
+                if (minimumFreq == Integer.MAX_VALUE || (minimumFreq == freq && frequency.get(freq - 1).size() == 0))
+                    minimumFreq = freq + 1;
+                store.put(key, new int[] {value, freq + 1});
+        }
+	} 		
+}
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache obj = new LFUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
