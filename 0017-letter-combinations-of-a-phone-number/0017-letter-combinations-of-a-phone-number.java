a6d89848ea9bd41545862d392ebb6a3c79fb9ab6
lyn <46067184+lynyam@users.noreply.github.com>
2025-02-20 07:48:10 +0100

    Time: 0 ms (100%), Space: 42 MB (88.59%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,26 +1,32 @@
-class Solution {
-    String[] matching = new String[] {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-    List<String> answer;
-    String digits;
-    public List<String> letterCombinations(String digits) {
-        if (digits.isEmpty())
-            return (new ArrayList());
-        answer = new ArrayList<>();
-        this.digits = digits;
-        backtrack(new StringBuilder(), 0);
+class	Solution {
+	String[] buttons = new String[] {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+};
+	String digits;
+	List<String> answer;
+	int n;
+	public List<String> letterCombinations(String digits) {
+        if (digits.isEmpty()) return (new ArrayList());
+		this.digits = digits;
+		answer = new ArrayList<>();
+		n = digits.length();
+		backtrack(new StringBuilder(), 0);
         return (answer);
-    }
+	}
 
-    public void backtrack(StringBuilder curr, int index) {
-        if (curr.length() == digits.length()) {
-            answer.add(new String(curr));
-            return ;
-        }
-        for (char c : matching[digits.charAt(index) - '0'].toCharArray()) {
-            curr.append(c);
-            backtrack(curr, index + 1);
-            curr.deleteCharAt(curr.length() - 1);
-        }
-    }
 
+
+	public void backtrack(StringBuilder curr, int i) {
+		if (curr.length() == n) {
+			answer.add(new String(curr));
+			return;
+		}
+		int digit = digits.charAt(i) - '0';
+		String options = buttons[digit - 2];
+		for (char c : options.toCharArray()) {
+			curr.append(c);
+			backtrack(curr, i + 1);
+			curr.deleteCharAt(curr.length() - 1);
+		}
+	}
 }
+
