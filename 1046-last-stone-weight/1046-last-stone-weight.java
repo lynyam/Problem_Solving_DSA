0353c700d99633032da26e0de66188d571db957a
lyn <46067184+lynyam@users.noreply.github.com>
2025-02-21 07:43:44 +0100

    Time: 1 ms (97.76%), Space: 41.3 MB (30.47%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,30 +1,19 @@
-class Solution {
-    public int lastStoneWeight(int[] stones) {
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>();
 
-        for (int n : stones) {
-            maxHeap.add(-1 * n);
-        }
-        while (maxHeap.size() > 1) {
-            int max1 = maxHeap.remove();
-            int max2 = maxHeap.remove();
-            if (max1 != max2)
-                maxHeap.add(max1 - max2);
-        }
-        return (!maxHeap.isEmpty() ? -1 * maxHeap.remove() : 0);
-    }
-}/*
-    - stones int[]
-        - stones[i] weight of ith stone
-    - each turn, choose max two stones[i] ans smash
-        - x, y stones turn i x<= y smatch =>
-            - x == y delete x && y
-            - x != y delete x and y = y - x
-    - end game: one stone left, return or 0
-    result:nlogn time n space 
-        - convert arr on heap max //nlogn time n space
-        - while heapMax not empty
-            - remove max1 and max2 //nlogn time reduce space => 0(1)
-            - if (max1 != max2)
-                - add to heap (max2 - max1) //n/2logn
-*/
+class Solution {
+	public int	lastStoneWeight(int[] nums) { //2,7,4,1,8,1//1 1 2 //1 1
+		PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());//
+
+
+	for (int num : nums) {
+		maxHeap.add(num);
+	}
+
+
+	while (maxHeap.size() > 1) {
+		int difference = maxHeap.poll() - maxHeap.poll();//8 -7=1, 4-2=2
+		if (difference > 0)
+			maxHeap.add(difference);
+	}
+	return (!maxHeap.isEmpty() ? maxHeap.poll() : 0);
+}
+}
