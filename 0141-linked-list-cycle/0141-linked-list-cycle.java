c16bfb011b016ec6152c66f3502321a0f901854c
lyn <46067184+lynyam@users.noreply.github.com>
2025-02-25 07:30:43 +0100

    Time: 0 ms (100%), Space: 44.2 MB (87.89%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -9,19 +9,21 @@
  *     }
  * }
  */
-public class Solution {
-    public boolean hasCycle(ListNode head) {
-        if (head == null)
-            return (false);
-        ListNode fast = head.next;
-        ListNode slow = head;
 
-        while (fast != null && fast.next != null) {
-            if (fast == slow)
-                return (true);
-            fast = fast.next.next;
-            slow = slow.next;
-        }
-        return (false);
-    }
+public class Solution {
+	public boolean hasCycle(ListNode head) {
+		if (head == null) return (false);
+		ListNode fast = head.next;
+		ListNode slow = head;
+		int i = 0;
+
+
+		while (fast != null && fast.next != null) {//2, 1
+			if (fast == slow)//2 # 1
+				return (true);
+			slow = slow.next;//2
+			fast = fast.next.next;//2
+		}
+		return (false);
+	}
 }
