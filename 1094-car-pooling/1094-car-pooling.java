9f93a0409216f4bba37f30e91e08c577ea345988
lyn <46067184+lynyam@users.noreply.github.com>
2025-02-27 09:23:14 +0100

    Time: 1 ms (100%), Space: 44.5 MB (36.13%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -1,21 +1,26 @@
 class Solution {
-    public boolean carPooling(int[][] trips, int capacity) {
-        int i = 0;
-        int max = 0;
-        for (int[] trip : trips) {
-            max = Math.max(max, trip[2]);
-        }
-        int[] intervalls = new int[max + 1];
-        for (int[] trip : trips) {
-            intervalls[trip[1]] += trip[0];
-            intervalls[trip[2]] -= trip[0];
-        }
-        int ans = 0;
-        for (int position : intervalls) {
-            ans += position;
-            if (ans > capacity)
-                return (false);
-        }
-        return (true);
-    }
+	public boolean carPooling(int[][] trips, int capacity) {
+		//search max drop and create area of this size
+		int maxDrop = 0;
+		int minDrop = 0;
+		for (int[] trip : trips) {
+			maxDrop = Math.max(maxDrop, trip[2]);
+            minDrop = Math.min(minDrop, trip[1]);
+		}
+
+
+		int[] road = new int[maxDrop + 1];
+		for (int[] trip : trips) {//0(n), n trip
+			road[trip[1]] += trip[0];
+			road[trip[2]] -= trip[0];
+		}
+        int curr = 0;
+		for (int i = minDrop; i <= maxDrop; i++) {//maxDrop
+			road[i] += curr;
+			if (road[i] > capacity)
+				return (false);
+            curr = road[i];
+		}
+		return (true);
+	}
 }
