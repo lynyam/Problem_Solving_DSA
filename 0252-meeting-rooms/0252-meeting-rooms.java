a8fb8d360a9c30585c42108a4963f2c717ad58c8
lyn <46067184+lynyam@users.noreply.github.com>
2025-03-02 18:47:48 +0100

    Time: 6 ms (78.81%), Space: 45.9 MB (75.62%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.java
+++ 0252-meeting-rooms/0252-meeting-rooms.java
@@ -1,20 +1,8 @@
 class Solution {
-	public boolean canAttendMeetings(int[][] intervals) {
-		int max = 0;
-		int min = 0;
-		for (int[] meeting : intervals) {//0(n) time
-			max = Math.max(max, meeting[1]);
-			min = Math.min(min, meeting[0]);
-        }
-        int[] diff = new int[max + 1];//0(max) space
-        for (int[] m : intervals) {//0(n) 
-            diff[m[0]] += 1;
-            diff[m[1]] -= 1;
-        }
-        int curr = 0;
-        for (int i = min; i <= max; i++) {//0(max - min) time
-            curr += diff[i];
-            if (curr > 1)
+    public boolean canAttendMeetings(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < intervals[i - 1][1])
                 return (false);
         }
         return (true);
