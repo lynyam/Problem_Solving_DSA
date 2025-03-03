23865e56709cfcb8f133236f5e41da547cf664f7
lyn <46067184+lynyam@users.noreply.github.com>
2025-03-03 08:44:36 +0100

    Time: 73 ms (5.34%), Space: 62.8 MB (6.52%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.java
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -1,47 +1,47 @@
-public class Bucket {
-	int max;
-	int min;
-	public Bucket() {
-		min = Integer.MAX_VALUE;
-		max = Integer.MIN_VALUE;
-    }
-} 
 class Solution {
-    public int maximumGap(int[] nums) {
-		int len = nums.length;
-        if (len <= 1)
-            return (0);
+	public int maximumGap(int[] nums) {
+        int n = nums.length;
+        if (n < 2) return (0);
 		int max = Integer.MIN_VALUE;
-		int min = Integer.MAX_VALUE;
-		
 		for (int num : nums) {
-			min = Math.min(min, num);
 			max = Math.max(max, num);
-		}
-		int bucketSize = Math.max(1, (max - min) / (len - 1));
-		int nbrBucket = (max - min) / bucketSize + 1;
-		Bucket[] buckets = new Bucket[nbrBucket];
-		for (int num : nums) {
-			int indexBucket = (num - min) / bucketSize;
-            if (buckets[indexBucket] == null)
-                buckets[indexBucket] = new Bucket();
-			buckets[indexBucket].min = Math.min(num, buckets[indexBucket].min);
-            buckets[indexBucket].max = Math.max(num, buckets[indexBucket].max);
-		}
-		int i = 0;
-		int maxGap = 0;
-		while (i < nbrBucket) {
-			int j = i + 1;
-			while (j < nbrBucket) {
-				if (buckets[j] != null) {
-					maxGap = Math.max(maxGap, buckets[j].min - buckets[i].max);
-                    break ;
-				}
-				j++;
-			}
-			i = j;
-		}
-		return (maxGap);
-}
+        }
+        int digitCount = 0;
+        while (max > 0) {
+            max /= 10;
+            digitCount++;
+        }
+        
+        int dec = 1;
 
+
+        for (int i = 0; i < digitCount; i++) {
+            List<Integer>[] bucket = new ArrayList[10];
+            for (int num : nums) {
+                int digit;
+                if (i == 0) {
+                    digit = num % 10;
+                } else {
+                    digit = (num / dec) % 10;
+                }
+                if (bucket[digit] == null)
+                    bucket[digit] = new ArrayList<Integer>();
+                bucket[digit].add(num);
+            }
+            int j = 0;
+            for (List<Integer> temp : bucket) {
+                if (temp == null) continue ;
+                for (int number : temp) {
+                    nums[j] = number;
+                    j++;
+                }
+            }
+            dec *= 10;
+        }
+        int result = Integer.MIN_VALUE;
+        for (int i = 1; i < n; i++) {
+            result = Math.max(result, nums[i] - nums[i - 1]);
+        }
+        return (result);
+    }
 }
