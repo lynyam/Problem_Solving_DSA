34272e445128b3dd47d0356cdb52efba7e093848
lyn <46067184+lynyam@users.noreply.github.com>
2025-03-07 10:46:10 +0100

    Time: 148 ms (7.45%), Space: 63.4 MB (91.85%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,24 +1,28 @@
-class MedianFinder {
-    PriorityQueue<Integer> minHeap;
-    PriorityQueue<Integer> maxHeap;
-
-    public MedianFinder() {
-        minHeap = new PriorityQueue<>(Comparator.reverseOrder());
-        maxHeap = new PriorityQueue<>(); 
+class MedianFinder{
+	PriorityQueue<Integer> leftHeap;
+	PriorityQueue<Integer> rightHeap;
+	public MedianFinder() {
+		leftHeap = new PriorityQueue<>(Comparator.reverseOrder());
+		rightHeap = new PriorityQueue<>();
     }
-    
+
+
     public void addNum(int num) {
-        minHeap.add(num);
-        maxHeap.add(minHeap.remove());
-        if (maxHeap.size() > minHeap.size()) {
-            minHeap.add(maxHeap.remove());
+        leftHeap.add(num);
+        rightHeap.add(leftHeap.poll());
+        while (rightHeap.size() > leftHeap.size() + 1) {
+            leftHeap.add(rightHeap.poll());
         }
     }
-    
+
+
     public double findMedian() {
-        return (maxHeap.size() == minHeap.size() ? (minHeap.peek() + maxHeap.peek()) / 2.0 : minHeap.peek());
+        if (rightHeap.size() > leftHeap.size())
+            return(rightHeap.peek());
+        return ((double) (leftHeap.peek() + rightHeap.peek()) / 2.0);
     }
 }
+
 
 /**
  * Your MedianFinder object will be instantiated and called as such:
@@ -26,12 +30,3 @@ class MedianFinder {
  * obj.addNum(num);
  * double param_2 = obj.findMedian();
  */
- /**
-    - median = middle val in ordered integer list
-    - size % 2 == 2
-        - median = midle1 + midle2 / 2
-    - void addNum(int num)
-        - add num to dsa
-    - double findMedian()
-        - return median of all elt
-  */
