7d85b62de1a78d3a188fe88b720999a12dcda78e
lyn <46067184+lynyam@users.noreply.github.com>
2025-03-09 23:14:27 +0100

    Time: 8 ms (86.98%), Space: 47 MB (10.1%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,25 +1,22 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         int n = intervals.length;
-        boolean[] visited = new boolean[n];
         List<int[]> result = new ArrayList<>();
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        for (int i = 0; i < n; i++) {
-            if (visited[i]) continue;
-            visited[i] = true;
-            int min = intervals[i][0];
-            int max = intervals[i][1];
-            for (int j = 0; j < n; j++) {
-                if (visited[j] == true) continue;
-                int start = intervals[j][0];
-                int end = intervals[j][1];
-                if (max < start || end < min) continue;
-                visited[j] = true;
-                min = Math.min(min, start);
+        int min = intervals[0][0];
+        int max = intervals[0][1];
+        for (int i = 1; i < n; i++) {
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            if (start <= max) {
                 max = Math.max(max, end);
+            } else {
+                result.add(new int[]{min, max});
+                min = start;
+                max = end;
             }
-            result.add(new int[]{min, max});
         }
+        result.add(new int[]{min, max});
         int[][] arr = new int[result.size()][];
         int k = 0;
         for (int[] interval : result) {
@@ -38,6 +35,6 @@ class Solution {
    [0 2] [3 6] 
    [1 6] [0 2]
    [4 5] [2 3]
-        Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+        Input: intervals = [[1,3],[2,6],[8,10],[15,18]] => 
         Output: [[1,6],[8,10],[15,18]]
  */
