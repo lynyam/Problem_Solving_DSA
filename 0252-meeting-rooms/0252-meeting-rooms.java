c8a74e150b866e9c4c82dacde48c30d121757d8b
lyn <46067184+lynyam@users.noreply.github.com>
2025-03-09 21:52:12 +0100

    Time: 162 ms (5.14%), Space: 46.1 MB (60.69%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.java
+++ 0252-meeting-rooms/0252-meeting-rooms.java
@@ -1,10 +1,25 @@
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        for (int i = 1; i < intervals.length; i++) {
-            if (intervals[i][0] < intervals[i - 1][1])
-                return (false);
+        int n = intervals.length;
+        for (int i = 0; i < n; i++) {
+            int startI = intervals[i][0];
+            int endI = intervals[i][1];
+            for (int j = i + 1; j < n; j++) {
+                int startJ = intervals[j][0];
+                int endJ = intervals[j][1];
+                if ((startI <= startJ && startJ < endI) || (startJ <= startI && startI < endJ))
+                    return (false);
+            }
         }
         return (true);
+        
     }
 }
+
+/**
+    [0 10] [5, x] 
+    [5 10] [7 x]
+    Input: intervals = [[0,30],[5,10],[15,20]]
+Output: false
+
+ */
