6b4c74c1c9454d81495f5d4414d18977c03090d0
lyn <46067184+lynyam@users.noreply.github.com>
2025-03-11 05:07:53 +0100

    Time: 27 ms (82.55%), Space: 55.6 MB (18.57%) - LeetHub

--- 1229-meeting-scheduler/1229-meeting-scheduler.java
+++ 1229-meeting-scheduler/1229-meeting-scheduler.java
@@ -1,5 +1,7 @@
 class Solution {
 	public List<Integer> minAvailableDuration(int[][] slot1, int[][] slot2, int duration) {
+        Arrays.sort(slot1, (a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(slot2, (a, b) -> Integer.compare(a[0], b[0]));
 		List<Integer> result = new ArrayList<>();
 		int i = 0;
 		int j = 0;
@@ -12,10 +14,10 @@ class Solution {
 			int s2Start = slot2[j][0];//60
 			int s2End = slot2[j][1];//70
 			int earliestStart = s1Start >= s2Start ? s1Start : s2Start;//60
-			int earliestEnd = earliestStart + duration;//68
+			long earliestEnd = (long)earliestStart + duration;//68
 			if (earliestEnd <= s1End && earliestEnd <= s2End){
                 result.add(earliestStart);
-                result.add(earliestEnd);
+                result.add((int)earliestEnd);
                 break ;
             }
 			if (earliestStart >= s1Start && earliestEnd <= s1End)
