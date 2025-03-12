484b09032c6e3c29b4591d591539db76730e721b
lyn <46067184+lynyam@users.noreply.github.com>
2025-03-12 02:11:30 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -1,21 +1,21 @@
 class Solution {
-    public int numSubarrayProductLessThanK(int[] nums, int k) {
-        int n = nums.length;
-        int left = 0;
-        int right = 0;
-        int ans = 0;
-        long prod = 1;
-
-        if (k <= 1)
-            return (0);
-        while (right < n) {
-            prod *= nums[right];
-            while (prod >= k) {
-                prod /= nums[left++];
+	public int numSubarrayProductLessThanK(int[] nums, int k){
+        if (k == 0) return (0);
+		int n = nums.length;
+		int left = 0;
+		int right = 0;
+		int curr = 1;
+        int result = 0;
+	
+		while (right < n) {
+			curr *= nums[right];
+			while (curr >= k) {
+				curr /= nums[left];
+				left++;
             }
-            ans += right - left + 1;
+            result += (right - left + 1);
             right++;
         }
-        return (ans);
+        return (result);
     }
 }
