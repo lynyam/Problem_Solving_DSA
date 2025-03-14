a0e0b6ec832a01d918341df7e0cc8c8e4bb5f1f2
lyn <46067184+lynyam@users.noreply.github.com>
2025-03-14 04:53:04 +0100

    Time: 6 ms (70.52%), Space: 64.4 MB (34.29%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
@@ -1,25 +1,20 @@
 class Solution {
-    public int[] getAverages(int[] nums, int k) {
+	public int[] getAverages (int[] nums, int k) {
+		if (nums == null || nums.length == 0) return new int[0];
         int n = nums.length;
-        int[] ans = new int[n];
-        Arrays.fill(ans, -1);
-        long prefix = 0;
-        int i = 0;
-
-        if (2 * k + 1 > n) return (ans);
-
-        while (i < 2 * k + 1) {
-            prefix += nums[i];
-            i++;
+		if (k == 0) return Arrays.copyOf(nums, n);
+		long[] prefixSum = new long[n];
+		prefixSum[0] = nums[0];
+        int[] result = new int[n];
+        Arrays.fill(result, -1);
+        if (2 * k + 1 > n) return (result);
+                for (int i = 1; i < n; i++) {
+                    prefixSum[i] = prefixSum[i - 1] + nums[i];
         }
-        int j = k + 1;
-        ans[k] = (int)(prefix / (2 * k + 1));
-        while (i < n) {
-            prefix += nums[i] - nums[i - 2 * k - 1];
-            ans[j] = (int)(prefix / (2 * k + 1));
-            i++;
-            j++;
+//[7 11 14 23 24 32 37 39 45]
+        for (int j = k; j + k < n; j++) {
+                result[j] = (int)((prefixSum[j + k] - prefixSum[j - k] + nums[j - k]) / (2 * k + 1));
         }
-        return (ans);
+        return (result);
     }
 }
