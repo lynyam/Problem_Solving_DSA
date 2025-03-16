e5df15e560c9d6a84bb251dd0ac86377a7b8d76d
lyn <46067184+lynyam@users.noreply.github.com>
2025-03-16 03:06:18 +0100

    Time: 6 ms (85.5%), Space: 44.3 MB (83.68%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,58 +1,41 @@
-class Node {
-	int vertex;
-	int price;
-	int stop;
-	public Node (int v, int p, int s) {
-		vertex = v;
-		price = p;
-		stop = s;
-	}
-}
-
 class Solution {
-	public List<List<Node>> buildGraph(int[][] prices, int n) {
-		List<List<Node>> graph = new ArrayList<>();
-        int i = 0;
-        while (i <= n) {
-            graph.add(new ArrayList<>());
-            i++;
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        List<List<int[]>> adjList = new ArrayList<>();
+        //build adjacent graph
+        for (int i = 0; i < n; i++) {
+            adjList.add(new ArrayList<int[]>());
         }
-        for (int[] price : prices) {
-            graph.get(price[0]).add(new Node(price[1], price[2], 0));
+        for (int[] flight : flights) {
+            int from = flight[0];
+            int to = flight[1];
+            int price = flight[2];
+            adjList.get(from).add(new int[]{to, price});
         }
-        return (graph);	
-	}
-
-	public int findCheapestPrice(int n, int[][] prices, int src, int dst, int k) {
-        List<List<Node>> graph = buildGraph(prices, n);
-        int[][] distance = new int[n+1][k + 1];
-        for (int[] dist : distance) {
-            Arrays.fill(dist, Integer.MAX_VALUE);
-        }
-        //Arrays.fill(distance[src], 0);
-        distance[src][0] = 0;
-        PriorityQueue<Node> minHeap = new PriorityQueue<>(Comparator.comparingInt((Node p) -> p.price).thenComparingInt(p -> p.stop));
-        int i = 0;
-        minHeap.add(new Node(src, 0, -1));
-        while (!minHeap.isEmpty()) {
-            Node curr = minHeap.poll();
-            for (Node nei : graph.get(curr.vertex)) {
-                int vertex = nei.vertex;
-                int price = nei.price + curr.price;
-                int stop = curr.stop + 1; 
-                if (stop <= k && price  < distance[vertex][stop]) {
-                    distance[vertex][stop] = price;
-                    minHeap.add(new Node(vertex, price, stop));
-                }
+        //create priority queue for djiskastra
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
+        int[] stops = new int[n];
+        Arrays.fill(stops, Integer.MAX_VALUE);
+        //stops[src] = 0;
+        queue.add(new int[]{src, 0, 0});
+        
+        //walk through on graph from src to dst
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int node = curr[0];
+            int price = curr[1];
+            int stop = curr[2];
+            if (stop > stops[node] || stop > k + 1) continue;
+            stops[node] = stop;
+            if (node == dst) return (price);
+            for (int[] nei : adjList.get(node)) {
+                int neiPrice = price + nei[1];
+                int neiNode = nei[0];
+                //if (prices[neiNode] > neiPrice && stop + 1 <= k) {
+                    
+                    queue.add(new int[]{neiNode, neiPrice, stop + 1});
+                //}
             }
-	    }
-        i = 0;
-        int max = Integer.MAX_VALUE;
-        while (i <= k) {
-            //System.out.println(distance[dst][i]);
-            max = Math.min(max, distance[dst][i]);
-            i++;
         }
-        return (max == Integer.MAX_VALUE ? -1 : max);
-	}
+        return -1;
+    }
 }
