c1c9c708fd0a9a5780420c601fe54e53c1e789d9
lyn <46067184+lynyam@users.noreply.github.com>
2025-03-16 04:05:31 +0100

    Time: 6 ms (85.5%), Space: 44.8 MB (27.36%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -15,7 +15,6 @@ class Solution {
         PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
         int[] stops = new int[n];
         Arrays.fill(stops, Integer.MAX_VALUE);
-        //stops[src] = 0;
         queue.add(new int[]{src, 0, 0});
         
         //walk through on graph from src to dst
@@ -24,16 +23,13 @@ class Solution {
             int node = curr[0];
             int price = curr[1];
             int stop = curr[2];
-            if (stop > stops[node] || stop > k + 1) continue;
+            if (stops[node] < stop || stop > k + 1) continue;
             stops[node] = stop;
             if (node == dst) return (price);
             for (int[] nei : adjList.get(node)) {
                 int neiPrice = price + nei[1];
                 int neiNode = nei[0];
-                //if (prices[neiNode] > neiPrice && stop + 1 <= k) {
-                    
-                    queue.add(new int[]{neiNode, neiPrice, stop + 1});
-                //}
+                queue.add(new int[]{neiNode, neiPrice, stop + 1});
             }
         }
         return -1;
