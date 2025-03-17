5138d86a647459e9d85ec67a6c928eb15c82326b
lyn <46067184+lynyam@users.noreply.github.com>
2025-03-17 05:13:00 +0100

    Time: 9 ms (31.79%), Space: 44.8 MB (99.94%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.java
@@ -1,20 +1,24 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        int max = 0;
-        for (int[] interval : intervals) {
-            max = Math.max(max, interval[1]);
-        }
-        int[] timelines = new int[max + 1];
-        for (int[] interval : intervals) {
-            timelines[interval[0]] += 1;
-            timelines[interval[1]] -= 1;
-        }
-        int answer = 0;
-        int curr = 0;
-        for (int meeting : timelines) {
-            curr += meeting;
-            answer = Math.max(answer, curr);
-        }
-        return (answer);
+	int maxEnd = 0;
+	int minStart = 0;
+	
+	for (int[] interval : intervals) {//0(N)
+		maxEnd = Math.max(maxEnd, interval[1]);
+		minStart = Math.min(minStart, interval[0]);
+    }
+    int[] diff = new int[maxEnd + 1];//0(M)
+    for (int[] meeting : intervals) {//0(N)
+        diff[meeting[0]] += 1;
+        diff[meeting[1]] -= 1;
+    }
+    int curr = 0;
+    int result = 0;
+    for (int i = minStart; i <= maxEnd; i++) {//0(M)
+        diff[i] += curr;
+        curr = diff[i];
+        result = Math.max(result, curr);
+    }
+        return(result);
     }
 }
