e1dfcaa27d1d1113d4afa8e0ffa2a653911c7699
lyn <46067184+lynyam@users.noreply.github.com>
2025-03-18 02:35:04 +0100

    Time: 0 ms (100%), Space: 43 MB (76.8%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,18 +1,14 @@
 class Solution {
     public int  minCostClimbingStairs(int[] cost) {
         if (cost == null || cost.length == 0) return (0);
-        Map<Integer, Integer> memo = new HashMap<>();
-        return (helper(cost, memo, -1));
-    }
-
-
-    public int helper(int[] cost, Map<Integer, Integer> memo,  int state) {
-        if (state >= cost.length)
-            return (0);
-        if (memo.containsKey(state)) return (memo.get(state));
-        int result = 0;
-        if (state >= 0) result = cost[state];
-        memo.put(state, result + Math.min(helper(cost, memo, state + 1), helper(cost, memo, state + 2)));
-        return (memo.get(state));
+        int n = cost.length;
+        if (n == 1) return (cost[0]);
+        int[] sol = new int[n + 1];
+        sol[0] = cost[0];
+        sol[1] = cost[1];
+        for (int state = 2; state < n; state++) {
+            sol[state] = cost[state] + Math.min(sol[state - 1], sol[state - 2]);
+        }
+        return Math.min(sol[n - 1], sol[n - 2]);
     }
 }
