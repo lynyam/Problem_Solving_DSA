b588b939668e5eea7acd267f1388a2ce4fdd2522
lyn <46067184+lynyam@users.noreply.github.com>
2025-03-19 07:15:40 +0100

    Time: 33 ms (70.59%), Space: 56.7 MB (88.54%) - LeetHub

--- 2294-partition-array-such-that-maximum-difference-is-k/2294-partition-array-such-that-maximum-difference-is-k.java
+++ 2294-partition-array-such-that-maximum-difference-is-k/2294-partition-array-such-that-maximum-difference-is-k.java
@@ -1,33 +1,24 @@
 class Solution {
     public int partitionArray(int[] nums, int k) {
         Arrays.sort(nums);
-        int i = 0;
+        int left = 0;
+        int right = 0;
         int n = nums.length;
         int ans = 0;
 
-        while (i < n) {
-            int min = nums[i];
-            i++;
+        while (right < n) {
             ans++;
-            while (i < n) {
-                if (nums[i] > k + min) {
-                    break ;
-                }
-                i++;
+            while (right < n && nums[right] - nums[left] <= k) {
+                right++;
             }
+            left = right;
         }
         return (ans);
     }
 }
 /**
-    - nums int[]
-    - int k
-    - partition nums into one or more subsequence
-        - each element in nums appears in exactly one of the subsequence
-    - ret minimum number of subsequence needed
-        - difference btw the max and min val in each subseq is at most k
-    - Subsequence is a sequence than can be
-        - derived from another seq by deleting some or no elts without changing the order of remaining elts
-    - ex: nums = [3,6,1,2,5], k = 2 => 2
-
+    - nums[] , int k
+    - partition nums into 1 0r more subsequence, each elt in nums appears one time
+    - ret mini number of subseq/ diff(max, min val) <= k
+    3-x <= 2 =>x >= 3-1
  */
