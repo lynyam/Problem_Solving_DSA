00a15b84a7e7c60ee59c0f2c50ce837e3189613e
lyn <46067184+lynyam@users.noreply.github.com>
2025-03-20 06:25:17 +0100

    Time: 1 ms (99.12%), Space: 44.9 MB (77.39%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -2,38 +2,20 @@ class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         int n = intervals.length;
         if (n == 0) return (new int[][] {newInterval});
-        int min = newInterval[0];
-        int max = newInterval[1];
-        int currStart;
-        int currEnd;
         List<int[]> result = new ArrayList<>();
         int i = 0;
-        while (i < n) {
-            currStart = intervals[i][0];
-            currEnd = intervals[i][1];
-            if (currEnd < newInterval[0])
-                result.add(intervals[i]);
-            else {
-                //min = Math.min(newInterval[0], currStart);
-                //max = Math.max(newInterval[1], currEnd);
-                break;
-            }
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            result.add(intervals[i++]);
+        }
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+                newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+                newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
             i++;
         }
-        while (i < n) {
-            currStart = intervals[i][0];
-            currEnd = intervals[i][1];
-            if (currStart <= max) {
-                min = Math.min(min, currStart);
-                max = Math.max(max, currEnd);
-            } else {
-                result.add(new int[]{min, max});
-                min = currStart;
-                max = currEnd;
-            }
-            i++;
+        result.add(new int[]{newInterval[0], newInterval[1]});
+         while (i < n) {
+            result.add(intervals[i++]);
         }
-        result.add(new int[]{min, max});
         return (result.toArray(new int[result.size()][]));
     }
 }
