22079e4bd1b61f6e45cea52a152ea1a700471b06
lyn <46067184+lynyam@users.noreply.github.com>
2025-03-20 01:41:18 +0100

    Time: 15 ms (40.46%), Space: 48.9 MB (33.68%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,22 +1,24 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> store = new HashMap<>();
-        int[] ans = new int[k];
-        PriorityQueue<Integer> heap = new PriorityQueue<>((n1, n2) -> (store.get(n1) -
-         store.get(n2)));
-        int i = 0;
-
+        int n = nums.length;
+        Map<Integer, Integer> frequency = new HashMap<>();
+        
         for (int num : nums) {
-            store.put(num, store.getOrDefault(num, 0) + 1);
+            frequency.put(num, frequency.getOrDefault(num, 0) + 1);//{1:3, 2:2, 3:1}
         }
-        for (int n : store.keySet()) {
-            heap.add(n);
-            if (heap.size() > k)
-                heap.remove();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b)-> frequency.get(a) - frequency.get(b));
+        for (int num : frequency.keySet()) {
+            maxHeap.add(num);//{3, 2, 1}
+            if (maxHeap.size() > k) {
+            maxHeap.poll();//{2, 1}
+            }
         }
-        while (!heap.isEmpty()) {
-            ans[i++] = heap.remove();
+        int i = 0;
+        int[] result = new int[k];
+        while (!maxHeap.isEmpty()) {
+            result[i++] = maxHeap.poll();
         }
-        return (ans);
+        return (result);
     }
+
 }
