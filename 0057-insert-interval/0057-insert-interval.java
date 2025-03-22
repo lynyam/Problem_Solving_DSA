69add13a7efcc351de596c947c62792527fe533f
lyn <46067184+lynyam@users.noreply.github.com>
2025-03-22 07:44:03 +0100

    Time: 40 ms (6.37%), Space: 45.8 MB (5.07%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int[][] insert(int[][] intervals, int[] newInterval) {
+    /*public int[][] insert1(int[][] intervals, int[] newInterval) {
         int n = intervals.length;
         if (n == 0) return (new int[][] {newInterval});
         List<int[]> result = new ArrayList<>();
@@ -17,9 +17,60 @@ class Solution {
             result.add(intervals[i++]);
         }
         return (result.toArray(new int[result.size()][]));
+        [[1,3],[6,9]]
+        [2,5]
+
+        1 6 / 2 => 1
+        [[1,3],[2,5],[6,9]]
+        [[1,5],[6,9]]
+
+        [2,5]
+        [1,3]
+         2 <= 3 [1,5]
+         [6,9]
+         6 > 5
+
+    }*/
+
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        int n = intervals.length;
+        if (n == 0) return (new int[][]{newInterval});
+        int left = 0;
+        int right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (intervals[mid][0] > newInterval[0])
+                right = mid - 1;
+            else if (intervals[mid][0] < newInterval[0])
+                left = mid + 1;
+            else {
+                left = mid;
+                break;
+            }
+        }
+        List<int[]> result = new ArrayList<>();
+        int i = 0;
+        while (i < left) {
+            result.add(intervals[i]);
+            i++;
+        }
+        if (result.size() != 0 && newInterval[0] <= result.get(result.size() - 1)[1]) {
+            result.get(result.size() - 1)[1] = Math.max(result.get(result.size() - 1)[1], newInterval[1]);
+        } else {
+            result.add(newInterval);
+        }
+        
+        while (i < n && intervals[i][0] <= result.get(result.size() - 1)[1]) {
+             System.out.println(result.get(result.size() - 1)[1]);
+             System.out.println(intervals[i][1]);
+            result.get(result.size() - 1)[1] = Math.max(result.get(result.size() - 1)[1], intervals[i][1]);
+           
+            i++;
+        }
+        while (i < n) {
+            result.add(intervals[i]);
+            i++;
+        }
+        return (result.toArray(new int[result.size()][]));
     }
 }
-/*       [     ]
-    [  ]
-
-*/
