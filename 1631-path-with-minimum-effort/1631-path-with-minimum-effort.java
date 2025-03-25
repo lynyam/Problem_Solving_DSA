4aef4eecc922139ac4545167a93f8325a9c19da3
lyn <46067184+lynyam@users.noreply.github.com>
2025-03-25 07:29:06 +0100

    Time: 87 ms (15.33%), Space: 45.9 MB (12.12%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,57 +1,47 @@
 class Solution {
-    int m;
-    int n;
-    int[][] directions = new int[][] {
-        {-1, 0}, {0, -1},
-        {1, 0}, {0, 1}
+    int[][] childrens = new int[][] {
+		{-1, 0}, {0, -1},
+		{1, 0}, {0, 1}
     };
-
-    public boolean isValid(int x, int y) {
-        return (x >= 0 && x < n && y >= 0 && y < m);
-    }
-
-    public int minimumEffortPath(int[][] heights) {
-        int left = 0;
-        int right = 0;
-        n = heights.length;
-        m = heights[0].length;
-
-        for (int[] row : heights) {
-            for (int col : row) {
-                right = Math.max(right, col);
+	public int minimumEffortPath(int[][] heights) {
+		int min  = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		int m = heights.length;
+		int n = heights[0].length;
+		for (int[] height : heights) {
+			for (int cell : height) {
+				min = Math.min(min, cell);
+				max = Math.max(max, cell);
             }
         }
-
+        int right = Math.abs(max - min);
+        int left = 0;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (check(mid, 0, 0, heights, new boolean[n][m]))
+            if (hasPassedDfs(0, 0, mid, m, n, heights, new boolean[m][n])) {
                 right = mid - 1;
-            else
-                left = mid + 1;
+        } else
+            left = mid + 1;
         }
         return (left);
     }
-
-    public boolean check(int mid, int x, int y, int[][] heights, boolean[][] seen) {
-        if (x == n - 1 && y == m - 1)
+    public boolean hasPassedDfs(int row, int col, int mid, int m, int n, int[][] heights, boolean[][] visited) {
+        if (row == m - 1 && col == n - 1)
             return (true);
-        seen[x][y] = true;
-        boolean ans = false;
-        for (int[] dir : directions) {
-            int row = x + dir[0];
-            int col = y + dir[1];
-            if (isValid(row, col) && !seen[row][col] && Math.abs(heights[x][y] - heights[row][col]) <= mid) {
-                ans |= check(mid, row, col, heights, seen);
+        visited[row][col] = true;
+        boolean result = false;
+        for (int[] neighbor : childrens) {
+            int neiCol = col + neighbor[1];
+            int neiRow = row + neighbor[0];
+            if (isValid(neiRow, neiCol, m, n) && !visited[neiRow][neiCol] && Math.abs(heights[row][col] - heights[neiRow][neiCol]) <= mid) {
+                result |= hasPassedDfs(neiRow, neiCol, mid, m, n, heights, visited); 
             }
         }
-        return (ans);
+        //visited[row][col] = false;
+        return (result);
+    }
+
+    public boolean isValid(int x, int y, int m, int n) {
+        return (x >= 0 && x < m && y >= 0 && y < n);
     }
 }
-/**
-    - heights[][] row x col
-        - heights[row][col] = height of (row, col)
-        - (0, 0) -> (row - 1, col - 1) moov left right  down up
-        - root requires minimum effort
-        - root effort = max abs(h1e - h2)
-        - ret mini effort
- */
