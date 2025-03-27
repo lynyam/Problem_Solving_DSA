7f6c82605b72be39aec2a537db52006728488d55
lyn <46067184+lynyam@users.noreply.github.com>
2025-03-27 01:49:47 +0100

    Time: 45 ms (53.24%), Space: 45.2 MB (67.69%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,47 +1,50 @@
 class Solution {
     int[][] childrens = new int[][] {
-		{-1, 0}, {0, -1},
-		{1, 0}, {0, 1}
+        {-1, 0}, {0, -1},
+        {1, 0}, {0, 1}
     };
-	public int minimumEffortPath(int[][] heights) {
-		int min  = Integer.MAX_VALUE;
-		int max = Integer.MIN_VALUE;
-		int m = heights.length;
-		int n = heights[0].length;
-		for (int[] height : heights) {
-			for (int cell : height) {
-				min = Math.min(min, cell);
-				max = Math.max(max, cell);
-            }
-        }
-        int right = Math.abs(max - min);
-        int left = 0;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (hasPassedDfs(0, 0, mid, m, n, heights, new boolean[m][n])) {
-                right = mid - 1;
-        } else
-            left = mid + 1;
-        }
-        return (left);
-    }
-    public boolean hasPassedDfs(int row, int col, int mid, int m, int n, int[][] heights, boolean[][] visited) {
-        if (row == m - 1 && col == n - 1)
-            return (true);
-        visited[row][col] = true;
-        boolean result = false;
-        for (int[] neighbor : childrens) {
-            int neiCol = col + neighbor[1];
-            int neiRow = row + neighbor[0];
-            if (isValid(neiRow, neiCol, m, n) && !visited[neiRow][neiCol] && Math.abs(heights[row][col] - heights[neiRow][neiCol]) <= mid) {
-                result |= hasPassedDfs(neiRow, neiCol, mid, m, n, heights, visited); 
-            }
-        }
-        visited[row][col] = false;
-        return (result);
-    }
 
     public boolean isValid(int x, int y, int m, int n) {
         return (x >= 0 && x < m && y >= 0 && y < n);
+    }
+
+    public int minimumEffortPath(int[][] heights) {
+        int m = heights.length;
+        int n = heights[0].length;
+        //create an arrays to store a minimum possible effort from (0, 0) to cell(i, j)
+        // fill initialy all effort to inf, and effort from (0, 0) ->(0, 0) is 0
+        int[][] efforts = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            Arrays.fill(efforts[i], Integer.MAX_VALUE);
+        }
+        efforts[0][0] = 0;
+        //to make sure to already choose a best path, it very importante that for every choise we
+        //choose a cell with which belong to a best path. it a cell with minimum effort from 0, 0 to it-> we use Priorytyqueue to always ensure to havee minimum.
+        // add (0, 0, 0) to minHeap => (0, 0) with effort 0
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a[2], b[2]));
+        minHeap.add(new int[]{0,0,0});
+
+        //we poll the best path cell in minHeap until minHeap is Empty()
+        //for every cell we loop on neighbor/children (up down right left). We add Children in minHeap in case who:
+            // it is in boundary of heights, efforts for this children > new effort
+        // if cell is bottom right return effort
+        while (!minHeap.isEmpty()) {
+            int[] cell = minHeap.poll();
+            if (cell[0] == m - 1 && cell[1] == n - 1) return (cell[2]);
+            //if (efforts[cell[0]][cell[1]] <= cell[2]) continue;
+            for (int[] child : childrens) {
+                int row = cell[0] + child[0];
+                int col = cell[1] + child[1];
+                if (isValid(row, col, m, n)) {
+                    int effort = Math.max(cell[2], Math.abs(heights[cell[0]][cell[1]] - heights[row][col])); 
+                    if (efforts[row][col] > effort) {
+                        efforts[row][col] = effort;
+                        minHeap.add(new int[]{row, col, effort});
+                    }
+                }
+            }
+        }
+        
+        return (efforts[m - 1][n - 1]);
     }
 }
