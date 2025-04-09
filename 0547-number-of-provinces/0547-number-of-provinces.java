a2de5015c0ca4d84636c948ac69e8b2227454649
lyn <46067184+lynyam@users.noreply.github.com>
2025-04-09 14:14:20 +0200

    Time: 1 ms (89.55%), Space: 48.2 MB (28.18%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,57 +1,26 @@
 class Solution {
-    public void buildGraph(HashMap<Integer, List<Integer>> graph, int[][] isConnected) {
-        int i = 0;
-        int j = 0;
-        int n = isConnected.length;
-        while (i < n) {
-            j = i + 1;
-            graph.putIfAbsent(i, new ArrayList<Integer>());
-            while (j < n) {
-                graph.putIfAbsent(j, new ArrayList<Integer>());
-                if (isConnected[i][j] == 1) {
-                    graph.get(i).add(j);
-                    graph.get(j).add(i);
-                }
-                j++;
-            }
-            i++;
-        }
-    }
-
     public int findCircleNum(int[][] isConnected) {
-        HashMap<Integer, List<Integer>> graph = new HashMap<>();
+        if (isConnected == null || isConnected.length == 0) return (0);
         int n = isConnected.length;
-        int i = 0;
-        int ans = 0;
         boolean[] seen = new boolean[n];
-        buildGraph(graph, isConnected);
+        int result = 0;
 
-        while (i < n) {
-            if (!seen[i]) {
-                ans++;
-                bfs(i, graph, seen);
+        for (int node = 0; node < n; node++) {
+            if (!seen[node]) {
+                result++;
+                seen[node] = true;
+                dfs(node, seen, isConnected);
             }
-            i++;
         }
-        return (ans);
+        return (result);
     }
 
-    public void bfs(int node, HashMap<Integer, List<Integer>> graph, boolean[] seen) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(node);
-        while (!queue.isEmpty()) {
-            int i = 0;
-            int size = queue.size();
-            while (i < size){
-                int currentNode = queue.remove();
-                if (!seen[currentNode]) {
-                    seen[currentNode] = true;
-                    for (Integer connect : graph.get(currentNode)) {
-                        queue.add(connect);
-                    }
-                }              
-                i++;
-            }  
+    public void dfs(int node, boolean[] seen, int[][] isConnected) {
+        for (int nei = 0; nei < isConnected[0].length; nei++) {
+            if (!seen[nei] && isConnected[node][nei] == 1) {
+                seen[nei] = true;
+                dfs(nei, seen, isConnected);
+            }
         }
-    }
+    } 
 }
