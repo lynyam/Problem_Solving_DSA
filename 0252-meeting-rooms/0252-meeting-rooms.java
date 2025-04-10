9feb33e96b999728582ab08ca1afd9016b1d5107
lyn <46067184+lynyam@users.noreply.github.com>
2025-04-10 19:56:53 +0200

    Time: 8 ms (11.33%), Space: 46 MB (77.7%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.java
+++ 0252-meeting-rooms/0252-meeting-rooms.java
@@ -1,35 +1,13 @@
 class Solution {
-    /*public boolean canAttendMeetings(int[][] intervals) {
-        int n = intervals.length;
-        for (int i = 0; i < n; i++) {
-            int startI = intervals[i][0];
-            int endI = intervals[i][1];
-            for (int j = i + 1; j < n; j++) {
-                int startJ = intervals[j][0];
-                int endJ = intervals[j][1];
-                if ((startI <= startJ && startJ < endI) || (startJ <= startI && startI < endJ))
-                    return (false);
-            }
-        }
-        return (true);
-        
-    }*/
-
     public boolean canAttendMeetings(int[][] intervals) {
-        int n = intervals.length;
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        for (int i = 1; i < n; i++) {
-            if (intervals[i][0] < intervals[i - 1][1])
-                return (false);
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        for (int i = 1; i < intervals.length; i++) {
+            int currStart = intervals[i][0];
+            int currEnd = intervals[i][1];
+            int cmpStart = intervals[i - 1][0];
+            int cmpEnd = intervals[i - 1][1];
+            if (currStart >= cmpStart && currStart < cmpEnd) return false;
         }
         return (true);
     }
 }
-
-/**
-    [0 10] [5, x] 
-    [5 10] [7 x]
-    Input: intervals = [[0,30],[5,10],[15,20]]
-Output: false
-
- */
