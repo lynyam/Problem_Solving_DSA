89582e181122a8c3668da12833c5bf046ba6ef27
lyn <46067184+lynyam@users.noreply.github.com>
2025-05-15 10:20:28 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -13,9 +13,32 @@
  *     }
  * }
  */
+public class State {
+    int sum;
+    TreeNode node;
+    public State(TreeNode n, int s) {
+        node = n;
+        sum = s;
+    }
+}
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        return (dfs(root, 0, targetSum));
+        //return (dfs(root, 0, targetSum));
+        if (root == null) return (false);
+        List<State> stack = new ArrayList<>();
+        stack.add(new Stack(root, 0));
+        while (stack.size() != 0) {
+            State curr = stack.remove(stack.size() - 1);
+            TreeNode node = curr.node;
+            int sum = curr.sum;
+            if (node == null) continue;
+            sum += node.val;
+            if (node.left == null && node.right == null && sum == targetSum)
+                return (true);
+            stack.add(new State(node.left, sum));
+            stack.add(new State(node.right, sum));
+        }
+        return (false);
     }
 
     public boolean dfs(TreeNode node, int sum, int targetSum) {
