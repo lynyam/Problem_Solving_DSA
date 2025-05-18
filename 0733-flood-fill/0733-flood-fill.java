079b48327a94a0b85723aaf2f50febf57f4b35e8
lyn <46067184+lynyam@users.noreply.github.com>
2025-05-18 13:57:01 +0200

    Time: 0 ms (100%), Space: 45 MB (49.01%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,13 +1,6 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        helper(image, sr, sc, color);
-        int n = image.length;
-        int m = image[0].length;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (image[i][j] < 0) image[i][j] = color;
-            }
-        }
+        helper(image, sr, sc, color, image[sr][sc]);
         return (image);
     }
 
@@ -18,16 +11,15 @@ class Solution {
         {-1, 0}, {1, 0}, {0, -1}, {0, 1}
     };
 
-    public void helper(int[][] image, int sr, int sc, int color) {
-        if (image[sr][sc] < 0) return;
-        int currColor = image[sr][sc];
-        image[sr][sc] = image[sr][sc] == 0 ? Integer.MIN_VALUE : -1 * image[sr][sc];
+    public void helper(int[][] image, int sr, int sc, int color, int startColor) {
+        if (image[sr][sc] == color) return;
+        image[sr][sc] = color;
         
         for (int[] nei : neighbor) {
             int x = nei[0] + sr;
             int y = nei[1] + sc;
-            if (isValid(x, y, image) && image[x][y] >= 0 && image[x][y] == currColor) {
-                helper(image, x, y, color);
+            if (isValid(x, y, image) && image[x][y] >= 0 && image[x][y] == startColor) {
+                helper(image, x, y, color, startColor);
             }
         }
     }
