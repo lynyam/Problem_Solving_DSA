70e590abd8c2cda29632929ecf6858818a6f9a21
lyn <46067184+lynyam@users.noreply.github.com>
2025-06-09 12:04:14 +0200

    Time: 8 ms (35.85%), Space: 42.8 MB (50.58%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.java
+++ 0257-binary-tree-paths/0257-binary-tree-paths.java
@@ -15,32 +15,21 @@
  */
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
-        List<String> result = new ArrayList<>();
-        helper(root, result, new ArrayList<Integer>());
-        return (result);
+        List<String> paths = new ArrayList<>();
+        if (root == null) return (paths);
+        inOrderTraversal(root, "", paths);
+        return (paths);
     }
 
-    public String transformToString(List<Integer> path) {
-        int n = path.size();
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            sb.append(path.get(i));
-            if (i != n - 1) sb.append("->");
-        }
-        return (sb.toString());
-    }
-
-    public void helper(TreeNode root, List<String> result, List<Integer> path) {
-        if (root == null)
-            return;
-        path.add(root.val);
+    public void inOrderTraversal(TreeNode root, String path, List<String> paths) {
+        if (root == null) return ;
+        path += root.val;
         if (root.left == null && root.right == null) {
-            result.add(transformToString(path));
-            path.removeLast();
+            paths.add(path);
             return ;
         }
-        if (root.left != null) helper(root.left, result, path);
-        if (root.right != null) helper(root.right, result, path);
-        path.removeLast();
+        path += "->"; 
+        inOrderTraversal(root.left, path, paths);
+        inOrderTraversal(root.right, path, paths);
     }
 }
