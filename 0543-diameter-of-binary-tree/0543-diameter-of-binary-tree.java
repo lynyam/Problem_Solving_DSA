beba8d9d87b3da357f430a5cbcdee5aeb4b923bd
lyn <46067184+lynyam@users.noreply.github.com>
2025-06-09 08:22:19 +0200

    Time: 0 ms (100%), Space: 45 MB (38.43%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -10,29 +10,28 @@
  *         this.val = val;
  *         this.left = left;
  *         this.right = right;
- *     }        1
-               2 3
-              5 6
-             7
-             1 2 (5 (7 ((null) (null)))
-             ans = 1, ret 2
+ *     }
  * }
  */
 class Solution {
-    int ans = 0;
+    int result;
     public int diameterOfBinaryTree(TreeNode root) {
-        if (root == null)
-            return (0);
-        dfs(root);
-        return (ans);
+        result = 0;
+        preOrderdfs(root);
+        return (result);
     }
+    public int preOrderdfs(TreeNode root) {
+        //base case 
+        if (root == null) return (0);
 
-    public int dfs(TreeNode root) {
-        if (root == null)
-            return (0);
-        int left = dfs(root.left);
-        int right = dfs(root.right);
-        ans = Math.max(ans, left + right);
-        return (Math.max(left, right) + 1);
+        //PostOrder Traversal
+        int left = preOrderdfs(root.left);
+        int right = preOrderdfs(root.right);
+
+        //some logic to define result for current node
+        result = Math.max(result, left + right);
+
+        //return value 
+        return (1 + Math.max(left, right));
     }
 }
