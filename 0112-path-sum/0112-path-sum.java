cf891f3d6131d74f7cec30e942c26f38930e6c92
lyn <46067184+lynyam@users.noreply.github.com>
2025-06-09 15:33:54 +0200

    Time: 0 ms (100%), Space: 43.5 MB (26.1%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -13,45 +13,16 @@
  *     }
  * }
  */
-public class State {
-    int sum;
-    TreeNode node;
-    public State(TreeNode n, int s) {
-        node = n;
-        sum = s;
-    }
-}
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        //return (dfs(root, 0, targetSum));
-        if (root == null) return (false);
-        List<State> stack = new ArrayList<>();
-        stack.add(new Stack(root, 0));
-        while (stack.size() != 0) {
-            State curr = stack.remove(stack.size() - 1);
-            TreeNode node = curr.node;
-            int sum = curr.sum;
-            if (node == null) continue;
-            sum += node.val;
-            if (node.left == null && node.right == null && sum == targetSum)
-                return (true);
-            stack.add(new State(node.left, sum));
-            stack.add(new State(node.right, sum));
-        }
-        return (false);
+        return (postOrderTraversal(root, 0, targetSum));
     }
 
-    public boolean dfs(TreeNode node, int sum, int targetSum) {
-        //base case 
-        if (node == null) return false;
-
-        //logic on current node
-        sum += node.val;
-        if (node.left == null && node.right == null) return (sum == targetSum);
-        return dfs(node.left, sum, targetSum) || dfs(node.right, sum, targetSum);
+    public boolean postOrderTraversal(TreeNode root, int sum, int targetSum) {
+        if (root == null) return (false);
+        sum += root.val;
+        if (root.left == null && root.right == null) return (sum == targetSum);
+        return (postOrderTraversal(root.left, sum, targetSum) 
+            || postOrderTraversal(root.right, sum, targetSum));
     }
 }
-
-
-
-
