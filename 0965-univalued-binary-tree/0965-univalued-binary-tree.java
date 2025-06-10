edbea8892979108cf6f1f126661ed1a1a2e294a8
lyn <46067184+lynyam@users.noreply.github.com>
2025-06-10 15:05:48 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0965-univalued-binary-tree/0965-univalued-binary-tree.java
+++ 0965-univalued-binary-tree/0965-univalued-binary-tree.java
@@ -16,8 +16,7 @@
 class Solution {
     public boolean isUnivalTree(TreeNode root) {
         if (root == null) return (true);
-        if (root.left != null && root.val != root.left.val) return (false); 
-        if (root.right != null && root.val != root.right.val) return (false);
-        return (isUnivalTree(root.left) && isUnivalTree(root.right));
+        if ((root.left != null && root.val != root.left.val) || (root.right != null && root.val != root.right.val)) return false;
+        return (isUnivalTree(root.left) && isUnivalTree(root.left));
     }
 }
