88d55db4ddb97c3bc2559f77b478f24796cd885a
lyn <46067184+lynyam@users.noreply.github.com>
2025-07-22 10:35:17 +0200

    Time: 7 ms (54.61%), Space: 46.5 MB (53.71%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -15,25 +15,38 @@
  */
 class Solution {
     public int deepestLeavesSum(TreeNode root) {
-        if (root == null) return (0);
+        int result = 0;
+        if (root == null)
+            return (result);
+        //Two step to resolve this problem
+        //1- Initialisation (2 step)
+        //1.1 declaration of queue 
         Queue<TreeNode> queue = new LinkedList<>();
+        //1.2 add Root to queue
         queue.add(root);
-        int sum = 0;
-
+        //2- Parcours (2 step)
+        //2.1 While loop while level exists (2 step)
         while (!queue.isEmpty()) {
-            int i = 0;
+            //2.1.1 calculate number of node in level
             int size = queue.size();
-            sum = 0;
-            while (i < size) {
+            //2.1.2 make logic for level (here it is scalar sum of element in a node)
+            int sum = 0;
+            //2.2 FOR loop to walkthrough each node in the level (2 level)
+            for (int i = 0; i < size; i++) {
+                //2.2.1 get the curr node by remove in queue
                 TreeNode node = queue.remove();
+                //2.2.2 make some logic on the node (here it just add to sum)
                 sum += node.val;
+                //2.2.3 call the left and right child if it exist
                 if (node.left != null)
                     queue.add(node.left);
                 if (node.right != null)
                     queue.add(node.right);
-                i++;
             }
+            result = sum;
+
         }
-        return (sum);
+        return (result);
+
     }
 }
