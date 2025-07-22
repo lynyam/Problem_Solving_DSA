d4c4e1a201435e5095eb4dbf9d5359f2040d4554
lyn <46067184+lynyam@users.noreply.github.com>
2025-07-22 09:49:25 +0200

    Time: 2 ms (84.36%), Space: 45.6 MB (32.07%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
@@ -15,26 +15,37 @@
  */
 class Solution {
     public List<Integer> largestValues(TreeNode root) {
-        if (root == null) return (new ArrayList<>());
+        List<Integer> result = new ArrayList<>();
+        if (root == null)
+            return (result);
+        //1 - Initialisation (2step) 
+        //1.1 init Queue
         Queue<TreeNode> queue = new LinkedList<>();
+        //1.2 Add root to queue
         queue.add(root);
-        List<Integer> ans = new ArrayList<>();
 
+        //2- Parcours (2 step)
+        //2.1 While loop if level exist (2 step)
         while (!queue.isEmpty()) {
-            int i = 0;
+            //2.1.1 calculate number of node in level
             int size = queue.size();
+            //2.1.2 Logic on level. Here we need to the max of all level
             int max = Integer.MIN_VALUE;
-            while (i < size) {
+            //2.2 For loop to walkthrough on each level (3 step)
+            for (int i = 0; i < size; i++) {
+                //2.2.1 get curr node with remove
                 TreeNode node = queue.remove();
+                //2.2.2 make some logic on node depends on the problem (Here it is see if the node is new max)
                 max = Math.max(max, node.val);
+                //2.2.3 call left and right child to add a queue if it exist
                 if (node.left != null)
                     queue.add(node.left);
                 if (node.right != null)
                     queue.add(node.right);
-                i++;
             }
-            ans.add(max);
+            result.add(max);
         }
-        return (ans);
+        return (result);
     }
+
 }
