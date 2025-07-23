bab3375a65d8f1c8662969c67fc59ddc36fb68ba
lyn <46067184+lynyam@users.noreply.github.com>
2025-07-23 06:15:53 +0200

    Time: 130 ms (5.64%), Space: 68.2 MB (20.21%) - LeetHub

--- 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.java
+++ 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.java
@@ -15,23 +15,36 @@
  */
 class Solution {
     public long kthLargestLevelSum(TreeNode root, int k) {
-        List<Long> levelSum = new ArrayList<>();
-        dfs(root, 0, levelSum);
-        if (levelSum.size() < k)
+        if (root == null || k <= 0)
             return (-1);
-        Collections.sort(levelSum);
-        return (levelSum.get(levelSum.size() - k));
-    }
+        //Initialisation of queue and add root node
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        PriorityQueue<Long> pq = new PriorityQueue<>(Comparator.reverseOrder());
 
-    public void dfs(TreeNode node, int depth, List<Long> levelSum) {
-        if (node == null)
-            return;
-        if (levelSum.size() <= depth) {
-            levelSum.add((long)node.val);
-        } else {
-            levelSum.set(depth, levelSum.get(depth) + node.val);
+        //Level by Level, compute sum and add to pq
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            long sum = 0;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.remove();
+                sum += node.val;
+                if (node.left != null)
+                    queue.add(node.left);
+                if (node.right != null)
+                    queue.add(node.right);
+            }
+            pq.add(sum);
         }
-        dfs(node.left, depth + 1, levelSum);
-        dfs(node.right, depth + 1, levelSum);
+        if (pq.size() < k)
+            return (-1);
+        int i = 1;
+        while (i < k) {
+            pq.remove();
+            i++;
+        }
+        return (pq.remove());
+
     }
 }
