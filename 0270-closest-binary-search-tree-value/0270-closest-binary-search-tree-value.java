784533132baf4d727f1dc0056aecb5a2c7e56382
lyn <46067184+lynyam@users.noreply.github.com>
2025-07-25 13:47:59 +0200

    Time: 0 ms (100%), Space: 43 MB (68.7%) - LeetHub

--- 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.java
+++ 0270-closest-binary-search-tree-value/0270-closest-binary-search-tree-value.java
@@ -14,25 +14,17 @@
  * }
  */
 class Solution {
-    double diff = Double.MAX_VALUE;
-    int ans = Integer.MAX_VALUE;
-    double target;
     public int closestValue(TreeNode root, double target) {
-        this.target = target;
-        if (root == null) return (0);
-        bst(root);
-        return (ans);
-    }
-
-    public void bst(TreeNode root) {
-        if (root == null)
-            return ;
-        ans = diff > Math.abs(root.val - target) ? root.val : diff == 
-            Math.abs(root.val - target) ? Math.min(ans, root.val) : ans;
-        diff = Math.min(diff , Math.abs(root.val - target));
-        if (root.val < target)
-            bst(root.right);
-        if (root.val > target)
-            bst(root.left);
+        int result = Integer.MAX_VALUE;
+        while (root != null) {
+            if (root.val - target == 0) return (root.val);
+            if (Math.abs(root.val - target) < Math.abs(result - target)) {
+                result = root.val;
+            } else if (Math.abs(root.val - target) == Math.abs(result - target)) {
+                result = Math.min(result, root.val);
+            }
+            root = root.val < target ? root.right : root.left;
+        }
+        return (result);
     }
 }
