c6b2bb65833735acff61c7fa08e25027f347aefa
lyn <46067184+lynyam@users.noreply.github.com>
2025-07-29 17:55:55 +0200

    Time: 3 ms (62.95%), Space: 45.3 MB (51.4%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -13,16 +13,76 @@
  *     }
  * }
  */
+class BSTIterator {
+        private Stack<TreeNode> stack = new Stack<TreeNode>();
+    
+        public BSTIterator(TreeNode root) {
+            pushAllLeft(root);
+        }
+
+        public int next() {
+            TreeNode node = stack.pop();
+            pushAllLeft(node.right);
+            return (node.val);
+        }
+
+        public void pushAllLeft(TreeNode node) {
+            while (node != null) {
+                stack.add(node);
+                node = node.left;
+            }
+        }
+
+        public boolean hasNext() {
+            return (!stack.isEmpty());
+        }
+}
+
+    class BSTIteratorRev {
+        private Stack<TreeNode> stack = new Stack<TreeNode>();
+    
+        public BSTIteratorRev(TreeNode root) {
+            pushAllRight(root);
+        }
+
+        public int next() {
+            TreeNode node = stack.pop();
+            pushAllRight(node.left);
+            return (node.val);
+        }
+
+        public void pushAllRight(TreeNode node) {
+            while (node != null) {
+                stack.add(node);
+                node = node.right;
+            }
+        }
+
+        public boolean hasNext() {
+            return (!stack.isEmpty());
+        }
+}
+
 class Solution {
     public boolean findTarget(TreeNode root, int k) {
-        Set<Integer> set = new HashSet<>();
-        return (dfs(root, k, set));
-    }
-
-    public boolean dfs(TreeNode root, int k, Set<Integer> set) {
-        if (root == null) return false;
-        if (set.contains(k - root.val)) return (true);
-        set.add(root.val);
-        return dfs(root.left, k, set) || dfs(root.right, k, set);
+        if (root == null) return (false);
+        BSTIterator leftIterator = new BSTIterator(root);
+        BSTIteratorRev rightIterator = new BSTIteratorRev(root);
+        int left = leftIterator.next();
+        int right = rightIterator.next();
+        while (left < right) {
+            if (left + right == k) return (true);
+            if (left + right < k) {
+                if (!leftIterator.hasNext())
+                    return (false);
+                left = leftIterator.next();
+            }  
+            else {
+                if (!rightIterator.hasNext())
+                    return (false);
+                right = rightIterator.next();
+            }     
+        }
+        return (false);
     }
 }
